e64b2330da0dccf0985c83453200943dd08d69c0
kgoodhop <kgoodhope@linkedin.com>
2014-02-13 13:31:59 -0800

    resolving conflicts

--- source-api/src/main/java/com/linkedin/uif/source/Source.java
+++ source-api/src/main/java/com/linkedin/uif/source/Source.java
@@ -1,16 +1,17 @@
 package com.linkedin.uif.source;
 
 import java.util.List;
-import java.util.Properties;
 
-import com.linkedin.uif.configuration.WorkUnitState;
 import com.linkedin.uif.configuration.SourceState;
+import com.linkedin.uif.configuration.WorkUnitState;
 import com.linkedin.uif.source.extractor.Extractor;
 import com.linkedin.uif.source.workunit.WorkUnit;
 
 public interface Source<S, D>
 {
-  public abstract List<WorkUnit> getWorkunits(SourceState context);
+  public abstract List<WorkUnit> getWorkunits(SourceState state);
 
   public abstract Extractor<S, D> getExtractor(WorkUnitState state);
+  
+  public abstract void publishSourceMeta(SourceState state);
 }
