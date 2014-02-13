be463cd0f62a7452b7016b5bcf7411aca8ece62b
kgoodhop <kgoodhope@linkedin.com>
2014-02-13 13:21:45 -0800

    committing current changes

--- source-api/src/main/java/com/linkedin/uif/source/Source.java
+++ source-api/src/main/java/com/linkedin/uif/source/Source.java
@@ -1,9 +1,10 @@
 package com.linkedin.uif.source;
 
 import java.util.List;
+import java.util.Properties;
 
-import com.linkedin.uif.configuration.SourceState;
 import com.linkedin.uif.configuration.WorkUnitState;
+import com.linkedin.uif.configuration.SourceState;
 import com.linkedin.uif.source.extractor.Extractor;
 import com.linkedin.uif.source.workunit.WorkUnit;
 
