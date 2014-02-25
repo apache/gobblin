2d7c2af209e369351b7eca56f4b1d2e113947fad
Sahil Takiar <stakiar@stakiar-mn1.linkedin.biz>
2014-02-25 15:53:02 -0800

    RB=266570

--- qualitychecker/src/main/java/com/linkedin/uif/publisher/TaskPublisherBuilderFactory.java
+++ qualitychecker/src/main/java/com/linkedin/uif/publisher/TaskPublisherBuilderFactory.java
@@ -1,12 +1,12 @@
 package com.linkedin.uif.publisher;
 
 import com.linkedin.uif.configuration.MetaStoreClient;
+import com.linkedin.uif.configuration.WorkUnitState;
 import com.linkedin.uif.qualitychecker.PolicyCheckResults;
-import com.linkedin.uif.scheduler.TaskState;
 
 public class TaskPublisherBuilderFactory
 {
-    public TaskPublisherBuilder newTaskPublisherBuilder(TaskState taskState, PolicyCheckResults results, MetaStoreClient collector) {
-        return new TaskPublisherBuilder(taskState, results, collector);
+    public TaskPublisherBuilder newTaskPublisherBuilder(WorkUnitState workUnitState, PolicyCheckResults results, MetaStoreClient collector) {
+        return new TaskPublisherBuilder(workUnitState, results, collector);
     }
 }
