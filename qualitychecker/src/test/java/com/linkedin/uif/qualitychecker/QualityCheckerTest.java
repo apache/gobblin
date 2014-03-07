e44237062c1d03267045c6a0ac7b45309b049b13
Sahil Takiar <stakiar@stakiar-mn1.linkedin.biz>
2014-03-06 17:13:34 -0800

    Fixing build

--- qualitychecker/src/test/java/com/linkedin/uif/qualitychecker/QualityCheckerTest.java
+++ qualitychecker/src/test/java/com/linkedin/uif/qualitychecker/QualityCheckerTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 import com.linkedin.uif.configuration.ConfigurationKeys;
 import com.linkedin.uif.configuration.State;
+import com.linkedin.uif.test.*;
 
 @Test(groups = {"com.linkedin.uif.qualitychecker"})
 public class QualityCheckerTest
@@ -15,7 +16,7 @@ public class QualityCheckerTest
     @Test
     public void testPolicyChecker() throws Exception {        
         State state = new State();
-        state.setProp(ConfigurationKeys.POLICY_LIST, "com.linkedin.uif.qualitychecker.TestPolicy");
+        state.setProp(ConfigurationKeys.POLICY_LIST, "com.linkedin.uif.test.TestPolicy");
         state.setProp(ConfigurationKeys.POLICY_LIST_TYPE, "MANDATORY");
 
         PolicyCheckResults results = getPolicyResults(state);
@@ -27,7 +28,7 @@ public class QualityCheckerTest
     @Test
     public void testMultiplePolicies() throws Exception {        
         State state = new State();
-        state.setProp(ConfigurationKeys.POLICY_LIST, "com.linkedin.uif.qualitychecker.TestPolicy,com.linkedin.uif.qualitychecker.TestPolicy");
+        state.setProp(ConfigurationKeys.POLICY_LIST, "com.linkedin.uif.test.TestPolicy,com.linkedin.uif.test.TestPolicy");
         state.setProp(ConfigurationKeys.POLICY_LIST_TYPE, "MANDATORY,MANDATORY");
 
         PolicyCheckResults results = getPolicyResults(state);
--- test/src/main/java/com/linkedin/uif/test/TestPolicy.java
+++ test/src/main/java/com/linkedin/uif/test/TestPolicy.java
@@ -2,8 +2,6 @@ package com.linkedin.uif.test;
 
 import com.linkedin.uif.configuration.State;
 import com.linkedin.uif.qualitychecker.Policy;
-import com.linkedin.uif.qualitychecker.Policy.Result;
-import com.linkedin.uif.qualitychecker.Policy.Type;
 
 public class TestPolicy extends Policy
 {
