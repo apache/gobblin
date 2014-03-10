7fa33e939018ad3479368b89b33886cd4ee42e82
Narasimha Veeramreddy <nveeramr@nveeramr-mn1.linkedin.biz>
2014-03-10 16:22:24 -0700

    Remove throws ExtractPrepareException from Source.java interface in common-api
    RB=272770
    R=stakiar,kgoodhop,lqiao,ynli
    A=stakiar

--- common-api/src/main/java/com/linkedin/uif/source/Source.java
+++ common-api/src/main/java/com/linkedin/uif/source/Source.java
@@ -5,13 +5,12 @@ import java.util.List;
 import com.linkedin.uif.configuration.SourceState;
 import com.linkedin.uif.configuration.WorkUnitState;
 import com.linkedin.uif.source.extractor.Extractor;
-import com.linkedin.uif.source.extractor.exception.ExtractPrepareException;
 import com.linkedin.uif.source.workunit.WorkUnit;
 
 public interface Source<S, D> {
   public abstract List<WorkUnit> getWorkunits(SourceState state);
 
-  public abstract Extractor<S, D> getExtractor(WorkUnitState state) throws ExtractPrepareException;
+  public abstract Extractor<S, D> getExtractor(WorkUnitState state);
   
   public abstract void shutdown(SourceState state);
 }
