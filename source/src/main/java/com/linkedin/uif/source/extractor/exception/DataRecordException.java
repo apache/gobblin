e3c07cd6c1c5b449219e8798960df78c90a6e5ec
Narasimha Veeramreddy <nveeramr@nveeramr-mn1.linkedin.biz>
2014-03-10 17:42:55 -0700

    Refer DataRecordException in common-api instead of source
    RB=272881
    R=stakiar,kgoodhop,lqiao,ynli
    A=stakiar

--- source/src/main/java/com/linkedin/uif/source/extractor/exception/DataRecordException.java
+++
@@ -1,9 +0,0 @@
-package com.linkedin.uif.source.extractor.exception;
-
-public class DataRecordException extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public DataRecordException(String message) {
-		super(message);
-	}
-}
