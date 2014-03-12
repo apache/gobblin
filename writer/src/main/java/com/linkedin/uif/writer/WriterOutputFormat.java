43d1e1de1d4f3da65c56b8f6623d54f56a6983b5
Sahil Takiar <stakiar@stakiar-mn1.linkedin.biz>
2014-03-12 14:05:47 -0700

    Adding file extensions

--- writer/src/main/java/com/linkedin/uif/writer/WriterOutputFormat.java
+++ writer/src/main/java/com/linkedin/uif/writer/WriterOutputFormat.java
@@ -6,7 +6,20 @@ package com.linkedin.uif.writer;
  * @author ynli
  */
 public enum WriterOutputFormat {
-    AVRO,
-    PARQUET,
-    CSV
+    AVRO ("avro"),
+    PARQUET ("parquet"),
+    CSV ("csv");
+    
+    /**
+     * Extension specifies the file name extension
+     */
+    private final String extension;
+    
+    WriterOutputFormat(String extension) {
+        this.extension = extension;
+    }
+    
+    public String getExtension() {
+        return this.extension;
+    }
 }
