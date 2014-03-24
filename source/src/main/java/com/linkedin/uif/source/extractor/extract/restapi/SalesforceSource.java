6aa4bfc82e7b1d73dd75a92dbc7dea7b2c55dea7
Narasimha Veeramreddy <nveeramr@nveeramr-mn1.linkedin.biz>
2014-03-24 11:41:52 -0700

    Use Logging instead of printStackTrace()
    RB=279101
    R=stakiar,kgoodhop,lqiao,ynli
    A=ynli

--- source/src/main/java/com/linkedin/uif/source/extractor/extract/restapi/SalesforceSource.java
+++ source/src/main/java/com/linkedin/uif/source/extractor/extract/restapi/SalesforceSource.java
@@ -2,6 +2,9 @@ package com.linkedin.uif.source.extractor.extract.restapi;
 
 import java.io.IOException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.linkedin.uif.configuration.WorkUnitState;
 import com.linkedin.uif.source.extractor.Extractor;
 import com.linkedin.uif.source.extractor.exception.ExtractPrepareException;
@@ -11,14 +14,15 @@ import com.linkedin.uif.source.extractor.extract.BaseSource;
  * An implementation of salesforce source to get work units
  */
 public class SalesforceSource<S, D> extends BaseSource<S, D> {
+	private static final Log LOG = LogFactory.getLog(BaseSource.class);
 
 	public Extractor<S, D> getExtractor(WorkUnitState state) throws IOException {
 		Extractor<S, D> extractor = null;
 		try {
 			extractor = new SalesforceExtractor<S, D>(state).build();
 		} catch (ExtractPrepareException e) {
-			e.printStackTrace();
-			throw new IOException("Failed to prepare extractor; error-" + e.getMessage());
+			LOG.error("Failed to prepare extractor: error -" + e.getMessage());
+			throw new IOException(e);
 		}
 		return extractor;
 	}
