28a98ea3ccefc572b146a227f144e7b8cb521912
Narasimha Veeramreddy <nveeramr@nveeramr-mn1.linkedin.biz>
2014-03-24 11:24:10 -0700

    Adding IOException to the getExtractor() method

--- source/src/main/java/com/linkedin/uif/source/extractor/extract/restapi/SalesforceSource.java
+++ source/src/main/java/com/linkedin/uif/source/extractor/extract/restapi/SalesforceSource.java
@@ -1,5 +1,7 @@
 package com.linkedin.uif.source.extractor.extract.restapi;
 
+import java.io.IOException;
+
 import com.linkedin.uif.configuration.WorkUnitState;
 import com.linkedin.uif.source.extractor.Extractor;
 import com.linkedin.uif.source.extractor.exception.ExtractPrepareException;
@@ -10,12 +12,13 @@ import com.linkedin.uif.source.extractor.extract.BaseSource;
  */
 public class SalesforceSource<S, D> extends BaseSource<S, D> {
 
-	public Extractor<S, D> getExtractor(WorkUnitState state) {
+	public Extractor<S, D> getExtractor(WorkUnitState state) throws IOException {
 		Extractor<S, D> extractor = null;
 		try {
 			extractor = new SalesforceExtractor<S, D>(state).build();
 		} catch (ExtractPrepareException e) {
 			e.printStackTrace();
+			throw new IOException("Failed to prepare extractor; error-" + e.getMessage());
 		}
 		return extractor;
 	}
