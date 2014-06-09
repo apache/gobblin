7024f68a91eb27f8d4c2ab06d22baa650ffee695
Yinan Li <liyinan926@gmail.com>
2014-06-09 11:25:17 -0700

    Updated the tool for state migration to do more things
    Signed-off-by: Yinan Li <ynli@linkedin.com>
    
    RB=314055
    R=stakiar,kgoodhop,lqiao
    A=stakiar

--- scheduler/src/main/java/com/linkedin/uif/scheduler/TaskStateMigrationUtil.java
+++ scheduler/src/main/java/com/linkedin/uif/scheduler/TaskStateMigrationUtil.java
@@ -2,6 +2,8 @@ package com.linkedin.uif.scheduler;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
@@ -14,6 +16,7 @@ import com.google.common.collect.Lists;
 
 import com.linkedin.uif.metastore.FsStateStore;
 import com.linkedin.uif.metastore.StateStore;
+import com.linkedin.uif.runtime.JobState;
 
 /**
  * A utility for migrating task states due to a recent package change.
@@ -31,7 +34,7 @@ public class TaskStateMigrationUtil {
      * @param srcDir Source task state store directory
      * @param destDir Destination task state store directory
      */
-    public static void migrate(String fsUri, String srcDir, String destDir) throws IOException {
+    public static void migrateTaskStates(String fsUri, String srcDir, String destDir) throws IOException {
         StateStore srcStore = new FsStateStore(fsUri, srcDir, TaskState.class);
         StateStore destStore = new FsStateStore(
                 fsUri, destDir, com.linkedin.uif.runtime.TaskState.class);
@@ -46,6 +49,7 @@ public class TaskStateMigrationUtil {
 
         for (FileStatus store : stores) {
             String storeName = store.getPath().getName();
+
             FileStatus[] tables = fs.listStatus(store.getPath(), new PathFilter() {
                 @Override
                 public boolean accept(Path path) {
@@ -57,10 +61,61 @@ public class TaskStateMigrationUtil {
                 System.out.println("No task states in store " + storeName);
                 continue;
             }
+
+            sortFileStatuses(tables);
+            // After sorting, the first table in the array has the largest modification time
+            destStore.createAlias(storeName, tables[0].getPath().getName(), "current" + ".tst");
+
             for (FileStatus table : tables) {
                 String tableName = table.getPath().getName();
                 List<TaskState> srcTaskStates = (List<TaskState>) srcStore.getAll(storeName, tableName);
-                destStore.putAll(storeName, tableName, migrateTaskStates(srcTaskStates));
+                destStore.putAll(storeName, tableName, createNewTaskStates(srcTaskStates));
+            }
+        }
+    }
+
+    /**
+     * Copy existing job states.
+     *
+     * @param fsUri File system URI
+     * @param srcDir Source task state store directory
+     * @param destDir Destination task state store directory
+     */
+    public static void copyJobStates(String fsUri, String srcDir, String destDir) throws IOException {
+        StateStore srcStore = new FsStateStore(fsUri, srcDir, JobState.class);
+        StateStore destStore = new FsStateStore(fsUri, destDir, JobState.class);
+
+        FileSystem fs = FileSystem.get(URI.create(fsUri), new Configuration());
+
+        FileStatus[] stores = fs.listStatus(new Path(srcDir));
+        if (stores == null || stores.length == 0) {
+            System.out.println("No job states to copy");
+            return;
+        }
+
+        for (FileStatus store : stores) {
+            String storeName = store.getPath().getName();
+
+            FileStatus[] tables = fs.listStatus(store.getPath(), new PathFilter() {
+                @Override
+                public boolean accept(Path path) {
+                    // We are only copying job states
+                    return path.getName().endsWith(".jst");
+                }
+            });
+            if (tables == null || tables.length == 0) {
+                System.out.println("No job states in store " + storeName);
+                continue;
+            }
+
+            sortFileStatuses(tables);
+            // After sorting, the first table in the array has the largest modification time
+            destStore.createAlias(storeName, tables[0].getPath().getName(), "current" + ".jst");
+
+            for (FileStatus table : tables) {
+                String tableName = table.getPath().getName();
+                List<JobState> jobStates = (List<JobState>) srcStore.getAll(storeName, tableName);
+                destStore.putAll(storeName, tableName, jobStates);
             }
         }
     }
@@ -68,7 +123,7 @@ public class TaskStateMigrationUtil {
     /**
      * Migrate individual task states.
      */
-    private static List<com.linkedin.uif.runtime.TaskState> migrateTaskStates(
+    private static List<com.linkedin.uif.runtime.TaskState> createNewTaskStates(
             List<TaskState> srcTaskStates) {
 
         List<com.linkedin.uif.runtime.TaskState> destTaskStates = Lists.newArrayList();
@@ -86,12 +141,28 @@ public class TaskStateMigrationUtil {
         return destTaskStates;
     }
 
+    /**
+     * Sort an array of {@link FileStatus} by modification time in descending order.
+     */
+    private static void sortFileStatuses(FileStatus[] statuses) {
+        Arrays.sort(statuses, new Comparator<FileStatus>() {
+            @Override
+            public int compare(FileStatus fileStatus1, FileStatus fileStatus2) {
+                Long modTime1 = fileStatus1.getModificationTime();
+                Long modTime2 = fileStatus2.getModificationTime();
+                // Sort the file statuses by modification time in descending order
+                return -modTime1.compareTo(modTime2);
+            }
+        });
+    }
+
     public static void main(String[] args) throws IOException {
         if (args.length != 3) {
             System.out.println("Usage: TaskStateMigrationUtil <fs uri> <src store dir> <dest store dir>");
             System.exit(1);
         }
 
-        migrate(args[0], args[1], args[2]);
+        migrateTaskStates(args[0], args[1], args[2]);
+        copyJobStates(args[0], args[1], args[2]);
     }
 }
