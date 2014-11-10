d40acbbcf0aa556695db1b46bf36c71bae3745b1
Yinan Li <liyinan926@gmail.com>
2014-11-10 12:36:16 -0800

    Use different property keys to control enabling of alert and notification emails
    Signed-off-by: Yinan Li <ynli@linkedin.com>
    
    RB=391724
    R=etl-infra-reviewers
    A=stakiar

--- runtime/src/main/java/com/linkedin/uif/runtime/EmailNotificationJobListener.java
+++ runtime/src/main/java/com/linkedin/uif/runtime/EmailNotificationJobListener.java
@@ -16,19 +16,17 @@ public class EmailNotificationJobListener implements JobListener {
 
     @Override
     public void jobCompleted(JobState jobState) {
-        boolean emailNotificationEnabled = Boolean.valueOf(jobState.getProp(
-                ConfigurationKeys.EMAIL_NOTIFICATION_ENABLED_KEY, Boolean.toString(false)));
-        if (!emailNotificationEnabled) {
-            LOGGER.info("Email notification is not enabled for job " + jobState.getJobName());
-            return;
-        }
+        boolean alertEmailEnabled = Boolean.valueOf(jobState.getProp(
+                ConfigurationKeys.ALERT_EMAIL_ENABLED_KEY, Boolean.toString(false)));
+        boolean notificationEmailEnabled = Boolean.valueOf(jobState.getProp(
+                ConfigurationKeys.NOTIFICATION_EMAIL_ENABLED_KEY, Boolean.toString(false)));
 
         // Send out alert email if the maximum number of consecutive failures is reached
         if (jobState.getState() == JobState.RunningState.FAILED) {
             int failures = jobState.getPropAsInt(ConfigurationKeys.JOB_FAILURES_KEY, 0);
             int maxFailures = jobState.getPropAsInt(ConfigurationKeys.JOB_MAX_FAILURES_KEY,
                     ConfigurationKeys.DEFAULT_JOB_MAX_FAILURES);
-            if (failures >= maxFailures) {
+            if (alertEmailEnabled && failures >= maxFailures) {
                 try {
                     EmailUtils.sendJobFailureAlertEmail(
                             jobState.getJobName(), jobState.toString(), failures, jobState);
@@ -39,11 +37,13 @@ public class EmailNotificationJobListener implements JobListener {
             }
         }
 
-        try {
-            EmailUtils.sendJobCompletionEmail(
-                    jobState.getJobName(), jobState.toString(), jobState.getState().name(), jobState);
-        } catch (EmailException ee) {
-            LOGGER.error("Failed to send job completion notification email for job " + jobState.getJobId(), ee);
+        if (notificationEmailEnabled) {
+            try {
+                EmailUtils.sendJobCompletionEmail(jobState.getJobName(), jobState.toString(),
+                        jobState.getState().name(), jobState);
+            } catch (EmailException ee) {
+                LOGGER.error("Failed to send job completion notification email for job " + jobState.getJobId(), ee);
+            }
         }
     }
 }
