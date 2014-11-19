80409f675790ae408e00daa5e59b2e7ee19450b3
Yinan Li <liyinan926@gmail.com>
2014-11-19 13:28:25 -0800

    Fixed a bug with staging data cleanup that caused data commit failures
    Jira: ETL-2321
    
    Signed-off-by: Yinan Li <ynli@linkedin.com>

--- writer/src/main/java/com/linkedin/uif/writer/AvroHdfsDataWriter.java
+++ writer/src/main/java/com/linkedin/uif/writer/AvroHdfsDataWriter.java
@@ -54,11 +54,11 @@ class AvroHdfsDataWriter implements DataWriter<GenericRecord> {
                 ForkOperatorUtils.getPropertyNameForBranch(ConfigurationKeys.WRITER_FILE_SYSTEM_URI, branch),
                 ConfigurationKeys.LOCAL_FS_URI);
         String stagingDir = properties.getProp(
-                ForkOperatorUtils.getPropertyNameForBranch(ConfigurationKeys.WRITER_STAGING_DIR, branch),
-                ConfigurationKeys.DEFAULT_STAGING_DIR) + Path.SEPARATOR + relFilePath;
+                ForkOperatorUtils.getPropertyNameForBranch(ConfigurationKeys.WRITER_STAGING_DIR, branch)) +
+                Path.SEPARATOR + relFilePath;
         String outputDir = properties.getProp(
-                ForkOperatorUtils.getPropertyNameForBranch(ConfigurationKeys.WRITER_OUTPUT_DIR, branch),
-                ConfigurationKeys.DEFAULT_OUTPUT_DIR) + Path.SEPARATOR + relFilePath;
+                ForkOperatorUtils.getPropertyNameForBranch(ConfigurationKeys.WRITER_OUTPUT_DIR, branch)) +
+                Path.SEPARATOR + relFilePath;
         String codecType = properties.getProp(
                 ForkOperatorUtils.getPropertyNameForBranch(ConfigurationKeys.WRITER_CODEC_TYPE, branch),
                 AvroHdfsDataWriter.CodecType.DEFLATE.name());
