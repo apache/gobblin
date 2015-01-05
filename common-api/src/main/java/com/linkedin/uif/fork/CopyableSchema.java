8b69b9468c6359944a9d7012d9b5b0c6db40f9d6
Sahil Takiar <stakiar@linkedin.com>
2015-01-05 15:08:38 -0800

    Set validation to false when copying a schema

--- common-api/src/main/java/com/linkedin/uif/fork/CopyableSchema.java
+++ common-api/src/main/java/com/linkedin/uif/fork/CopyableSchema.java
@@ -1,9 +1,9 @@
 /* (c) 2014 LinkedIn Corp. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
  * this file except in compliance with the License. You may obtain a copy of the
  * License at  http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed
  * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
  * CONDITIONS OF ANY KIND, either express or implied.
@@ -13,6 +13,7 @@ package com.linkedin.uif.fork;
 
 import org.apache.avro.Schema;
 
+
 /**
  * A wrapper class for {@link org.apache.avro.Schema} that is also {@link Copyable}.
  *
@@ -20,14 +21,15 @@ import org.apache.avro.Schema;
  */
 public class CopyableSchema implements Copyable<Schema> {
 
-    private final Schema schema;
+  private final Schema schema;
 
-    public CopyableSchema(Schema schema) {
-        this.schema = schema;
-    }
+  public CopyableSchema(Schema schema) {
+    this.schema = schema;
+  }
 
-    @Override
-    public Schema copy() throws CopyNotSupportedException {
-        return new Schema.Parser().parse(this.schema.toString());
-    }
+  @Override
+  public Schema copy()
+      throws CopyNotSupportedException {
+    return new Schema.Parser().setValidate(false).parse(this.schema.toString());
+  }
 }
