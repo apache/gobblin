08d83829df0042faa03acab17eafbee564a1011a
jfleong <jason.f.leong@gmail.com>
2015-05-21 13:33:11 -0700

    adding jvm flags to gobblin-standalone
    * also adding jvm falgs to gobblin-env.sh

--- conf/gobblin-env.sh
+++ conf/gobblin-env.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 # Set Gobblin specific environment variables here.
+export JVM_FLAGS="-Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=localhost"
