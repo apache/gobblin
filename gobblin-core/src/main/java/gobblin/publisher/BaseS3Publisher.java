b2068399671394078defe5b13b185d53f052dd46
Akshay Nanavati <akshay@nerdwallet.com>
2015-05-26 20:27:01 -0700

    Set part size to 500 to prevent part number overflowing

--- gobblin-core/src/main/java/gobblin/publisher/BaseS3Publisher.java
+++ gobblin-core/src/main/java/gobblin/publisher/BaseS3Publisher.java
@@ -51,7 +51,7 @@ public abstract class BaseS3Publisher extends BaseDataPublisher {
   protected final int s3Partitions;
 
   private static final Logger LOG = LoggerFactory.getLogger(BaseS3Publisher.class);
-  private static final long PART_SIZE = 50 * 1024 * 1024; // 50 mb chunks to s3
+  private static final long PART_SIZE = 500 * 1024 * 1024; // 500 mb chunks to s3
 
   public BaseS3Publisher(State state) {
     super(state);
