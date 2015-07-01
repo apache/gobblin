c7ac6fa452e803d44ae6268d778577c86645594b
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2015-07-01 15:22:27 -0700

    Generalized protobuf serialization converter
    Generalized protobuf serialization converter to work for all messages,
    not just log file

--- gobblin-core/src/main/java/gobblin/converter/s3/ProtobufMessageSerializerConverter.java
+++ gobblin-core/src/main/java/gobblin/converter/s3/ProtobufMessageSerializerConverter.java
@@ -10,28 +10,28 @@
  */
 package gobblin.converter.s3;
 
+import com.google.protobuf.Message;
 import gobblin.configuration.WorkUnitState;
 import gobblin.converter.Converter;
 import gobblin.converter.DataConversionException;
 import gobblin.converter.SchemaConversionException;
 import gobblin.converter.SingleRecordIterable;
-import gobblin.converter.s3.LogFileOuterClass.LogFile;
 
 
 /**
- * Writes the Protobuf LogFile to a byte array to be read in by a byte writer.
+ * Writes a protobuf message to a byte array to be read in by a byte writer.
  *
  * @author ahollenbach@nerdwallet.com
  */
-public class ProtobufSerializerConverter extends Converter<Class<LogFile>, Class<byte[]>, LogFile, byte[]> {
+public class ProtobufMessageSerializerConverter extends Converter<Class<Message>, Class<byte[]>, Message, byte[]> {
   @Override
-  public Class<byte[]> convertSchema(Class<LogFile> inputSchema, WorkUnitState workUnit)
+  public Class<byte[]> convertSchema(Class<Message> inputSchema, WorkUnitState workUnit)
       throws SchemaConversionException {
     return byte[].class;
   }
 
   @Override
-  public Iterable<byte[]> convertRecord(Class<byte[]> outputSchema, LogFile inputRecord, WorkUnitState workUnit)
+  public Iterable<byte[]> convertRecord(Class<byte[]> outputSchema, Message inputRecord, WorkUnitState workUnit)
       throws DataConversionException {
     return new SingleRecordIterable<byte[]>(inputRecord.toByteArray());
   }
