729d3aa32583a9161cafffd67559ff19ca5fc720
Erik Krogen <erikkrogen@gmail.com>
2015-07-01 17:30:10 -0700

    Mostly finished addressing review comments but realized that my current scheme can't handle multiple files being output.

--- gobblin-core/src/main/java/gobblin/publisher/TimePartitionedDataWithRowCountsPublisher.java
+++ gobblin-core/src/main/java/gobblin/publisher/TimePartitionedDataWithRowCountsPublisher.java
@@ -12,21 +12,20 @@
 
 package gobblin.publisher;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Set;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.google.common.collect.Sets;
 import gobblin.configuration.ConfigurationKeys;
 import gobblin.configuration.State;
 import gobblin.configuration.WorkUnitState;
 import gobblin.util.ForkOperatorUtils;
 import gobblin.util.WriterUtils;
 import gobblin.writer.WriterOutputFormat;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.Path;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 /**
  * For time partition jobs, writer output directory is
@@ -55,7 +54,7 @@ public class TimePartitionedDataWithRowCountsPublisher extends TimePartitionedDa
   @Override
   public void publishData(Collection<? extends WorkUnitState> states) throws IOException {
 
-    Set<Path> publisherDirsProcessed = new HashSet<Path>();
+    Set<Path> publisherDirsProcessed = Sets.newHashSet();
 
     for (WorkUnitState workUnitState : states) {
       for (int branchId = 0; branchId < this.numBranches; branchId++) {
@@ -70,55 +69,14 @@ public class TimePartitionedDataWithRowCountsPublisher extends TimePartitionedDa
           break;
         }
 
-        String fileExtension = WriterOutputFormat.valueOf(workUnitState.getProp(ForkOperatorUtils
-                .getPropertyNameForBranch(ConfigurationKeys.WRITER_OUTPUT_FORMAT_KEY, this.numBranches, branchId),
-            WriterOutputFormat.AVRO.name())).getExtension();
-
-        String writerOutputFileName = WriterUtils.getWriterFileName(workUnitState, this.numBranches,
-            branchId, workUnitState.getId(), fileExtension);
-
-        Path writerOutputFullGlob =
-            getWriterOutputGlobMatcher(workUnitState, branchId, writerOutputDir, writerOutputFileName);
-
-        Path writerOutputFilePath = null;
-        for (FileStatus status : this.fss.get(branchId).globStatus(writerOutputFullGlob)) {
-          if (writerOutputFilePath != null) {
-            LOG.warn("Found more than one output for WorkUnit " + workUnitState.getId() + " branch " + branchId
-                + " matching glob: " + writerOutputFullGlob);
-          } else {
-            writerOutputFilePath = status.getPath();
-          }
-        }
-        if (writerOutputFilePath == null) {
-          LOG.error("Unable to find output for WorkUnit " + workUnitState.getId() + " branch " + branchId
-              + "; expected to find file matching glob: " + writerOutputFullGlob);
-          continue;
-        }
+        boolean replaceFinalOutputDir = this.getState().getPropAsBoolean(ForkOperatorUtils
+            .getPropertyNameForBranch(ConfigurationKeys.DATA_PUBLISHER_REPLACE_FINAL_DIR, this.numBranches, branchId));
 
         // The directory where the final output directory for this job will be placed. It is a combination of
         // DATA_PUBLISHER_FINAL_DIR and WRITER_FILE_PATH
         Path publisherOutputDir = WriterUtils.getDataPublisherFinalDir(workUnitState, this.numBranches, branchId);
 
-        String writerOutputPathStr = writerOutputFilePath.getParent().toString();
-
-        // The suffix at the end of the writer output path, e.g. 2015/06/29/10
-        String pathSuffix = writerOutputPathStr.substring(
-            writerOutputPathStr.indexOf(writerOutputDir.toString()) + writerOutputDir.toString().length() + 1);
-
-        int recordsWritten = workUnitState.getPropAsInt(ForkOperatorUtils
-            .getPropertyNameForBranch(ConfigurationKeys.WRITER_ROWS_WRITTEN, this.numBranches, branchId));
-
-        // Remove extension, insert number of records written, put extension back on the end
-        String outputFileName = writerOutputFileName.substring(0,
-            writerOutputFileName.length() - fileExtension.length()) + recordsWritten + "." + fileExtension;
-
-        Path outputPath = new Path(publisherOutputDir, pathSuffix + Path.SEPARATOR + outputFileName);
-
-        boolean replaceFinalOutputDir = this.getState().getPropAsBoolean(ForkOperatorUtils
-            .getPropertyNameForBranch(ConfigurationKeys.DATA_PUBLISHER_REPLACE_FINAL_DIR, this.numBranches, branchId));
-
-        if (!publisherDirsProcessed.contains(publisherOutputDir)
-            && replaceFinalOutputDir
+        if (!publisherDirsProcessed.contains(publisherOutputDir) && replaceFinalOutputDir
             && this.fss.get(branchId).exists(publisherOutputDir)) {
           // If the final output directory is configured to be replaced, and we haven't yet
           // processed (deleted) it, and it exists, then delete it to begin replacing it
@@ -126,12 +84,23 @@ public class TimePartitionedDataWithRowCountsPublisher extends TimePartitionedDa
         }
         publisherDirsProcessed.add(publisherOutputDir);
 
-        this.fss.get(branchId).mkdirs(outputPath.getParent());
-        // Move from writer dir to publisher dir, rename to include row count
-        if (this.fss.get(branchId).rename(writerOutputFilePath, outputPath)) {
-          LOG.info(String.format("Moved %s to %s", writerOutputFilePath, outputPath));
-        } else {
-          throw new IOException("Failed to move from " + writerOutputFilePath + " to " + outputPath);
+        Path writerOutFullGlob = getWriterOutputGlobMatcher(workUnitState, branchId, writerOutputDir);
+
+        for (FileStatus writerOutFileStatus : this.fss.get(branchId).globStatus(writerOutFullGlob)) {
+
+          Path writerOutFilePath = writerOutFileStatus.getPath();
+
+          int recordsWritten = workUnitState.getPropAsInt(ForkOperatorUtils
+              .getPropertyNameForBranch(ConfigurationKeys.WRITER_ROWS_WRITTEN, this.numBranches, branchId));
+          Path outputPath = getFinalOutputPath(writerOutputDir, publisherOutputDir, writerOutFilePath, recordsWritten);
+
+          this.fss.get(branchId).mkdirs(outputPath.getParent());
+          // Move from writer dir to publisher dir, rename to include row count
+          if (this.fss.get(branchId).rename(writerOutFilePath, outputPath)) {
+            LOG.info(String.format("Moved %s to %s", writerOutFilePath, outputPath));
+          } else {
+            throw new IOException("Failed to move from " + writerOutFilePath + " to " + outputPath);
+          }
         }
       }
 
@@ -142,12 +111,67 @@ public class TimePartitionedDataWithRowCountsPublisher extends TimePartitionedDa
     }
   }
 
-  private Path getWriterOutputGlobMatcher(WorkUnitState workUnitState, int branchId, Path writerOutputDir,
-      String writerOutputFileName) {
+  // TODO this is close but... right now still assuming there was only one file outputted since I
+  // only have one row count to work off of
+
+  /**
+   * Get the final output path to move writerOutFilePath to. Takes the directory structure that the
+   * original file had within writerOutputDir and replicates this within publisherOutputDir. Also
+   * changes the file name, inserting .{rowCount} before the file extension. For example, if
+   * writerOutputDir is /data/staging, publisherOutputDir is /data/output, writerOutFilePath
+   * is /data/staging/2015/06/29/10/taskId42.avro, and recordsWritten is 2048, returns:
+   *
+   * "/data/output/2015/06/29/10/taskId42.2048.avro" as a Path object
+   *
+   * @param writerOutputDir The path where the writer outputs its files.
+   * @param publisherOutputDir The path where the publisher is to output its files to.
+   * @param writerOutFilePath The full path to the file written out by the writer.
+   * @param recordsWritten How many records were written to this file
+   * @return Final output Path
+   */
+  private Path getFinalOutputPath(Path writerOutputDir, Path publisherOutputDir, Path writerOutFilePath,
+      int recordsWritten) {
+
+    String fileName = writerOutFilePath.getName();
+    String writerOutputPathStr = writerOutFilePath.getParent().toString();
+
+    // The suffix at the end of the writer output path, e.g. 2015/06/29/10
+    String pathSuffix = writerOutputPathStr.substring(
+        writerOutputPathStr.indexOf(writerOutputDir.toString()) + writerOutputDir.toString().length() + 1);
+
+    // Remove extension, insert number of records written, put extension back on the end
+    String outputFileName = fileName.substring(0, fileName.lastIndexOf(".")) + "."
+            + recordsWritten + fileName.substring(fileName.lastIndexOf("."));
+
+    return new Path(publisherOutputDir, pathSuffix + Path.SEPARATOR + outputFileName);
+  }
+
+  /**
+   * Generates a glob Path to find files matching the writer output. Looks in writerOutputDir for
+   * files matching the file name which would be generated by the corresponding writer, using
+   * WRITER_PARTITION_LEVEL and WRITER_PARTITION_PATTERN to determine the correct directory structure.
+   *
+   * For example, if writerOutputDir is /data/Events, the writer output file name is taskId42.avro,
+   * the partition level is hourly, and the partition pattern is yyyy/MM/dd/hh, this returns:
+   *
+   * "/data/Events/hourly/* /* /* /* /taskId42.avro" as a Path object
+   *
+   * @return A Path which can be used with FileSystem.globStatus to find all files within
+   *         writerOutputDir which match the correct writer output file name
+   */
+  private Path getWriterOutputGlobMatcher(WorkUnitState workUnitState, int branchId, Path writerOutputDir) {
+
+    String fileExtension = WriterOutputFormat.valueOf(workUnitState.getProp(ForkOperatorUtils
+            .getPropertyNameForBranch(ConfigurationKeys.WRITER_OUTPUT_FORMAT_KEY, this.numBranches, branchId),
+        WriterOutputFormat.AVRO.name())).getExtension();
+
+    String writerOutputFileName = WriterUtils
+        .getWriterFileName(workUnitState, this.numBranches, branchId, workUnitState.getId(), fileExtension);
 
     String partitionLevel = workUnitState.getProp(ForkOperatorUtils
             .getPropertyNameForBranch(ConfigurationKeys.WRITER_PARTITION_LEVEL, this.numBranches, branchId),
         ConfigurationKeys.DEFAULT_WRITER_PARTITION_LEVEL);
+
     String partitionPattern = workUnitState.getProp(ForkOperatorUtils
             .getPropertyNameForBranch(ConfigurationKeys.WRITER_PARTITION_PATTERN, this.numBranches, branchId),
         ConfigurationKeys.DEFAULT_WRITER_PARTITION_PATTERN);
