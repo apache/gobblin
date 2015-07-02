b9176c440c626a486c86002df875abc3a36e7d2c
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2015-07-02 15:00:28 -0700

    Updated tests for ELBRecord class

--- gobblin-core/src/test/java/gobblin/converter/s3/ELBRecordTest.java
+++ gobblin-core/src/test/java/gobblin/converter/s3/ELBRecordTest.java
@@ -10,6 +10,13 @@
  */
 package gobblin.converter.s3;
 
+import gobblin.configuration.ConfigurationKeys;
+import gobblin.converter.DataConversionException;
+import gobblin.source.extractor.utils.InputStreamCSVReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -23,16 +30,80 @@ import java.text.SimpleDateFormat;
  */
 @Test(groups = {"gobblin.converter.s3"})
 public class ELBRecordTest {
+  private static final Logger LOG = LoggerFactory.getLogger(ELBRecordTest.class);
+
+  public static ArrayList<String> generateCsv(String recordString) {
+    InputStreamCSVReader r = new InputStreamCSVReader(recordString, '\u0020');
+
+    try {
+      return r.splitRecord();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return new ArrayList<String>();
+    }
+  }
+
+  public static ELBRecord generateELBRecord(String recordString)
+      throws DataConversionException {
+    ArrayList<String> recordArray = generateCsv(recordString);
+
+    return new ELBRecord(recordArray);
+  }
 
   /**
    * A simple use case test.
    */
   @Test
-  public void testParseRequestStringSimple() {
-    SimpleDateFormat df = new SimpleDateFormat(ELBRecord.ISO8601_DATE_FORMAT);
+  public void testParseRequestStringSimple()
+      throws DataConversionException {
+    String requestString = "\"POST http://example.url.com:80/example/path HTTP/1.1\"";
+    String elbRecordString = "2015-05-13T23:39:43.945958Z my-loadbalancer 192.168.131.39:2817 10.0.0.1:80 0.000073 0"
+        + ".001048 0.000057 200 200 0 29 " + requestString + " \"curl/7.38.0\" - -";
+    ELBRecord elbRecord = generateELBRecord(elbRecordString);
 
-    Assert.assertNotEquals(ELBRecord.parseDate("2015-06-08T21:54:25.889826Z", df), null);
-    Assert.assertNotEquals(ELBRecord.parseDate("2015-06-10T13:55:44.504862Z", df), null);
-    Assert.assertNotEquals(ELBRecord.parseDate("2015-10-08T01:08:04.997527Z", df), null);
+    Assert.assertEquals(elbRecord.requestMethod, "POST");
+    Assert.assertEquals(elbRecord.requestProtocol, "http");
+    Assert.assertEquals(elbRecord.requestHostHeader, "example.url.com");
+    Assert.assertEquals(elbRecord.requestPort, 80);
+    Assert.assertEquals(elbRecord.requestPath, "example/path");
+    Assert.assertEquals(elbRecord.requestHttpVersion, "HTTP/1.1");
+  }
+
+  /**
+   * Tests if a path ending in a file ending (.html for example) works
+   */
+  @Test
+  public void testParseRequestStringFile()
+      throws DataConversionException {
+    String requestString = "\"POST http://example.url.com:80/example/path.html HTTP/1.1\"";
+    String elbRecordString = "2015-05-13T23:39:43.945958Z my-loadbalancer 192.168.131.39:2817 10.0.0.1:80 0.000073 0"
+        + ".001048 0.000057 200 200 0 29 " + requestString + " \"curl/7.38.0\" - -";
+    ELBRecord elbRecord = generateELBRecord(elbRecordString);
+
+    Assert.assertEquals(elbRecord.requestMethod, "POST");
+    Assert.assertEquals(elbRecord.requestProtocol, "http");
+    Assert.assertEquals(elbRecord.requestHostHeader, "example.url.com");
+    Assert.assertEquals(elbRecord.requestPort, 80);
+    Assert.assertEquals(elbRecord.requestPath, "example/path.html");
+    Assert.assertEquals(elbRecord.requestHttpVersion, "HTTP/1.1");
+  }
+
+  /**
+   * Tests if the TCP input for the request string ("- - -") fails
+   */
+  @Test
+  public void testParseRequestStringTcpIn()
+      throws DataConversionException {
+    String requestString = "\"- - -\"";
+    String elbRecordString = "2015-05-13T23:39:43.945958Z my-loadbalancer 192.168.131.39:2817 10.0.0.1:80 0.000073 0"
+        + ".001048 0.000057 200 200 0 29 " + requestString + " \"curl/7.38.0\" - -";
+    ELBRecord elbRecord = generateELBRecord(elbRecordString);
+
+    Assert.assertNull(elbRecord.requestMethod);
+    Assert.assertNull(elbRecord.requestProtocol);
+    Assert.assertNull(elbRecord.requestHostHeader);
+    Assert.assertEquals(elbRecord.requestPort, 0);
+    Assert.assertNull(elbRecord.requestPath);
+    Assert.assertNull(elbRecord.requestHttpVersion);
   }
 }
