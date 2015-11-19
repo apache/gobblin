b4ec4c514caf092dfa15765858d08d9c61b4d79d
ziliu <ziliu@linkedin.com>
2015-11-19 11:57:14 -0800

    Add schema registry to KafkaAvroExtractor

--- gobblin-core/src/main/java/gobblin/source/extractor/extract/kafka/KafkaAvroExtractor.java
+++ gobblin-core/src/main/java/gobblin/source/extractor/extract/kafka/KafkaAvroExtractor.java
@@ -15,6 +15,7 @@ package gobblin.source.extractor.extract.kafka;
 import java.io.IOException;
 
 import kafka.message.MessageAndOffset;
+import lombok.extern.slf4j.Slf4j;
 
 import org.apache.avro.Schema;
 import org.apache.avro.SchemaBuilder;
@@ -22,12 +23,13 @@ import org.apache.avro.generic.GenericData.Record;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.Decoder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 
 import gobblin.configuration.WorkUnitState;
+import gobblin.metrics.kafka.KafkaSchemaRegistry;
+import gobblin.metrics.kafka.SchemaRegistryException;
 import gobblin.source.extractor.DataRecordException;
 import gobblin.source.extractor.Extractor;
 import gobblin.util.AvroUtils;
@@ -36,26 +38,52 @@ import gobblin.util.AvroUtils;
 /**
  * An abstract implementation of {@link Extractor} for Kafka, where events are in Avro format.
  *
+ * Subclasses should implement {@link #getRecordSchema(byte[])} and {@link #getDecoder(byte[])}. Additionally, if
+ * schema registry is not used (i.e., property {@link KafkaSchemaRegistry#KAFKA_SCHEMA_REGISTRY_CLASS} is not
+ * specified, method {@link #getExtractorSchema()} should be overriden.
+ *
  * @author ziliu
  */
-public abstract class KafkaAvroExtractor extends KafkaExtractor<Schema, GenericRecord> {
+@Slf4j
+public abstract class KafkaAvroExtractor<K> extends KafkaExtractor<Schema, GenericRecord> {
 
-  private static final Logger LOG = LoggerFactory.getLogger(KafkaAvroExtractor.class);
   private static final Schema DEFAULT_SCHEMA = SchemaBuilder.record("DefaultSchema").fields().name("header")
       .type(SchemaBuilder.record("header").fields().name("time").type("long").withDefault(0).endRecord()).noDefault()
       .endRecord();
 
+  private final Optional<KafkaSchemaRegistry<K, Schema>> schemaRegistry;
   private final Optional<Schema> schema;
   private final Optional<GenericDatumReader<Record>> reader;
 
   public KafkaAvroExtractor(WorkUnitState state) {
     super(state);
+    this.schemaRegistry = state.contains(KafkaSchemaRegistry.KAFKA_SCHEMA_REGISTRY_CLASS)
+        ? Optional.of(KafkaSchemaRegistry.<K, Schema> get(state.getProperties()))
+        : Optional.<KafkaSchemaRegistry<K, Schema>> absent();
     this.schema = getExtractorSchema();
     if (this.schema.isPresent()) {
       this.reader = Optional.of(new GenericDatumReader<Record>(this.schema.get()));
     } else {
-      LOG.error(String.format("Cannot find latest schema for topic %s. This topic will be skipped", this.topicName));
+      log.error(String.format("Cannot find latest schema for topic %s. This topic will be skipped", this.topicName));
       this.reader = Optional.absent();
+    }
+  }
+
+  /**
+   * Get the schema to be used by this extractor. All extracted records that have different schemas
+   * will be converted to this schema.
+   */
+  protected Optional<Schema> getExtractorSchema() {
+    return Optional.fromNullable(getLatestSchemaByTopic());
+  }
+
+  private Schema getLatestSchemaByTopic() {
+    Preconditions.checkState(this.schemaRegistry.isPresent());
+    try {
+      return this.schemaRegistry.get().getLatestSchemaByTopic(this.topicName);
+    } catch (SchemaRegistryException e) {
+      log.error(String.format("Cannot find latest schema for topic %s. This topic will be skipped", this.topicName), e);
+      return null;
     }
   }
 
@@ -83,24 +111,18 @@ public abstract class KafkaAvroExtractor extends KafkaExtractor<Schema, GenericR
       record = AvroUtils.convertRecordSchema(record, this.schema.get());
       return record;
     } catch (IOException e) {
-      LOG.error(String.format("Error during decoding record for partition %s: ", this.getCurrentPartition()));
+      log.error(String.format("Error during decoding record for partition %s: ", this.getCurrentPartition()));
       throw e;
     }
   }
 
   /**
-   * Get the schema to be used by this extractor. All extracted records that have different schemas
-   * will be converted to this schema.
-   */
-  protected abstract Optional<Schema> getExtractorSchema();
-
-  /**
-   * Obtain the {@link Schema} of a Kafka record given the payload of the record.
+   * Obtain the Avro {@link Schema} of a Kafka record given the payload of the record.
    */
   protected abstract Schema getRecordSchema(byte[] payload);
 
   /**
-   * Obtain the {@link Decoder} for a Kafka record given the payload of the record.
+   * Obtain the Avro {@link Decoder} for a Kafka record given the payload of the record.
    */
   protected abstract Decoder getDecoder(byte[] payload);
 }
