81318ce900f0b59628bad6a264fa1aab0ff216f8
Prashant Bhardwaj <prashant.bhardwaj@applift.com>
2016-01-06 15:45:08 +0530

    Added log line to catch null

--- gobblin-example/src/main/java/gobblin/applift/simpleconsumer/TimeBasedReqLogWriterPartitioner.java
+++ gobblin-example/src/main/java/gobblin/applift/simpleconsumer/TimeBasedReqLogWriterPartitioner.java
@@ -1,5 +1,8 @@
 package gobblin.applift.simpleconsumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -8,6 +11,7 @@ import gobblin.configuration.State;
 import gobblin.writer.partitioner.TimeBasedWriterPartitioner;
 
 public class TimeBasedReqLogWriterPartitioner extends TimeBasedWriterPartitioner<String> {
+	private static final Logger LOG = LoggerFactory.getLogger(TimeBasedReqLogWriterPartitioner.class);
 	
 	public TimeBasedReqLogWriterPartitioner(State state, int numBranches, int branchId) {
 		super(state, numBranches, branchId);
@@ -18,6 +22,8 @@ public class TimeBasedReqLogWriterPartitioner extends TimeBasedWriterPartitioner
 		JsonElement element = new JsonParser().parse(record);
 		JsonObject reqLogObject = element.getAsJsonObject();
 		JsonObject reqInfoObject = reqLogObject.getAsJsonObject("req_info");
+		if(reqInfoObject.get("unix_ts").toString() != null)
+			LOG.warn("Applift: FaultyRecord = "+record);
 		float unixTS = Float.valueOf(reqInfoObject.get("unix_ts").toString());
 		long timestampMS = (long) (unixTS*1000);
 		return timestampMS;
