2a926dd997a9fa5709bcc06f97762ab051e43f90
Joel Baranick <joel.baranick@ensighten.com>
2016-01-07 16:33:58 -0800

    Fixed import ordering

--- gobblin-core/src/main/java/gobblin/publisher/BaseDataPublisher.java
+++ gobblin-core/src/main/java/gobblin/publisher/BaseDataPublisher.java
@@ -19,8 +19,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Sets;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -29,7 +27,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.google.common.io.Closer;
 
 import gobblin.configuration.State;
