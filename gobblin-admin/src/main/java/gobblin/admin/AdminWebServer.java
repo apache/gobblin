69d565a89896abf8af0cf1183ec87d8116ea5cdf
Eric Ogren <eric@nerdwallet.com>
2016-01-09 15:07:21 -0800

    Add Apache license, clean up a few unused imports

--- gobblin-admin/src/main/java/gobblin/admin/AdminWebServer.java
+++ gobblin-admin/src/main/java/gobblin/admin/AdminWebServer.java
@@ -1,7 +1,16 @@
+/* (c) 2015 NerdWallet All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+* this file except in compliance with the License. You may obtain a copy of the
+* License at  http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed
+* under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+* CONDITIONS OF ANY KIND, either express or implied.
+*/
 package gobblin.admin;
 
 import com.google.common.base.Preconditions;
-import com.google.common.util.concurrent.AbstractExecutionThreadService;
 import com.google.common.util.concurrent.AbstractIdleService;
 import gobblin.configuration.ConfigurationKeys;
 import org.eclipse.jetty.server.Handler;
@@ -21,7 +30,7 @@ import java.net.URI;
 import java.util.Properties;
 
 /**
- * Serves the admin UI interface
+ * Serves the admin UI interface using embedded Jetty.
  */
 public class AdminWebServer extends AbstractIdleService {
     private static final Logger LOGGER = LoggerFactory.getLogger(AdminWebServer.class);
--- gobblin-admin/src/test/java/gobblin/admin/AdminWebServerTest.java
+++ gobblin-admin/src/test/java/gobblin/admin/AdminWebServerTest.java
@@ -1,7 +1,16 @@
+/* (c) 2015 NerdWallet All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+* this file except in compliance with the License. You may obtain a copy of the
+* License at  http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed
+* under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+* CONDITIONS OF ANY KIND, either express or implied.
+*/
 package gobblin.admin;
 
 import gobblin.configuration.ConfigurationKeys;
-import org.apache.commons.httpclient.HttpClient;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
