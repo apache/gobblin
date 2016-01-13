118e17e45525b24f5dc9dc118f2c0da0b765cab5
Min Tu <mitu@linkedin.com>
2016-01-13 10:31:07 -0800

    caching layer for ConfigStore.getConfig()

---
+++ gobblin-config-management/gobblin-config-core/src/main/java/gobblin/config/common/impl/ConfigStoreBackedValueInspector.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2015 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package gobblin.config.common.impl;
+
+import java.util.List;
+
+import com.typesafe.config.Config;
+
+import gobblin.config.store.api.ConfigKeyPath;
+import gobblin.config.store.api.ConfigStore;
+import gobblin.config.store.api.ConfigStoreWithResolution;
+
+/**
+ * ConfigStoreBackedValueInspector always query the underline {@ConfigStore} to get the freshest 
+ * {@com.typesafe.config.Config}
+ * @author mitu
+ *
+ */
+public class ConfigStoreBackedValueInspector implements ConfigStoreValueInspector{
+
+  private final ConfigStore cs;
+  private final String version;
+  private final ConfigStoreTopologyInspector topology;
+
+  /**
+   * @param cs       - internal {@ConfigStore} to retrieve configuration 
+   * @param version  - version of the {@ConfigStore}
+   * @param topology - corresponding {@ConfigStoreTopologyInspector} for the input {@ConfigStore}
+   */
+  public ConfigStoreBackedValueInspector(ConfigStore cs, String version, ConfigStoreTopologyInspector topology) {
+    this.cs = cs;
+    this.version = version;
+    this.topology = topology;
+  }
+
+  public ConfigStore getConfigStore() {
+    return this.cs;
+  }
+
+  public String getVersion() {
+    return this.version;
+  }
+  
+  /**
+   * {@inheritDoc}.
+   *
+   * <p>
+   *   This implementation simply delegate the functionality to the internal {@ConfigStore}/version
+   * </p>
+   */
+  @Override
+  public Config getOwnConfig(ConfigKeyPath configKey) {
+    return this.cs.getOwnConfig(configKey, version);
+  }
+
+  /**
+   * {@inheritDoc}.
+   *
+   * <p>
+   *   This implementation simply delegate the functionality to the internal {@ConfigStore}/version if
+   *   the internal {@ConfigStore} is {@ConfigStoreWithResolution}, otherwise based on {@ConfigStoreTopologyInspector}
+   *   
+   *   1. find out all the imports recursively
+   *   2. resolved the config on the fly
+   * </p>
+   */
+  @Override
+  public Config getResolvedConfig(ConfigKeyPath configKey) {
+    if (this.cs instanceof ConfigStoreWithResolution) {
+      return ((ConfigStoreWithResolution) this.cs).getResolvedConfig(configKey, this.version);
+    }
+
+    List<ConfigKeyPath> recursiveImports = this.topology.getImportsRecursively(configKey);
+    Config initialConfig = this.getOwnConfig(configKey);
+    
+    // merge with other configs from imports
+    for(ConfigKeyPath p: recursiveImports){
+      initialConfig.withFallback(this.getConfigInSelfChain(p));
+    }
+    
+    // merge with configs from parent
+    initialConfig.withFallback(this.getConfigInSelfChain(configKey.getParent()));
+    
+    return initialConfig;
+  }
+  
+  // return resolved config from self -> parent .. -> root
+  private Config getConfigInSelfChain(ConfigKeyPath configKey){
+    Config result = this.getOwnConfig(configKey);
+    if(configKey.isRootPath())
+      return result;
+    
+    ConfigKeyPath parent = configKey.getParent();
+    while(!parent.isRootPath()){
+      result.withFallback(this.getOwnConfig(parent));
+      parent = parent.getParent();
+    }
+    
+    // parent is root now, need to merge the config for root
+    result.withFallback(this.getOwnConfig(parent));
+    
+    return result;
+  }
+}
---
+++ gobblin-config-management/gobblin-config-core/src/main/java/gobblin/config/common/impl/InMemoryValueInspector.java
@@ -0,0 +1,87 @@
+package gobblin.config.common.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import com.typesafe.config.Config;
+
+import gobblin.config.store.api.ConfigKeyPath;
+
+/**
+ * InMemoryValueInspector provide the caching layer for getting the {@com.typesafe.config.Config} from {@ConfigStore}
+ * 
+ * @author mitu
+ *
+ */
+public class InMemoryValueInspector implements ConfigStoreValueInspector{
+
+  private final ConfigStoreValueInspector valueFallback;
+
+  private final Map<ConfigKeyPath, Config> ownConfigMap;
+  private final Map<ConfigKeyPath, Config> recursiveConfigMap;
+
+  /**
+   * 
+   * @param valueFallback - the fall back {@ConfigStoreValueInspector} which used to get the raw {@com.typesafe.config.Config} 
+   * @param useStrongRef  - if true, use Strong reference in cache, else, use Weak reference in cache
+   */
+  public InMemoryValueInspector (ConfigStoreValueInspector valueFallback, boolean useStrongRef){
+    this.valueFallback = valueFallback;
+
+    if(useStrongRef){
+      this.ownConfigMap = new HashMap<>();
+      this.recursiveConfigMap = new HashMap<>();
+    }
+    else{
+      this.ownConfigMap = new WeakHashMap<>();
+      this.recursiveConfigMap = new WeakHashMap<>();
+    }
+  }
+
+  /**
+   * {@inheritDoc}.
+   *
+   * <p>
+   *   If present in the cache, return the cached {@com.typesafe.config.Config} for given input
+   *   Otherwise, simply delegate the functionality to the internal {ConfigStoreValueInspector} and store the value into cache
+   * </p>
+   */
+  @Override
+  public Config getOwnConfig(ConfigKeyPath configKey) {
+    if(this.ownConfigMap.containsKey(configKey)){
+      Config result = this.ownConfigMap.get(configKey);
+      // in case of the GC happened when containsKey() and get() function
+      if(result!=null){
+        return result;
+      }
+    }
+
+    Config ownConfig = this.valueFallback.getOwnConfig(configKey);
+    this.ownConfigMap.put(configKey, ownConfig);
+    return ownConfig;
+  }
+
+  /**
+   * {@inheritDoc}.
+   *
+   * <p>
+   *   If present in the cache, return the cached {@com.typesafe.config.Config} for given input
+   *   Otherwise, simply delegate the functionality to the internal {ConfigStoreValueInspector} and store the value into cache
+   * </p>
+   */
+  @Override
+  public Config getResolvedConfig(ConfigKeyPath configKey) {
+    if(this.recursiveConfigMap.containsKey(configKey)){
+      Config result = this.recursiveConfigMap.get(configKey);
+      // in case of the GC happened when containsKey() and get() function
+      if(result!=null){
+        return result;
+      }
+    }
+
+    Config recursiveConfig = this.valueFallback.getResolvedConfig(configKey);
+    this.recursiveConfigMap.put(configKey, recursiveConfig);
+    return recursiveConfig;
+  }
+}
