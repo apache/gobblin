85a20a4e345974fc4c30be1a31909c9d863b63d2
Lorand Bendig <Lorand.Bendig@swisscom.com>
2016-01-25 10:37:22 +0100

    Client side logging in MR mode

--- bin/gobblin-mapreduce.sh
+++ bin/gobblin-mapreduce.sh
@@ -16,6 +16,7 @@ function print_usage(){
   echo "  --fs <file system URL>                         Target file system: if not set, taken from \${HADOOP_HOME}/conf"
   echo "  --jars <comma-separated list of job jars>      Job jar(s): if not set, \"$FWDIR_LIB\" is examined"
   echo "  --workdir <job work dir>                       Gobblin's base work directory: if not set, taken from \${GOBBLIN_WORK_DIR}"
+  echo "  --logdir <log dir>                             Gobblin's log directory: if not set, taken from \${GOBBLIN_LOG_DIR}"
   echo "  --help                                         Display this help and exit"
 }
 
@@ -37,12 +38,16 @@ do
       FS_URL="$2"
       shift
       ;;
+    --jars)
+      JARS="$2"
+      shift
+      ;;
     --workdir)
       WORK_DIR="$2"
       shift
       ;;
-    --jars)
-      JARS="$2"
+    --logdir)
+      GOBBLIN_LOG_DIR="$2"
       shift
       ;;
     --conf)
@@ -70,6 +75,15 @@ fi
 
 if [ -z "$GOBBLIN_WORK_DIR" ]; then
   die "GOBBLIN_WORK_DIR is not set!"
+fi
+
+# User defined log directory overrides $GOBBLIN_LOG_DIR
+if [ -n "$LOG_DIR" ]; then
+  export GOBBLIN_LOG_DIR="$LOG_DIR"
+fi
+
+if [ -z "$GOBBLIN_LOG_DIR" ]; then
+  GOBBLIN_LOG_DIR="$FWDIR/logs"
 fi
 
 . $FWDIR_BIN/gobblin-env.sh
@@ -102,7 +116,7 @@ set_user_jars "$JARS"
 LIBJARS=$USER_JARS$separator$FWDIR_LIB/gobblin-metastore.jar,$FWDIR_LIB/gobblin-metrics.jar,\
 $FWDIR_LIB/gobblin-core.jar,$FWDIR_LIB/gobblin-api.jar,$FWDIR_LIB/gobblin-utility.jar,\
 $FWDIR_LIB/guava-15.0.jar,$FWDIR_LIB/avro-1.7.7.jar,$FWDIR_LIB/metrics-core-3.1.0.jar,\
-$FWDIR_LIB/gson-2.3.1.jar,$FWDIR_LIB/joda-time-2.9.jar,$FWDIR_LIB/data-1.15.9.jar
+$FWDIR_LIB/gson-2.3.1.jar,$FWDIR_LIB/joda-time-2.9.jar,$FWDIR_LIB/data-1.15.9.jar,$FWDIR_LIB/commons-lang3-3.4.jar
 
 # Add libraries to the Hadoop classpath
 GOBBLIN_DEP_JARS=`echo "$USER_JARS" | tr ',' ':' `
@@ -120,12 +134,15 @@ GOBBLIN_CONFIG_FILE=$FWDIR_CONF/gobblin-mapreduce.properties
 JT_COMMAND=$([ -z $JOB_TRACKER_URL ] && echo "" || echo "-jt $JOB_TRACKER_URL")
 FS_COMMAND=$([ -z $FS_URL ] && echo "" || echo "-fs $FS_URL")
 
+export HADOOP_CLIENT_OPTS="$HADOOP_CLIENT_OPTS -Dgobblin.logs.dir=$GOBBLIN_LOG_DIR"
+
 # Launch the job to run on Hadoop
 $HADOOP_BIN_DIR/hadoop jar \
         $FWDIR_LIB/gobblin-runtime.jar \
         gobblin.runtime.mapreduce.CliMRJobLauncher \
         -D mapreduce.user.classpath.first=true \
         -D mapreduce.job.user.classpath.first=true \
+        -D gobblin.log4j.conf=$FWDIR_CONF/log4j-mapreduce.xml \
         $JT_COMMAND \
         $FS_COMMAND \
         -libjars $LIBJARS \
