75dbe01b52709f00720e08ff00410817cd2da354
Chavdar Botev <cbotev@linkedin.com>
2016-05-02 19:34:24 -0700

    Make the default store for SimpleHDFSConfigStoreFactory configurable

--- gobblin-config-management/gobblin-config-core/src/test/java/gobblin/config/store/hdfs/SimpleHdfsConfigureStoreFactoryTest.java
+++ gobblin-config-management/gobblin-config-core/src/test/java/gobblin/config/store/hdfs/SimpleHdfsConfigureStoreFactoryTest.java
@@ -7,9 +7,12 @@ import java.net.URISyntaxException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigValueFactory;
 
 import gobblin.config.store.api.ConfigStoreCreationException;
 
@@ -42,6 +45,49 @@ public class SimpleHdfsConfigureStoreFactoryTest {
       if (fs != null && fs.exists(configStoreDir)) {
         fs.delete(configStoreDir, true);
       }
+    }
+  }
+
+
+  @Test
+  public void testConfiguration() throws Exception {
+    FileSystem localFS = FileSystem.getLocal(new Configuration());
+    Path testRoot = localFS.makeQualified(new Path("dir1"));
+    Path configRoot = localFS.makeQualified(new Path(testRoot, "dir2"));
+    Path configStoreRoot = new Path(configRoot,
+                                    SimpleHDFSConfigStore.CONFIG_STORE_NAME);
+    Assert.assertTrue(localFS.mkdirs(configStoreRoot));
+    try {
+      Config confConf1 =
+          ConfigFactory.empty().withValue(SimpleHDFSConfigStoreFactory.DEFAULT_STORE_URI_KEY,
+                                          ConfigValueFactory.fromAnyRef(configRoot.toString()));
+      SimpleHDFSConfigStoreFactory confFactory = new SimpleHDFSConfigStoreFactory(confConf1);
+      Assert.assertTrue(confFactory.hasDefaultStoreURI());
+      Assert.assertEquals(confFactory.getDefaultStoreURI(), configRoot.toUri());
+      Assert.assertEquals(confFactory.getPhysicalScheme(), "file");
+
+      // Valid path
+      SimpleHDFSConfigStore store1 = confFactory.createConfigStore(new URI("simple-file:/d"));
+      Assert.assertEquals(store1.getStoreURI().getScheme(), confFactory.getScheme());
+      Assert.assertEquals(store1.getStoreURI().getAuthority(),
+                          confFactory.getDefaultStoreURI().getAuthority());
+      Assert.assertEquals(store1.getStoreURI().getPath(),
+                          confFactory.getDefaultStoreURI().getPath());
+
+      // Invalid path
+      Config confConf2 =
+          ConfigFactory.empty().withValue(SimpleHDFSConfigStoreFactory.DEFAULT_STORE_URI_KEY,
+                                          ConfigValueFactory.fromAnyRef(testRoot.toString()));
+      try {
+        new SimpleHDFSConfigStoreFactory(confConf2);
+        Assert.fail("Exception expected");
+      }
+      catch (IllegalArgumentException e) {
+        Assert.assertTrue(e.getMessage().contains("Path does not appear to be a config store root"));
+      }
+    }
+    finally {
+      localFS.delete(testRoot, true);
     }
   }
 }
