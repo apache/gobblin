41690e2e581dd82f74b1e829b6f1095d51d68f9f
Abhishek Tiwari <abhishektiwari.btech@gmail.com>
2016-06-25 01:01:19 -0700

    Added no-op AWSAppSecurityManager as placeholder class

---
+++ gobblin-aws/src/main/java/gobblin/aws/AWSAppSecurityManager.java
@@ -0,0 +1,9 @@
+package gobblin.aws;
+
+/**
+ * A class for obtaining access and secret from config to launch resources.
+ *
+ * @author Abhishek Tiwari
+ */
+public class AWSAppSecurityManager {
+}
