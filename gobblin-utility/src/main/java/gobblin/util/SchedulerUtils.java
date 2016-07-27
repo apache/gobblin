f7bbb95771865e4869a378bbbe43502970a3608a
autumnust <360322606@qq.com>
2016-07-27 14:25:05 -0700

    add two template

--- gobblin-utility/src/main/java/gobblin/util/SchedulerUtils.java
+++ gobblin-utility/src/main/java/gobblin/util/SchedulerUtils.java
@@ -141,8 +141,10 @@ public class SchedulerUtils {
     jobProps.putAll(ConfigurationConverter.getProperties(
         new PropertiesConfiguration(new Path("file://", jobConfigPath).toUri().toURL())));
 
-    jobProps = (new SimpleGeneralJobTemplate(
-        jobProps.getProperty(ConfigurationKeys.JOB_TEMPLATE_PATH))).getResolvedConfigAsProperties(jobProps);
+    if (jobProps.containsKey(ConfigurationKeys.JOB_TEMPLATE_PATH)) {
+      jobProps = (new SimpleGeneralJobTemplate(
+          jobProps.getProperty(ConfigurationKeys.JOB_TEMPLATE_PATH))).getResolvedConfigAsProperties(jobProps);
+    }
 
     jobProps.setProperty(ConfigurationKeys.JOB_CONFIG_FILE_PATH_KEY, jobConfigPath.toString());
     return jobProps;
@@ -232,8 +234,10 @@ public class SchedulerUtils {
             propertiesConfiguration.load(inputStreamReader);
             jobProps.putAll(ConfigurationConverter.getProperties(propertiesConfiguration));
 
-            jobProps = (new SimpleGeneralJobTemplate(
-                jobProps.getProperty(ConfigurationKeys.JOB_TEMPLATE_PATH))).getResolvedConfigAsProperties(jobProps);
+            if (jobProps.containsKey(ConfigurationKeys.JOB_TEMPLATE_PATH)) {
+              jobProps = (new SimpleGeneralJobTemplate(
+                  jobProps.getProperty(ConfigurationKeys.JOB_TEMPLATE_PATH))).getResolvedConfigAsProperties(jobProps);
+            }
 
             jobProps.setProperty(ConfigurationKeys.JOB_CONFIG_FILE_PATH_KEY, configFilePath.toString());
             jobConfigs.add(jobProps);
