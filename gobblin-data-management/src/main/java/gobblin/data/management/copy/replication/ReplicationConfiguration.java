8b2da031f86ad16053f43e28635abe07d76c6f69
Min Tu <mitu@linkedin.com>
2016-09-26 17:17:19 -0700

    Configuration related to EndPoints

---
+++ gobblin-data-management/src/main/java/gobblin/data/management/copy/replication/ReplicationConfiguration.java
@@ -0,0 +1,20 @@
+package gobblin.data.management.copy.replication;
+
+public class ReplicationConfiguration {
+  public static final String REPLICATION_COPY_MODE = "copymode";
+  public static final String METADATA = "metadata";
+  public static final String METADATA_JIRA = "jira";
+  public static final String METADATA_OWNER = "owner";
+  public static final String METADATA_NAME = "name";
+
+  public static final String REPLICATION_SOURCE = "source";
+  public static final String REPLICATION_REPLICAS = "replicas";
+  public static final String REPLICATOIN_REPLICAS_LIST = "list";
+
+  public static final String DATA_FLOW_TOPOLOGY = "dataFlowTopology";
+
+  public static final String DEFAULT_DATA_FLOW_TOPOLOGIES = "defaultDataFlowTopologies";
+
+  public static final String ROUTES_PICKER_TYPE = "routePickerType";
+
+}
