414bea63628506752e24c076313b7f38692e8717
Min Tu <mitu@linkedin.com>
2016-09-30 10:51:28 -0700

    removed java8 imports

--- gobblin-data-management/src/main/java/gobblin/data/management/copy/replication/ReplicationConfiguration.java
+++ gobblin-data-management/src/main/java/gobblin/data/management/copy/replication/ReplicationConfiguration.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.google.common.base.Function;
 import com.google.common.base.Optional;
