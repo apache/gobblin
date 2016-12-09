922ded65aaf72f72b847bb1dd966541479c5531f
Shirshanka Das <sdas@linkedin.com>
2016-12-08 18:18:35 -0800

    Fix install script

--- gobblin-modules/gobblin-couchbase/scripts/install_test_deps.sh
+++ gobblin-modules/gobblin-couchbase/scripts/install_test_deps.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 mkdir -p mock-couchbase
-git -C mock-couchbase pull || git clone https://github.com/couchbase/CouchbaseMock.git mock-couchbase
+if [ -d mock-couchbase/.git ];
+  then
+    git -C mock-couchbase pull
+  else
+    git clone https://github.com/couchbase/CouchbaseMock.git mock-couchbase
+  fi
 pushd mock-couchbase
 mvn install
 popd
