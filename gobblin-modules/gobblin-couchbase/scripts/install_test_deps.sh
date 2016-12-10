c17358af4700d0ced9c905e6bcb1cf1e3abff270
Shirshanka Das <sdas@linkedin.com>
2016-12-09 19:06:19 -0800

    Locking mock couchbase git version

--- gobblin-modules/gobblin-couchbase/scripts/install_test_deps.sh
+++ gobblin-modules/gobblin-couchbase/scripts/install_test_deps.sh
@@ -4,8 +4,10 @@ mkdir -p mock-couchbase
 if [ -d mock-couchbase/.git ];
   then
     git -C mock-couchbase pull
+    git -C mock-couchbase checkout b929a9f99a31a233c43bd94b6a696ad966206e02
   else
     git clone https://github.com/couchbase/CouchbaseMock.git mock-couchbase
+    git -C mock-couchbase checkout b929a9f99a31a233c43bd94b6a696ad966206e02
   fi
 pushd mock-couchbase
 mvn package -Dmaven.test.skip=true
