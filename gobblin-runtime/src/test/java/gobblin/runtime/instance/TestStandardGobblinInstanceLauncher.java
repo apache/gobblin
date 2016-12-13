279def58d91234e4bcb1e786d37f17f32831291a
ibuenros <issac.buenrostro@gmail.com>
2016-12-13 11:50:39 -0800

    Default gobblin instance job launcher correctly uses templates.

--- gobblin-runtime/src/test/java/gobblin/runtime/instance/TestStandardGobblinInstanceLauncher.java
+++ gobblin-runtime/src/test/java/gobblin/runtime/instance/TestStandardGobblinInstanceLauncher.java
@@ -11,6 +11,7 @@
  */
 package gobblin.runtime.instance;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutionException;
@@ -21,6 +22,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.google.common.base.Function;
+import com.google.common.collect.ImmutableMap;
 import com.typesafe.config.ConfigFactory;
 
 import gobblin.runtime.api.GobblinInstanceDriver;
@@ -30,6 +32,7 @@ import gobblin.runtime.api.JobExecutionResult;
 import gobblin.runtime.api.JobLifecycleListener;
 import gobblin.runtime.api.JobSpec;
 import gobblin.runtime.instance.DefaultGobblinInstanceDriverImpl.JobSpecRunnable;
+import gobblin.runtime.job_spec.ResolvedJobSpec;
 import gobblin.runtime.std.DefaultJobLifecycleListenerImpl;
 import gobblin.runtime.std.FilteredJobLifecycleListener;
 import gobblin.runtime.std.JobSpecFilter;
@@ -211,6 +214,64 @@ public class TestStandardGobblinInstanceLauncher {
     instanceLauncher.awaitTerminated(5, TimeUnit.SECONDS);
   }
 
+  @Test
+  public void testSubmitWithTemplate() throws Exception {
+    StandardGobblinInstanceLauncher.Builder instanceLauncherBuilder =
+        StandardGobblinInstanceLauncher.builder()
+            .withInstanceName("testSubmitWithTemplate");
+    instanceLauncherBuilder.driver();
+    StandardGobblinInstanceLauncher instanceLauncher =
+        instanceLauncherBuilder.build();
+    instanceLauncher.startAsync();
+    instanceLauncher.awaitRunning(5, TimeUnit.SECONDS);
+
+    JobSpec js1 = JobSpec.builder()
+        .withConfig(ConfigFactory.parseMap(ImmutableMap.of("numHellos", "5")))
+        .withTemplate(new URI("resource:///gobblin/runtime/instance/SimpleHelloWorldJob.template"))
+        .build();
+
+    ResolvedJobSpec js1Resolved = new ResolvedJobSpec(js1);
+    final String eventBusId = js1Resolved.getConfig().getString(GobblinTestEventBusWriter.FULL_EVENTBUSID_KEY);
+    TestingEventBusAsserter asserter = new TestingEventBusAsserter(eventBusId);
+
+    final StandardGobblinInstanceDriver instance =
+        (StandardGobblinInstanceDriver)instanceLauncher.getDriver();
+
+    final ArrayBlockingQueue<JobExecutionDriver> jobDrivers = new ArrayBlockingQueue<>(1);
+
+    JobLifecycleListener js1Listener = new FilteredJobLifecycleListener(
+        JobSpecFilter.eqJobSpecURI(js1.getUri()),
+        new DefaultJobLifecycleListenerImpl(instance.getLog()) {
+          @Override public void onJobLaunch(JobExecutionDriver jobDriver) {
+            super.onJobLaunch(jobDriver);
+            try {
+              jobDrivers.offer(jobDriver, 5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+              instance.getLog().error("Offer interrupted.");
+            }
+          }
+        });
+    instance.registerWeakJobLifecycleListener(js1Listener);
+
+    instance.getMutableJobCatalog().put(js1);
+
+    JobExecutionDriver jobDriver = jobDrivers.poll(10, TimeUnit.SECONDS);
+    Assert.assertNotNull(jobDriver);
+    JobExecutionResult jobResult = jobDriver.get(5, TimeUnit.SECONDS);
+
+    Assert.assertTrue(jobResult.isSuccessful());
+    instanceLauncher.stopAsync();
+
+    final int numHellos = js1Resolved.getConfig().getInt(HelloWorldSource.NUM_HELLOS_FULL_KEY);
+    ArrayList<String> expectedEvents = new ArrayList<>();
+    for (int i = 1; i <= numHellos; ++i) {
+      expectedEvents.add(HelloWorldSource.ExtractorImpl.helloMessage(i));
+    }
+    asserter.assertNextValuesEq(expectedEvents);
+    asserter.close();
+
+    instanceLauncher.awaitTerminated(5, TimeUnit.SECONDS);
+  }
 
 
 }
