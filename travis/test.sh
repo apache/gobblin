ddb9c42c6b7164abbfbace767c8bfb4ca6c9cf1c
Chavdar Botev <cbotev@cbotev-ld.(none)>
2017-01-10 09:40:44 -0800

    More test

--- travis/test.sh
+++ travis/test.sh
@@ -30,7 +30,7 @@ RUN_TEST_GROUP=${RUN_TEST_GROUP:-default}
 script_dir=$(dirname $0)
 echo "Old GRADLE_OPTS=$GRADLE_OPTS"
 
-export GOBBLIN_GRADLE_OPTS="-Dorg.gradle.daemon=false -Dgobblin.metastore.testing.embeddedMysqlEnabled=false"
+export GOBBLIN_GRADLE_OPTS="-Dorg.gradle.daemon=false -Dgobblin.metastore.testing.embeddedMysqlEnabled=false -ProotProject.usePreinstalledMysql=true"
 
 TEST_SCRIPT=${script_dir}/test-${RUN_TEST_GROUP}.sh
 if [ -x $TEST_SCRIPT ] ; then
