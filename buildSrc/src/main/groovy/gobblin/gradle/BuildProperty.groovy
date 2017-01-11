26bc28ea5376691ee8654a91ec0b3758bc3132cf
Chavdar Botev <cbotev@linkedin.com>
2017-01-10 19:09:37 -0800

    Adding java8 build support; build.gradle clean up

---
+++ buildSrc/src/main/groovy/gobblin/gradle/BuildProperty.groovy
@@ -0,0 +1,18 @@
+package gobblin.gradle;
+
+public class BuildProperty {
+    private final String HELP_FORMAT = "\t%-20s - %s. Default: %s";
+    public final String name;
+    public final Object defaultValue;
+    public final String description;
+
+    public BuildProperty(String name, Object defaultValue, String description) {
+        this.name = name;
+        this.defaultValue = defaultValue;
+        this.description = description;
+    }
+
+    public String getHelp() {
+        return String.format(HELP_FORMAT, this.name, this.description, this.defaultValue)
+    }
+}
