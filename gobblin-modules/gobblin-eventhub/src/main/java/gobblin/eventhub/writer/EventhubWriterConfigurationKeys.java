2129501d33d03cafe44f6a3b05820c86deb1871e
Kuai Yu <yukuai518@gmail.com>
2017-01-26 18:34:17 -0800

    increase eventhub default batch size and make configuration to public (#1584)
    * increase eventhub default batch size and make configuration keys to public
    
    * Update EventhubWriterConfigurationKeys.java

--- gobblin-modules/gobblin-eventhub/src/main/java/gobblin/eventhub/writer/EventhubWriterConfigurationKeys.java
+++ gobblin-modules/gobblin-eventhub/src/main/java/gobblin/eventhub/writer/EventhubWriterConfigurationKeys.java
@@ -9,17 +9,17 @@ public class EventhubWriterConfigurationKeys {
 
     /** Writer specific configuration keys go here **/
 
-  static final String COMMIT_TIMEOUT_MILLIS_CONFIG = "writer.eventhub.commitTimeoutMillis";
-  static final long   COMMIT_TIMEOUT_MILLIS_DEFAULT = 60000; // 1 minute
-  static final String COMMIT_STEP_WAIT_TIME_CONFIG = "writer.eventhub.commitStepWaitTimeMillis";
-  static final long   COMMIT_STEP_WAIT_TIME_DEFAULT = 500; // 500ms
-  static final String FAILURE_ALLOWANCE_PCT_CONFIG = "writer.eventhub.failureAllowancePercentage";
-  static final double FAILURE_ALLOWANCE_PCT_DEFAULT = 20.0;
+  public static final String COMMIT_TIMEOUT_MILLIS_CONFIG = "writer.eventhub.commitTimeoutMillis";
+  public static final long   COMMIT_TIMEOUT_MILLIS_DEFAULT = 60000; // 1 minute
+  public static final String COMMIT_STEP_WAIT_TIME_CONFIG = "writer.eventhub.commitStepWaitTimeMillis";
+  public static final long   COMMIT_STEP_WAIT_TIME_DEFAULT = 500; // 500ms
+  public static final String FAILURE_ALLOWANCE_PCT_CONFIG = "writer.eventhub.failureAllowancePercentage";
+  public static final double FAILURE_ALLOWANCE_PCT_DEFAULT = 20.0;
 
-  static final String BATCH_TTL = "writer.eventhub.batch.ttl";
-  static final long   BATCH_TTL_DEFAULT = 3000; // 3 seconds
-  static final String BATCH_SIZE = "writer.eventhub.batch.size";
-  static final long   BATCH_SIZE_DEFAULT = 128 * 1024; // 128KB
+  public static final String BATCH_TTL = "writer.eventhub.batch.ttl";
+  public static final long   BATCH_TTL_DEFAULT = 3000; // 3 seconds
+  public static final String BATCH_SIZE = "writer.eventhub.batch.size";
+  public static final long   BATCH_SIZE_DEFAULT = 256 * 1024; // 256KB
 
   public final static String  EVH_NAMESPACE = "eventhub.namespace";
   public final static String  EVH_HUBNAME = "eventhub.hubname";
