e0f973dc5fa15004eb7677dc380a5f9f93bd670d
Kuai Yu <yukuai518@gmail.com>
2017-02-06 14:26:30 -0800

    Ignore single Json record if size is larger than 256KB which is size limit for eventhub (#1600)
    * Ignore single record larger than 256KB for eventhub batch
    
    * resolve comments

--- gobblin-modules/gobblin-eventhub/src/test/java/gobblin/eventhub/writer/EventhubBatchTest.java
+++ gobblin-modules/gobblin-eventhub/src/test/java/gobblin/eventhub/writer/EventhubBatchTest.java
@@ -13,41 +13,43 @@ public class EventhubBatchTest {
   @Test
   public void testBatchWithLargeRecord() throws IOException {
     // Assume memory size has only 2 bytes
-    EventhubBatch batch = new EventhubBatch(2, 3000);
+    EventhubBatch batch = new EventhubBatch(8, 3000);
 
     JsonObject obj = new JsonObject();
     obj.addProperty("id", 1); // size is 8 bytes
 
-    // Although record is larger than the memory size limit, the first append still succeed
-    Assert.assertNotNull(batch.tryAppend(obj, WriteCallback.EMPTY));
+    // Record is larger than the memory size limit, the first append should fail
+    Assert.assertNull(batch.tryAppend(obj, WriteCallback.EMPTY));
 
-    // The second append should fail
+    // The second append should still fail
     Assert.assertNull(batch.tryAppend(obj, WriteCallback.EMPTY));
   }
 
   @Test
   public void testBatch() throws IOException {
-    // Assume memory size has only 64 bytes
-    EventhubBatch batch = new EventhubBatch(64, 3000);
+    // Assume memory size has only 200 bytes
+    EventhubBatch batch = new EventhubBatch(200, 3000);
 
-    JsonObject obj = new JsonObject();
-    obj.addProperty("id", 1); // size is 8 bytes
+    JsonObject record = new JsonObject();
+
+    // single record has 8 bytes, but overhead has 15 bytes, total size is 23 bytes
+    record.addProperty("id", 1);
 
     // Add 8 records into batch
-    Assert.assertNotNull(batch.tryAppend(obj, WriteCallback.EMPTY));
-    Assert.assertNotNull(batch.tryAppend(obj, WriteCallback.EMPTY));
-    Assert.assertNotNull(batch.tryAppend(obj, WriteCallback.EMPTY));
-    Assert.assertNotNull(batch.tryAppend(obj, WriteCallback.EMPTY));
-    Assert.assertNotNull(batch.tryAppend(obj, WriteCallback.EMPTY));
-    Assert.assertNotNull(batch.tryAppend(obj, WriteCallback.EMPTY));
-    Assert.assertNotNull(batch.tryAppend(obj, WriteCallback.EMPTY));
+    Assert.assertNotNull(batch.tryAppend(record, WriteCallback.EMPTY));
+    Assert.assertNotNull(batch.tryAppend(record, WriteCallback.EMPTY));
+    Assert.assertNotNull(batch.tryAppend(record, WriteCallback.EMPTY));
+    Assert.assertNotNull(batch.tryAppend(record, WriteCallback.EMPTY));
+    Assert.assertNotNull(batch.tryAppend(record, WriteCallback.EMPTY));
+    Assert.assertNotNull(batch.tryAppend(record, WriteCallback.EMPTY));
+    Assert.assertNotNull(batch.tryAppend(record, WriteCallback.EMPTY));
 
     // Batch has room for 8th record
-    Assert.assertEquals(batch.hasRoom(obj), true);
-    Assert.assertNotNull(batch.tryAppend(obj, WriteCallback.EMPTY));
+    Assert.assertEquals(batch.hasRoom(record), true);
+    Assert.assertNotNull(batch.tryAppend(record, WriteCallback.EMPTY));
 
     // Batch has no room for 9th record
-    Assert.assertEquals(batch.hasRoom(obj), false);
-    Assert.assertNull(batch.tryAppend(obj, WriteCallback.EMPTY));
+    Assert.assertEquals(batch.hasRoom(record), false);
+    Assert.assertNull(batch.tryAppend(record, WriteCallback.EMPTY));
   }
 }
