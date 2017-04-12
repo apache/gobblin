1792e105ca3f0b070f326fc9afe91585fb583278
Jin Hyuk Chang <jnchang@linkedin.com>
2017-04-12 11:12:03 -0700

    Added support for Azure Data Lake(ADL) as a source (#1764)

--- gobblin-api/src/main/java/gobblin/configuration/ConfigurationKeys.java
+++ gobblin-api/src/main/java/gobblin/configuration/ConfigurationKeys.java
@@ -451,6 +451,8 @@ public class ConfigurationKeys {
   public static final String SOURCE_FILEBASED_FS_URI = "source.filebased.fs.uri";
   public static final String SOURCE_FILEBASED_PRESERVE_FILE_NAME = "source.filebased.preserve.file.name";
   public static final String SOURCE_FILEBASED_OPTIONAL_DOWNLOADER_CLASS = "source.filebased.downloader.class";
+  public static final String SOURCE_FILEBASED_ENCRYPTED_CONFIG_PATH = "source.filebased.encrypted";
+
   public static final String SOURCE_FILEBASED_FS_PRIOR_SNAPSHOT_REQUIRED = "source.filebased.fs.prior.snapshot.required";
   public static final boolean DEFAULT_SOURCE_FILEBASED_FS_PRIOR_SNAPSHOT_REQUIRED = false;
 
