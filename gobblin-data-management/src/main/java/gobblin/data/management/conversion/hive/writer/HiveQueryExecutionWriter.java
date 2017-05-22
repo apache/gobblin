fbbf515b0c2817791efd87d39213da891442c819
Aditya Sharma <aditya11051993@gmail.com>
2017-05-22 10:24:33 +0530

    Preserving partition params during avro2orc conversion (#1827)
    * Preserving partition params during avro2orc conversion
    
    * Adding CopyPartitionParametersTest in gobblin-data-management

--- gobblin-data-management/src/main/java/gobblin/data/management/conversion/hive/writer/HiveQueryExecutionWriter.java
+++ gobblin-data-management/src/main/java/gobblin/data/management/conversion/hive/writer/HiveQueryExecutionWriter.java
@@ -18,13 +18,21 @@ package gobblin.data.management.conversion.hive.writer;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.base.Optional;
 
 import lombok.AllArgsConstructor;
 
 import gobblin.configuration.State;
+import gobblin.data.management.conversion.hive.dataset.ConvertibleHiveDataset;
 import gobblin.data.management.conversion.hive.entities.QueryBasedHiveConversionEntity;
+import gobblin.data.management.conversion.hive.entities.SchemaAwareHivePartition;
 import gobblin.data.management.conversion.hive.events.EventWorkunitUtils;
+import gobblin.data.management.conversion.hive.publisher.HiveConvertPublisher;
 import gobblin.util.HiveJdbcConnector;
 import gobblin.writer.DataWriter;
 import lombok.extern.slf4j.Slf4j;
@@ -40,6 +48,7 @@ public class HiveQueryExecutionWriter implements DataWriter<QueryBasedHiveConver
 
   private final HiveJdbcConnector hiveJdbcConnector;
   private final State workUnit;
+  private static final String AT_CHAR = "@";
 
   @Override
   public void write(QueryBasedHiveConversionEntity hiveConversionEntity) throws IOException {
@@ -48,6 +57,8 @@ public class HiveQueryExecutionWriter implements DataWriter<QueryBasedHiveConver
       conversionQueries = hiveConversionEntity.getQueries();
       EventWorkunitUtils.setBeginConversionDDLExecuteTimeMetadata(this.workUnit, System.currentTimeMillis());
       this.hiveJdbcConnector.executeStatements(conversionQueries.toArray(new String[conversionQueries.size()]));
+      // Adding properties for preserving partitionParams:
+      addPropsForPublisher(hiveConversionEntity);
       EventWorkunitUtils.setEndConversionDDLExecuteTimeMetadata(this.workUnit, System.currentTimeMillis());
     } catch (SQLException e) {
       log.warn("Failed to execute queries: ");
@@ -55,6 +66,38 @@ public class HiveQueryExecutionWriter implements DataWriter<QueryBasedHiveConver
         log.warn("Conversion query attempted by Hive Query writer: " + conversionQuery);
       }
       throw new IOException(e);
+    }
+  }
+
+  /**
+   * Method to add properties needed by publisher to preserve partition params
+   */
+  private void addPropsForPublisher(QueryBasedHiveConversionEntity hiveConversionEntity) {
+    if (!hiveConversionEntity.getHivePartition().isPresent()) {
+      return;
+    }
+    ConvertibleHiveDataset convertibleHiveDataset = hiveConversionEntity.getConvertibleHiveDataset();
+    for (String format : convertibleHiveDataset.getDestFormats()) {
+      Optional<ConvertibleHiveDataset.ConversionConfig> conversionConfigForFormat =
+          convertibleHiveDataset.getConversionConfigForFormat(format);
+      if (!conversionConfigForFormat.isPresent()) {
+        continue;
+      }
+      SchemaAwareHivePartition sourcePartition = hiveConversionEntity.getHivePartition().get();
+
+      // Get complete source partition name dbName@tableName@partitionName
+      String completeSourcePartitionName = StringUtils.join(Arrays
+          .asList(sourcePartition.getTable().getDbName(), sourcePartition.getTable().getTableName(),
+              sourcePartition.getName()), AT_CHAR);
+      ConvertibleHiveDataset.ConversionConfig config = conversionConfigForFormat.get();
+
+      // Get complete destination partition name dbName@tableName@partitionName
+      String completeDestPartitionName = StringUtils.join(
+          Arrays.asList(config.getDestinationDbName(), config.getDestinationTableName(), sourcePartition.getName()),
+          AT_CHAR);
+
+      workUnit.setProp(HiveConvertPublisher.COMPLETE_SOURCE_PARTITION_NAME, completeSourcePartitionName);
+      workUnit.setProp(HiveConvertPublisher.COMPLETE_DEST_PARTITION_NAME, completeDestPartitionName);
     }
   }
 
