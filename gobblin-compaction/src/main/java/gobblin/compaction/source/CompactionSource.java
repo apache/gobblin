92ba8646609ac432351bbbc6044078e1631fc518
Kuai Yu <kuyu@linkedin.com>
2017-05-23 21:23:06 -0700

    Add workunit stream to CompactionSource

--- gobblin-compaction/src/main/java/gobblin/compaction/source/CompactionSource.java
+++ gobblin-compaction/src/main/java/gobblin/compaction/source/CompactionSource.java
@@ -2,6 +2,7 @@ package gobblin.compaction.source;
 
 import com.google.common.collect.Lists;
 
+import gobblin.annotation.Alias;
 import gobblin.compaction.mapreduce.MRCompactor;
 import gobblin.compaction.suite.CompactionSuiteUtils;
 import gobblin.data.management.dataset.DatasetUtils;
@@ -18,8 +19,11 @@ import gobblin.dataset.DatasetsFinder;
 import gobblin.runtime.JobState;
 import gobblin.runtime.task.TaskUtils;
 import gobblin.source.Source;
+import gobblin.source.WorkUnitStreamSource;
 import gobblin.source.extractor.Extractor;
+import gobblin.source.workunit.BasicWorkUnitStream;
 import gobblin.source.workunit.WorkUnit;
+import gobblin.source.workunit.WorkUnitStream;
 import gobblin.util.HadoopUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.conf.Configuration;
@@ -27,11 +31,10 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
-
 import java.io.IOException;
 import java.net.URI;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.*;
 
 /**
  * A compaction source derived from {@link Source} which uses {@link DefaultFileSystemGlobFinder} to find all
@@ -39,49 +42,132 @@ import java.util.UUID;
  * all verification. Each found dataset will be serialized to {@link WorkUnit} by {@link CompactionSuite#save(Dataset, State)}
  */
 @Slf4j
-public class CompactionSource implements Source<String, String> {
+public class CompactionSource implements WorkUnitStreamSource<String, String> {
   private CompactionSuite suite;
   private Path tmpJobDir;
   private FileSystem fs;
+  private ExecutorService service;
 
+  @Override
   public List<WorkUnit> getWorkunits(SourceState state) {
-    List<WorkUnit> workUnits = Lists.newArrayList();
+    throw new UnsupportedOperationException("Please use getWorkunitStream");
+  }
+
+  @Override
+  public WorkUnitStream getWorkunitStream(SourceState state) {
+    service = Executors.newSingleThreadExecutor();
 
     try {
       fs = getSourceFileSystem(state);
-      suite = CompactionSuiteUtils.getCompactionSuiteFactory (state).createSuite(state);
+      suite = CompactionSuiteUtils.getCompactionSuiteFactory(state).createSuite(state);
 
-      initJobDir (state);
-      copyJarDependencies (state);
-
-      DatasetsFinder finder  = DatasetUtils.instantiateDatasetFinder(state.getProperties(),
-               getSourceFileSystem(state),
-               DefaultFileSystemGlobFinder.class.getName());
-
+      initJobDir(state);
+      copyJarDependencies(state);
       List<CompactionVerifier> verifiers = suite.getDatasetsFinderVerifiers();
+      DatasetsFinder finder = DatasetUtils.instantiateDatasetFinder(state.getProperties(),
+              getSourceFileSystem(state),
+              DefaultFileSystemGlobFinder.class.getName());
+
       List<Dataset> datasets = finder.findDatasets();
-
-
-      for (Dataset dataset: datasets) {
-        // all verifier should be passed before we compact the dataset
-        boolean verificationPassed = true;
-        for (CompactionVerifier verifier : verifiers) {
-          if (!verifier.verify(dataset)) {
-            verificationPassed = false;
-            break;
-          }
-        }
-
-        if (verificationPassed) {
-          WorkUnit workUnit = createWorkUnit(dataset);
-          workUnits.add(workUnit);
-        }
-      }
-    } catch (Exception e) {
+      CompactionDatasetIterator iterator = new CompactionDatasetIterator(datasets, verifiers);
+      service.submit(iterator.getDatasetProcessor());
+      return new BasicWorkUnitStream.Builder (iterator).build();
+    } catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
 
-    return workUnits;
+
+  /**
+   * Iterator that provides {@link WorkUnit}s for all verified {@link Dataset}s
+   */
+  class CompactionDatasetIterator implements Iterator<WorkUnit> {
+    private LinkedBlockingDeque<WorkUnit> workUnits;
+
+    private List<Dataset> datasets;
+    private List<CompactionVerifier> verifiers;
+    private WorkUnit last;
+    private volatile int unProcessed;
+    private volatile int success;
+
+    /**
+     * Constructor
+     */
+    public CompactionDatasetIterator (List<Dataset> datasets, List<CompactionVerifier> verifiers) {
+      this.datasets = datasets;
+      this.workUnits = new LinkedBlockingDeque<>();
+      this.verifiers = verifiers;
+      this.unProcessed = datasets.size();
+      this.success = 0;
+      this.last = null;
+    }
+
+    /**
+     * Check if any {@link WorkUnit} is available. The producer is {@link CompactionDatasetIterator#getDatasetProcessor()}
+     * @return true when a new {@link WorkUnit}  is available
+     *         false when producer exits
+     */
+    public boolean hasNext () {
+      try {
+        while (true) {
+          if (last != null) return true;
+          if (this.unProcessed <= 0 && this.workUnits.isEmpty()) return false;
+          this.last = this.workUnits.poll(1, TimeUnit.SECONDS);
+        }
+      } catch (InterruptedException e) {
+        log.error(e.toString());
+        return false;
+      }
+    }
+
+    /**
+     * Obtain next available {@link WorkUnit}.
+     * Block the consumer thread until a new {@link WorkUnit} is provided. Otherwise throw an exception
+     */
+    public WorkUnit next () {
+      if (hasNext()) {
+        if (last != null) {
+          WorkUnit tmp = last;
+          last = null;
+          return tmp;
+        } else {
+          throw new IllegalStateException("last variable cannot be empty");
+        }
+      }
+
+      throw new NoSuchElementException("work units queue has been exhausted");
+    }
+
+    public void remove() {
+      throw new UnsupportedOperationException("No remove supported on " + this.getClass().getName());
+    }
+
+    public Callable<Integer> getDatasetProcessor () {
+      return new Callable<Integer>() {
+        @Override
+        public Integer call() throws Exception {
+          for (Dataset dataset: datasets) {
+            // all verifier should be passed before we compact the dataset
+            boolean verificationPassed = true;
+            if (verifiers != null) {
+              for (CompactionVerifier verifier : verifiers) {
+                if (!verifier.verify(dataset)) {
+                  verificationPassed = false;
+                  break;
+                }
+              }
+            }
+            if (verificationPassed) {
+              success++;
+              workUnits.add (createWorkUnit(dataset));
+            }
+            unProcessed--;
+          }
+
+          return success;
+        }
+      };
+    }
   }
 
   protected WorkUnit createWorkUnit (Dataset dataset) throws IOException {
@@ -101,6 +187,7 @@ public class CompactionSource implements Source<String, String> {
     try {
       boolean f = fs.delete(this.tmpJobDir, true);
       log.info("Job dir is removed from {} with status {}", this.tmpJobDir, f);
+      service.shutdown();
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
@@ -159,4 +246,5 @@ public class CompactionSource implements Source<String, String> {
       }
     }
   }
+
 }
