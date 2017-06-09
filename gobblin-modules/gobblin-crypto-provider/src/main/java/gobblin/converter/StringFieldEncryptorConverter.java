ad79af3bddf411afb08f995d5e700ff1748a1f72
Eric Ogren <eogren@gmail.com>
2017-06-09 08:25:00 -0700

    - Add one more level of config for encryption - previously converters (#1934)
    only looked converter.encrypt.*, but this breaks down if there are
    multiple converters in a chain that are doing encryption. This change
    allows you to specify config at converter.<ConverterName>.encrypt.*

--- gobblin-modules/gobblin-crypto-provider/src/main/java/gobblin/converter/StringFieldEncryptorConverter.java
+++ gobblin-modules/gobblin-crypto-provider/src/main/java/gobblin/converter/StringFieldEncryptorConverter.java
@@ -49,7 +49,7 @@ public abstract class StringFieldEncryptorConverter<SCHEMA, DATA> extends Conver
   public Converter<SCHEMA, SCHEMA, DATA, DATA> init(WorkUnitState workUnit) {
     super.init(workUnit);
     Map<String, Object> config =
-        EncryptionConfigParser.getConfigForBranch(EncryptionConfigParser.EntityType.CONVERTER, workUnit);
+        EncryptionConfigParser.getConfigForBranch(EncryptionConfigParser.EntityType.CONVERTER, getClass().getSimpleName(), workUnit);
     encryptor = EncryptionFactory.buildStreamCryptoProvider(config);
 
     String fieldsToEncryptConfig = workUnit.getProp(FIELDS_TO_ENCRYPT_CONFIG_NAME, null);
