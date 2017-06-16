04d92a8594ebe7a6ef603deda420f4c5c61ca68b
zhchen <zhchen@linkedin.com>
2017-06-16 14:32:56 -0700

    Add body information

--- gobblin-modules/gobblin-http/src/main/java/gobblin/http/ApacheHttpRequest.java
+++ gobblin-modules/gobblin-http/src/main/java/gobblin/http/ApacheHttpRequest.java
@@ -1,8 +1,11 @@
 package gobblin.http;
 
+import java.io.IOException;
 import java.util.Arrays;
 
+import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.util.EntityUtils;
 
 import gobblin.async.AsyncRequest;
 
@@ -13,7 +16,7 @@ public class ApacheHttpRequest<D> extends AsyncRequest<D, HttpUriRequest> {
     HttpUriRequest request = getRawRequest();
     StringBuilder outBuffer = new StringBuilder();
     String endl = "\n";
-    outBuffer.append("ApacheHttpRequest Info:").append(endl);
+    outBuffer.append("ApacheHttpRequest Info").append(endl);
     outBuffer.append("type: HttpUriRequest").append(endl);
     outBuffer.append("uri: ").append(request.getURI().toString()).append(endl);
     outBuffer.append("headers: ");
@@ -21,6 +24,15 @@ public class ApacheHttpRequest<D> extends AsyncRequest<D, HttpUriRequest> {
         outBuffer.append("[").append(header.getName()).append(":").append(header.getValue()).append("] ")
     );
     outBuffer.append(endl);
+
+    if (request instanceof HttpEntityEnclosingRequest) {
+      try {
+        String body = EntityUtils.toString(((HttpEntityEnclosingRequest) request).getEntity());
+        outBuffer.append("body: ").append(body).append(endl);
+      } catch (IOException e) {
+        outBuffer.append("body: ").append(e.getMessage()).append(endl);
+      }
+    }
     return outBuffer.toString();
   }
 }
--- gobblin-modules/gobblin-http/src/main/java/gobblin/r2/R2Request.java
+++ gobblin-modules/gobblin-http/src/main/java/gobblin/r2/R2Request.java
@@ -1,5 +1,8 @@
 package gobblin.r2;
 
+import java.nio.charset.Charset;
+
+import com.linkedin.data.ByteString;
 import com.linkedin.r2.message.rest.RestRequest;
 
 import gobblin.async.AsyncRequest;
@@ -11,7 +14,7 @@ public class R2Request<D> extends AsyncRequest<D, RestRequest> {
     RestRequest request = getRawRequest();
     StringBuilder outBuffer = new StringBuilder();
     String endl = "\n";
-    outBuffer.append("R2Request Info:").append(endl);
+    outBuffer.append("R2Request Info").append(endl);
     outBuffer.append("type: RestRequest").append(endl);
     outBuffer.append("uri: ").append(request.getURI().toString()).append(endl);
     outBuffer.append("headers: ");
@@ -19,6 +22,11 @@ public class R2Request<D> extends AsyncRequest<D, RestRequest> {
         outBuffer.append("[").append(k).append(":").append(v).append("] ")
     );
     outBuffer.append(endl);
+
+    ByteString entity = request.getEntity();
+    if (entity != null) {
+      outBuffer.append("body: ").append(entity.asString(Charset.defaultCharset())).append(endl);
+    }
     return outBuffer.toString();
   }
 }
