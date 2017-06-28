0c0036da8b57bcff8b2b4ecbe3e912b68f886a27
Jin Hyuk Chang <jnchang@linkedin.com>
2017-06-28 11:16:04 -0700

    Added partition and table information on HiveWorkUnit Added a capability of taking filesystem uri from job property for HiveSource

--- gobblin-data-management/src/main/java/gobblin/data/management/conversion/hive/source/HiveWorkUnit.java
+++ gobblin-data-management/src/main/java/gobblin/data/management/conversion/hive/source/HiveWorkUnit.java
@@ -16,10 +16,15 @@
  */
 package gobblin.data.management.conversion.hive.source;
 
+import java.lang.reflect.Type;
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.metastore.api.FieldSchema;
 
 import com.google.common.base.Optional;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 
 import gobblin.configuration.ConfigurationKeys;
 import gobblin.data.management.copy.hive.HiveDataset;
@@ -33,10 +38,17 @@ import gobblin.source.workunit.WorkUnit;
  */
 public class HiveWorkUnit extends WorkUnit {
 
-  private static final String HIVE_DATASET_SERIALIZED_KEY = "hive.source.dataset.serialized";
-  private static final String HIVE_TABLE_SCHEMA_URL_KEY = "hive.source.dataset.table.schemaUrl";
-  private static final String HIVE_PARTITION_SCHEMA_URL_KEY = "hive.source.dataset.partition.schemaUrl";
-  private static final String HIVE_PARTITION_NAME_KEY = "hive.source.dataset.partition.name";
+  private static final String PREFIX = "hive.source.dataset";
+  private static final String HIVE_DATASET_SERIALIZED_KEY = PREFIX + ".serialized";
+  private static final String HIVE_TABLE_SCHEMA_URL_KEY = PREFIX + ".table.schemaUrl";
+  private static final String HIVE_TABLE_LOCATION_KEY = PREFIX + ".table.location";
+  private static final String HIVE_PARTITION_SCHEMA_URL_KEY = PREFIX + ".partition.schemaUrl";
+  private static final String HIVE_PARTITION_NAME_KEY = PREFIX + ".partition.name";
+  private static final String HIVE_PARTITION_LOCATION_KEY = PREFIX + ".partition.location";
+  private static final String HIVE_PARTITION_KEYS = PREFIX + ".partition.keys";
+
+  private static final Gson GSON = new Gson();
+  private static final Type FIELD_SCHEMA_TYPE = new TypeToken<List<FieldSchema>>() {}.getType();
 
   @SuppressWarnings("deprecation")
   public HiveWorkUnit() {
@@ -122,5 +134,59 @@ public class HiveWorkUnit extends WorkUnit {
    */
   public Optional<String> getPartitionName() {
     return Optional.fromNullable(this.getProp(HIVE_PARTITION_NAME_KEY));
+  }
+
+  /**
+   * Set the name of the partition into the {@link WorkUnit}
+   * @param partitionName
+   */
+  public void setTableLocation(String partitionLocation) {
+    this.setProp(HIVE_TABLE_LOCATION_KEY, partitionLocation);
+  }
+
+  /**
+   * Get the name for the partition if this {@link WorkUnit} is for a partitioned table.
+   * If not, return {@link Optional#absent()}
+   */
+  public Optional<String> getTableLocation() {
+    return Optional.fromNullable(this.getProp(HIVE_TABLE_LOCATION_KEY));
+  }
+
+
+  /**
+   * Set the name of the partition into the {@link WorkUnit}
+   * @param partitionName
+   */
+  public void setPartitionLocation(String partitionLocation) {
+    this.setProp(HIVE_PARTITION_LOCATION_KEY, partitionLocation);
+  }
+
+  /**
+   * Get the name for the partition if this {@link WorkUnit} is for a partitioned table.
+   * If not, return {@link Optional#absent()}
+   */
+  public Optional<String> getPartitionLocation() {
+    return Optional.fromNullable(this.getProp(HIVE_PARTITION_LOCATION_KEY));
+  }
+
+  /**
+   * Set partition keys into the {@link WorkUnit}
+   * @param partitionName
+   */
+  public void setPartitionKeys(List<FieldSchema> partitionKeys) {
+    this.setProp(HIVE_PARTITION_KEYS, GSON.toJson(partitionKeys, FIELD_SCHEMA_TYPE));
+  }
+
+  /**
+   * Get the partition keys if this {@link WorkUnit} is for a partitioned table.
+   * If not, return {@link Optional#absent()}
+   */
+  public Optional<List<FieldSchema>> getPartitionKeys() {
+    String serialzed = this.getProp(HIVE_PARTITION_KEYS);
+    if (serialzed == null) {
+      return Optional.absent();
+    }
+    List<FieldSchema> deserialized = GSON.fromJson(serialzed, FIELD_SCHEMA_TYPE);
+    return Optional.of(deserialized);
   }
 }
