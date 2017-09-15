e5a78e11db954160fe3f07533e1cc5b0422fb72a
ibuenros <issac.buenrostro@gmail.com>
2017-09-15 13:56:54 -0700

    Add tests and partition awareness.

--- gobblin-data-management/src/main/java/org/apache/gobblin/data/management/conversion/hive/materializer/HiveMaterializerFromEntityQueryGenerator.java
+++ gobblin-data-management/src/main/java/org/apache/gobblin/data/management/conversion/hive/materializer/HiveMaterializerFromEntityQueryGenerator.java
@@ -166,8 +166,9 @@ public abstract class HiveMaterializerFromEntityQueryGenerator extends HiveMater
       log.debug("Staging table directory to delete: " + stagingDataLocation);
       cleanupDirectories.add(stagingDataLocation);
 
-    publishQueries.addAll(HiveAvroORCQueryGenerator.generateDropPartitionsDDL(outputDatabaseName, outputTableName,
-        AbstractAvroToOrcConverter.getDropPartitionsDDLInfo(conversionEntity)));
+      publishQueries.addAll(HiveAvroORCQueryGenerator.generateDropPartitionsDDL(outputDatabaseName, outputTableName,
+          AbstractAvroToOrcConverter.getDropPartitionsDDLInfo(conversionEntity)));
+    }
 
     log.info("Publish partition entity: " + publishEntity);
     return publishEntity;
