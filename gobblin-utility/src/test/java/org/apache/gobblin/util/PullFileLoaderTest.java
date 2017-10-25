af621357fd27b68df1789cf01299f51d81325efb
vickykak@gmail.com <vickykak@gmail.com>
2017-10-25 15:40:08 +0530

    Code Indentation as per Gobblin Coding Standard

--- gobblin-utility/src/test/java/org/apache/gobblin/util/PullFileLoaderTest.java
+++ gobblin-utility/src/test/java/org/apache/gobblin/util/PullFileLoaderTest.java
@@ -228,8 +228,6 @@ public class PullFileLoaderTest {
             .put(PullFileLoader.PROPERTY_DELIMITER_PARSING_ENABLED_KEY, true)
             .build());
     pullFile = loader.loadPullFile(path, cfg, false);
-    System.out.println(pullFile.getString("json.property.key"));
-    System.out.println(pullFile.getString("json.property.key1"));
     Assert.assertEquals(pullFile.getString("json.property.key"), pullFile.getString("json.property.key1"));
   }
   
