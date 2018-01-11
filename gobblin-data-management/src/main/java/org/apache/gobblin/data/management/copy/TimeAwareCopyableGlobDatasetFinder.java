13e6b802fc1cd8463a7684eb4d21dc3bad419023
suvasude <suvasude@linkedin.biz>
2018-01-11 09:45:45 -0800

    GOBBLIN-365: Addressing reviewer comments.

--- gobblin-data-management/src/main/java/org/apache/gobblin/data/management/copy/TimeAwareCopyableGlobDatasetFinder.java
+++ gobblin-data-management/src/main/java/org/apache/gobblin/data/management/copy/TimeAwareCopyableGlobDatasetFinder.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.gobblin.data.management.copy;
 
 import java.io.IOException;
@@ -19,6 +36,6 @@ public class TimeAwareCopyableGlobDatasetFinder extends ConfigurableGlobDatasetF
 
   @Override
   public CopyableDataset datasetAtPath(Path path) throws IOException {
-    return new TimeAwareRecursiveCopyableDataset(this.fs,path,this.props,this.datasetPattern);
+    return new TimeAwareRecursiveCopyableDataset(this.fs, path, this.props, this.datasetPattern);
   }
 }
--- gobblin-data-management/src/main/java/org/apache/gobblin/data/management/copy/TimeAwareRecursiveCopyableDataset.java
+++ gobblin-data-management/src/main/java/org/apache/gobblin/data/management/copy/TimeAwareRecursiveCopyableDataset.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.gobblin.data.management.copy;
 
 import com.google.common.collect.Lists;
@@ -12,10 +29,11 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.PathFilter;
 
+
 public class TimeAwareRecursiveCopyableDataset extends RecursiveCopyableDataset {
   private static final String CONFIG_PREFIX = CopyConfiguration.COPY_PREFIX + ".recursive";
-  public static final String DATE_PATTERN_KEY=CONFIG_PREFIX + ".date.pattern";
-  public static final String LOOKBACK_DAYS_KEY=CONFIG_PREFIX + ".lookback.days";
+  public static final String DATE_PATTERN_KEY = CONFIG_PREFIX + ".date.pattern";
+  public static final String LOOKBACK_DAYS_KEY = CONFIG_PREFIX + ".lookback.days";
 
   private final Integer lookbackDays;
   private final String datePattern;
@@ -34,7 +52,16 @@ public class TimeAwareRecursiveCopyableDataset extends RecursiveCopyableDataset 
     public DateRangeIterator(LocalDateTime startDate, LocalDateTime endDate, String datePattern) {
       this.startDate = startDate;
       this.endDate = endDate;
-      this.isDatePatternHourly = datePattern.contains("HH")? true: false;
+      this.isDatePatternHourly = isDatePatternHourly(datePattern);
+    }
+
+    private boolean isDatePatternHourly(String datePattern) {
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern(datePattern);
+      LocalDateTime refDateTime = LocalDateTime.of(2017, 01, 01, 10, 0, 0);
+      String refDateTimeString = refDateTime.format(formatter);
+      LocalDateTime refDateTimeAtStartOfDay = refDateTime.withHour(0);
+      String refDateTimeStringAtStartOfDay = refDateTimeAtStartOfDay.format(formatter);
+      return !refDateTimeString.equals(refDateTimeStringAtStartOfDay);
     }
 
     @Override
@@ -45,7 +72,7 @@ public class TimeAwareRecursiveCopyableDataset extends RecursiveCopyableDataset 
     @Override
     public LocalDateTime next() {
       LocalDateTime dateTime = startDate;
-      startDate = isDatePatternHourly? startDate.plusHours(1) : startDate.plusDays(1);
+      startDate = isDatePatternHourly ? startDate.plusHours(1) : startDate.plusDays(1);
       return dateTime;
     }
 
@@ -57,14 +84,14 @@ public class TimeAwareRecursiveCopyableDataset extends RecursiveCopyableDataset 
 
   @Override
   protected List<FileStatus> getFilesAtPath(FileSystem fs, Path path, PathFilter fileFilter) throws IOException {
-    DateTimeFormatter format = DateTimeFormatter.ofPattern(datePattern);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(datePattern);
     LocalDateTime endDate = LocalDateTime.now();
     LocalDateTime startDate = endDate.minusDays(lookbackDays);
-    DateRangeIterator dateRangeIterator = new DateRangeIterator(startDate,endDate,datePattern);
+    DateRangeIterator dateRangeIterator = new DateRangeIterator(startDate, endDate, datePattern);
     List<FileStatus> fileStatuses = Lists.newArrayList();
     while (dateRangeIterator.hasNext()) {
-      Path pathWithDateTime = new Path(path,dateRangeIterator.next().format(format));
-      fileStatuses.addAll(super.getFilesAtPath(fs,pathWithDateTime,fileFilter));
+      Path pathWithDateTime = new Path(path, dateRangeIterator.next().format(formatter));
+      fileStatuses.addAll(super.getFilesAtPath(fs, pathWithDateTime, fileFilter));
     }
     return fileStatuses;
   }
