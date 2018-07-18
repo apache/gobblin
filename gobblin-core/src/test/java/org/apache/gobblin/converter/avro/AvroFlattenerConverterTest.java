0c48748f4b9c8b3cd5aba648acfe26329f233c0b
treff7es <tamas.nemeth@prezi.com>
2018-07-18 15:52:54 +0200

    Adding missing Apache license header

--- gobblin-core/src/test/java/org/apache/gobblin/converter/avro/AvroFlattenerConverterTest.java
+++ gobblin-core/src/test/java/org/apache/gobblin/converter/avro/AvroFlattenerConverterTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.gobblin.converter.avro;
 
 import java.io.File;
@@ -8,16 +24,13 @@ import org.apache.avro.file.DataFileReader;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.commons.io.FileUtils;
-import org.testng.annotations.Test;
-
 import org.junit.Assert;
+import org.testng.annotations.Test;
 
 import org.apache.gobblin.configuration.WorkUnitState;
 import org.apache.gobblin.converter.DataConversionException;
 import org.apache.gobblin.converter.SchemaConversionException;
 import org.apache.gobblin.util.AvroUtils;
-
-import static org.testng.Assert.*;
 
 public class AvroFlattenerConverterTest {
 
