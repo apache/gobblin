d34b6454dcc95e2abd8d60ee14aeef5034694b05
Rodrigo Nicolas Garcia <rodrigo.garcia@despegar.com>
2018-11-05 20:58:18 -0300

    Fix TimeBasedORCSerDeWriterPartitioner Add TimeBasedAvroWriterPartitioner
    TimeBasedORCSerDeWriterPartitioner format like goblin said.
    Add suport ot date time strings on TimeBasedAvroWriterPartitioner.
    writer.partition.columns.pattern that contains format of column
    when you have a date_time as string that use DateTimeFormatter,
    e.g., "yyyy-MM-dd HH:mm:ss:SSS"
    writer.partition.columns.timezone that contains time zone to use
    on DateTimeFormatter default UTC,
    e.g., "GMT-3"
    
    Example of new props:
    writer.partition.columns.pattern=yyyy-MM-dd HH:mm:ss
    writer.partition.columns.timezone=UTC

--- gobblin-core/src/main/java/org/apache/gobblin/writer/partitioner/TimeBasedAvroWriterPartitioner.java
+++ gobblin-core/src/main/java/org/apache/gobblin/writer/partitioner/TimeBasedAvroWriterPartitioner.java
@@ -17,34 +17,48 @@
 
 package org.apache.gobblin.writer.partitioner;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.apache.avro.generic.GenericRecord;
-
-import com.google.common.base.Optional;
-
 import org.apache.gobblin.configuration.ConfigurationKeys;
 import org.apache.gobblin.configuration.State;
 import org.apache.gobblin.util.AvroUtils;
 import org.apache.gobblin.util.ForkOperatorUtils;
+
+import com.google.common.base.Optional;
 
 
 /**
  * A {@link TimeBasedWriterPartitioner} for {@link GenericRecord}s.
  *
  * The {@link org.apache.avro.Schema.Field} that contains the timestamp can be specified using
- * {@link WRITER_PARTITION_COLUMNS}, and multiple values can be specified, e.g., "header.timestamp,device.timestamp".
+ * {@link TimeBasedAvroWriterPartitioner#WRITER_PARTITION_COLUMNS}, and multiple values can be specified, e.g., "header.timestamp,device.timestamp".
+ * The {@link TimeBasedAvroWriterPartitioner#WRITER_PARTITION_COLUMNS_PATTERN} that contains format of column when you have a date_time as string.
+ * format use {@link DateTimeFormatter} on default UTC, e.g., "yyyy-MM-dd HH:mm:ss:SSS"
+ * The {@link TimeBasedAvroWriterPartitioner#WRITER_PARTITION_COLUMNS_TIMEZONE} that contains timezone of column to convert date_time.
  *
  * If multiple values are specified, they will be tried in order. In the above example, if a record contains a valid
  * "header.timestamp" field, its value will be used, otherwise "device.timestamp" will be used.
+ *
  *
  * If a record contains none of the specified fields, or if no field is specified, the current timestamp will be used.
  */
 public class TimeBasedAvroWriterPartitioner extends TimeBasedWriterPartitioner<GenericRecord> {
 
   public static final String WRITER_PARTITION_COLUMNS = ConfigurationKeys.WRITER_PREFIX + ".partition.columns";
+  public static final String WRITER_PARTITION_COLUMNS_PATTERN =
+      ConfigurationKeys.WRITER_PREFIX + ".partition.columns.pattern";
+  public static final String WRITER_PARTITION_COLUMNS_TIMEZONE =
+      ConfigurationKeys.WRITER_PREFIX + ".partition.columns.timezone";
+  private static final String UTC = "UTC";
 
   private final Optional<List<String>> partitionColumns;
+  private final Optional<String> partitionColumnPattern;
+  private final Optional<String> partitionColumnTimeZone;
+  private DateTimeFormatter partitionColumnDateTimeFormatter;
 
   public TimeBasedAvroWriterPartitioner(State state) {
     this(state, 1, 0);
@@ -53,11 +67,21 @@ public class TimeBasedAvroWriterPartitioner extends TimeBasedWriterPartitioner<G
   public TimeBasedAvroWriterPartitioner(State state, int numBranches, int branchId) {
     super(state, numBranches, branchId);
     this.partitionColumns = getWriterPartitionColumns(state, numBranches, branchId);
+    this.partitionColumnPattern = getPropertyAsString(WRITER_PARTITION_COLUMNS_PATTERN, state, numBranches, branchId);
+    this.partitionColumnTimeZone = getPropertyAsString(WRITER_PARTITION_COLUMNS_TIMEZONE, state, numBranches, branchId);
+    if (this.partitionColumnPattern.isPresent()) {
+      this.partitionColumnDateTimeFormatter = DateTimeFormatter.ofPattern(partitionColumnPattern.get());
+    }
   }
 
   private static Optional<List<String>> getWriterPartitionColumns(State state, int numBranches, int branchId) {
     String propName = ForkOperatorUtils.getPropertyNameForBranch(WRITER_PARTITION_COLUMNS, numBranches, branchId);
-    return state.contains(propName) ? Optional.of(state.getPropAsList(propName)) : Optional.<List<String>> absent();
+    return state.contains(propName) ? Optional.of(state.getPropAsList(propName)) : Optional.<List<String>>absent();
+  }
+
+  private static Optional<String> getPropertyAsString(String property, State state, int numBranches, int branchId) {
+    String propName = ForkOperatorUtils.getPropertyNameForBranch(property, numBranches, branchId);
+    return state.contains(propName) ? Optional.of(state.getProp(propName)) : Optional.absent();
   }
 
   @Override
@@ -68,9 +92,19 @@ public class TimeBasedAvroWriterPartitioner extends TimeBasedWriterPartitioner<G
   /**
    *  Check if the partition column value is present and is a Long object. Otherwise, use current system time.
    */
-  private static long getRecordTimestamp(Optional<Object> writerPartitionColumnValue) {
-    return writerPartitionColumnValue.orNull() instanceof Long ? (Long) writerPartitionColumnValue.get()
-        : System.currentTimeMillis();
+  private long getRecordTimestamp(Optional<Object> writerPartitionColumnValue) {
+    long ret = System.currentTimeMillis();
+    if (!this.partitionColumnPattern.isPresent()) {
+      if (writerPartitionColumnValue.orNull() instanceof Long) {
+        ret = (Long) writerPartitionColumnValue.get();
+      }
+    } else {
+      if (writerPartitionColumnValue.orNull() instanceof String) {
+        ret = LocalDateTime.parse((String) writerPartitionColumnValue.get(), partitionColumnDateTimeFormatter)
+            .atZone(ZoneId.of(partitionColumnTimeZone.or(UTC))).toInstant().toEpochMilli();
+      }
+    }
+    return ret;
   }
 
   /**
