c086ac0c5a2a41a8366afda926a9aee9d2536c15
lupin <francis.developpeur@gmail.com>
2019-01-07 23:06:24 +0100

    increase property key length in mysql

---
+++ gobblin-metastore/src/main/java/org/apache/gobblin/metastore/database/DatabaseJobHistoryStoreV103.java
@@ -0,0 +1,6 @@
+package org.apache.gobblin.metastore.database;
+
+@SupportedDatabaseVersion(isDefault = false, version = "1.0.3")
+public class DatabaseJobHistoryStoreV103 extends DatabaseJobHistoryStoreV101 implements VersionedDatabaseJobHistoryStore {
+
+}
