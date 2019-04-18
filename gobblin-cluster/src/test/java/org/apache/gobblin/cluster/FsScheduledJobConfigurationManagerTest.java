9e306a86b4e1992c12c79c7fe998b785de64ac8a
sv2000 <sudarshanv@gmail.com>
2019-04-18 14:33:11 -0700

    GOBBLIN-742: Implement a FileSystem based JobConfigurationManager.

---
+++ gobblin-cluster/src/test/java/org/apache/gobblin/cluster/FsScheduledJobConfigurationManagerTest.java
@@ -0,0 +1,159 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.gobblin.cluster;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.google.common.eventbus.EventBus;
+import com.google.common.io.Files;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigValueFactory;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.gobblin.configuration.ConfigurationKeys;
+import org.apache.gobblin.runtime.api.FsSpecConsumer;
+import org.apache.gobblin.runtime.api.JobSpec;
+import org.apache.gobblin.runtime.api.JobSpecNotFoundException;
+import org.apache.gobblin.runtime.api.MutableJobCatalog;
+import org.apache.gobblin.runtime.api.SpecExecutor;
+import org.apache.gobblin.runtime.job_catalog.NonObservingFSJobCatalog;
+import org.apache.gobblin.runtime.job_spec.AvroJobSpec;
+
+@Slf4j
+public class FsScheduledJobConfigurationManagerTest {
+  private MutableJobCatalog _jobCatalog;
+  private FsScheduledJobConfigurationManager jobConfigurationManager;
+
+  private String jobConfDir = "/tmp/" + this.getClass().getSimpleName() + "/jobCatalog";
+  private String fsSpecConsumerPathString = "/tmp/fsJobConfigManagerTest";
+  private String jobSpecUriString = "testJobSpec";
+
+  private FileSystem fs;
+
+  @BeforeClass
+  public void setUp() throws IOException {
+    this.fs = FileSystem.getLocal(new Configuration(false));
+    Path jobConfDirPath = new Path(jobConfDir);
+    if (!this.fs.exists(jobConfDirPath)) {
+      Path parentDir = jobConfDirPath.getParent();
+      if (this.fs.exists(parentDir)) {
+        this.fs.mkdirs(parentDir);
+      }
+      this.fs.mkdirs(jobConfDirPath);
+    }
+
+    EventBus eventBus = new EventBus(FsScheduledJobConfigurationManagerTest.class.getSimpleName());
+    Config config = ConfigFactory.empty()
+        .withValue(ConfigurationKeys.JOB_CONFIG_FILE_GENERAL_PATH_KEY, ConfigValueFactory.fromAnyRef(jobConfDir));
+    this._jobCatalog = new NonObservingFSJobCatalog(config);
+    ((NonObservingFSJobCatalog) this._jobCatalog).startAsync().awaitRunning();
+
+    Config jobConfigurationManagerConfig = ConfigFactory.empty()
+        .withValue(GobblinClusterConfigurationKeys.SPEC_CONSUMER_CLASS_KEY, ConfigValueFactory.fromAnyRef(FsSpecConsumer.class.getName()))
+        .withValue(FsSpecConsumer.SPEC_PATH_KEY, ConfigValueFactory.fromAnyRef(fsSpecConsumerPathString));
+    jobConfigurationManager = new FsScheduledJobConfigurationManager(eventBus, jobConfigurationManagerConfig, this._jobCatalog);
+  }
+
+  private void addJobSpec(String jobSpecName, String version, String verb) throws IOException {
+    Map<String, String> metadataMap = new HashMap<>();
+    metadataMap.put(FsSpecConsumer.VERB_KEY, verb);
+
+    AvroJobSpec jobSpec = AvroJobSpec.newBuilder().
+        setUri(Files.getNameWithoutExtension(jobSpecName)).
+        setProperties(new HashMap<>()).
+        setTemplateUri("FS:///").
+        setDescription("test").
+        setVersion(version).
+        setMetadata(metadataMap).build();
+
+    DatumWriter<AvroJobSpec> datumWriter = new SpecificDatumWriter<>(AvroJobSpec.SCHEMA$);
+    DataFileWriter<AvroJobSpec> dataFileWriter = new DataFileWriter<>(datumWriter);
+
+    Path fsSpecConsumerPath = new Path(fsSpecConsumerPathString, jobSpecName);
+    FileSystem fs = fsSpecConsumerPath.getFileSystem(new Configuration());
+    OutputStream out = fs.create(fsSpecConsumerPath);
+
+    dataFileWriter.create(AvroJobSpec.SCHEMA$, out);
+    dataFileWriter.append(jobSpec);
+    dataFileWriter.close();
+  }
+
+  @Test (expectedExceptions = {JobSpecNotFoundException.class})
+  public void testFetchJobSpecs() throws ExecutionException, InterruptedException, URISyntaxException, JobSpecNotFoundException, IOException {
+    //Test adding a JobSpec
+    String verb1 = SpecExecutor.Verb.ADD.name();
+    String version1 = "1";
+    addJobSpec(jobSpecUriString, version1, verb1);
+    this.jobConfigurationManager.fetchJobSpecs();
+    JobSpec jobSpec = this._jobCatalog.getJobSpec(new URI(jobSpecUriString));
+    Assert.assertTrue(jobSpec != null);
+    Assert.assertTrue(jobSpec.getVersion().equals(version1));
+    Assert.assertTrue(jobSpec.getUri().getPath().equals(jobSpecUriString));
+    //Ensure the JobSpec is deleted from the FsSpecConsumer path.
+    Path fsSpecConsumerPath = new Path(fsSpecConsumerPathString);
+    Assert.assertEquals(this.fs.listStatus(fsSpecConsumerPath).length, 0);
+
+    //Test that the updated JobSpec has been added to the JobCatalog.
+    String verb2 = SpecExecutor.Verb.UPDATE.name();
+    String version2 = "2";
+    addJobSpec(jobSpecUriString, version2, verb2);
+    this.jobConfigurationManager.fetchJobSpecs();
+    jobSpec = this._jobCatalog.getJobSpec(new URI(jobSpecUriString));
+    Assert.assertTrue(jobSpec != null);
+    Assert.assertTrue(jobSpec.getVersion().equals(version2));
+    //Ensure the JobSpec is deleted from the FsSpecConsumer path.
+    Assert.assertEquals(this.fs.listStatus(fsSpecConsumerPath).length, 0);
+
+    //Test that the JobSpec has been deleted from the JobCatalog.
+    String verb3 = SpecExecutor.Verb.DELETE.name();
+    addJobSpec(jobSpecUriString, version2, verb3);
+    this.jobConfigurationManager.fetchJobSpecs();
+    Assert.assertEquals(this.fs.listStatus(fsSpecConsumerPath).length, 0);
+    this._jobCatalog.getJobSpec(new URI(jobSpecUriString));
+  }
+
+  @AfterClass
+  public void tearDown() throws IOException {
+    Path fsSpecConsumerPath = new Path(fsSpecConsumerPathString);
+    if (fs.exists(fsSpecConsumerPath)) {
+      fs.delete(fsSpecConsumerPath, true);
+    }
+    Path jobCatalogPath = new Path(jobConfDir);
+    if (fs.exists(jobCatalogPath)) {
+      fs.delete(jobCatalogPath, true);
+    }
+  }
+}
---
+++ gobblin-runtime/src/main/java/org/apache/gobblin/runtime/api/FsSpecConsumer.java
@@ -0,0 +1,127 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.gobblin.runtime.api;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.mapred.FsInput;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import com.typesafe.config.Config;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.gobblin.runtime.job_spec.AvroJobSpec;
+import org.apache.gobblin.util.CompletedFuture;
+
+@Slf4j
+public class FsSpecConsumer implements SpecConsumer<Spec> {
+  public static final String SPEC_PATH_KEY = "gobblin.cluster.specConsumer.path";
+  public static final String VERB_KEY = "Verb";
+
+  private final Path specDirPath;
+  private final FileSystem fs;
+  private Map<URI, Path> specToPathMap = new HashMap<>();
+
+
+  public FsSpecConsumer(Config config) {
+    this.specDirPath = new Path(config.getString(SPEC_PATH_KEY));
+    try {
+      this.fs = this.specDirPath.getFileSystem(new Configuration());
+    } catch (IOException e) {
+      throw new RuntimeException("Unable to detect spec directory file system: " + e, e);
+    }
+  }
+
+  /** List of newly changed {@link Spec}s for execution on {@link SpecExecutor}. */
+  @Override
+  public Future<? extends List<Pair<SpecExecutor.Verb, Spec>>> changedSpecs() {
+    List<Pair<SpecExecutor.Verb, Spec>> specList = new ArrayList<>();
+    FileStatus[] fileStatuses;
+    try {
+      fileStatuses = this.fs.listStatus(this.specDirPath);
+    } catch (IOException e) {
+      log.error("Error when listing files at path: {}", this.specDirPath.toString(), e);
+      return null;
+    }
+
+    for (FileStatus fileStatus : fileStatuses) {
+      DataFileReader<AvroJobSpec> dataFileReader;
+      try {
+        dataFileReader = new DataFileReader<>(new FsInput(fileStatus.getPath(), this.fs.getConf()), new SpecificDatumReader<>());
+      } catch (IOException e) {
+        log.error("Error creating DataFileReader for: {}", fileStatus.getPath().toString(), e);
+        continue;
+      }
+
+      AvroJobSpec avroJobSpec = null;
+      while (dataFileReader.hasNext()) {
+        avroJobSpec = dataFileReader.next();
+        break;
+      }
+
+      if (avroJobSpec != null) {
+        JobSpec.Builder jobSpecBuilder = new JobSpec.Builder(avroJobSpec.getUri());
+        Properties props = new Properties();
+        props.putAll(avroJobSpec.getProperties());
+        jobSpecBuilder.withJobCatalogURI(avroJobSpec.getUri()).withVersion(avroJobSpec.getVersion())
+            .withDescription(avroJobSpec.getDescription()).withConfigAsProperties(props);
+
+        try {
+          if (!avroJobSpec.getTemplateUri().isEmpty()) {
+            jobSpecBuilder.withTemplate(new URI(avroJobSpec.getTemplateUri()));
+          }
+        } catch (URISyntaxException u) {
+          log.error("Error building a job spec: ", u);
+          continue;
+        }
+
+        String verbName = avroJobSpec.getMetadata().get(VERB_KEY);
+        SpecExecutor.Verb verb = SpecExecutor.Verb.valueOf(verbName);
+
+        JobSpec jobSpec = jobSpecBuilder.build();
+        specList.add(new ImmutablePair<SpecExecutor.Verb, Spec>(verb, jobSpec));
+        this.specToPathMap.put(jobSpec.getUri(), fileStatus.getPath());
+      }
+    }
+    return new CompletedFuture<>(specList, null);
+  }
+
+
+  @Override
+  public void commit(Spec spec) throws IOException {
+    Path path = this.specToPathMap.get(spec.getUri());
+    if (path != null) {
+      this.fs.delete(path, true);
+    }
+  }
+}
