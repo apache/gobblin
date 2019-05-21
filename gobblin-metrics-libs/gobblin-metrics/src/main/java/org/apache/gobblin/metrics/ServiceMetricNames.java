323546668f3d869893a9fe0a0d25bde9d5d9a5b3
Arjun <abora@linkedin.com>
2019-05-21 14:38:07 -0700

    add compiled metrics

--- gobblin-metrics-libs/gobblin-metrics/src/main/java/org/apache/gobblin/metrics/ServiceMetricNames.java
+++ gobblin-metrics-libs/gobblin-metrics/src/main/java/org/apache/gobblin/metrics/ServiceMetricNames.java
@@ -37,5 +37,5 @@ public class ServiceMetricNames {
   public static final String RUN_IMMEDIATELY_FLOW_METER = "RunImmediatelyFlow";
 
   public static final String RUNNING_FLOWS_COUNTER = "RunningFlows";
-  public static final String FLOWS_ALREADY_RUNNING_GAUGE = "FlowsAlreadyRunning";
+  public static final String COMPILED = "Compiled";
 }
--- gobblin-service/src/main/java/org/apache/gobblin/service/modules/orchestration/Orchestrator.java
+++ gobblin-service/src/main/java/org/apache/gobblin/service/modules/orchestration/Orchestrator.java
@@ -24,7 +24,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.commons.lang3.reflect.ConstructorUtils;
 import org.slf4j.Logger;
@@ -46,7 +45,6 @@ import org.apache.gobblin.configuration.ConfigurationKeys;
 import org.apache.gobblin.configuration.State;
 import org.apache.gobblin.instrumented.Instrumentable;
 import org.apache.gobblin.instrumented.Instrumented;
-import org.apache.gobblin.metrics.ContextAwareGauge;
 import org.apache.gobblin.metrics.MetricContext;
 import org.apache.gobblin.metrics.ServiceMetricNames;
 import org.apache.gobblin.metrics.Tag;
@@ -87,10 +85,8 @@ public class Orchestrator implements SpecCatalogListener, Instrumentable {
   protected final Optional<DagManager> dagManager;
 
   protected final MetricContext metricContext;
-  private final java.util.Optional<ContextAwareGauge> flowAlreadyRunningGauge;
 
   protected final Optional<EventSubmitter> eventSubmitter;
-  private final AtomicLong jobAlreadyRunning = new AtomicLong(0L);
   @Getter
   private Optional<Meter> flowOrchestrationSuccessFulMeter;
   @Getter
@@ -133,16 +129,12 @@ public class Orchestrator implements SpecCatalogListener, Instrumentable {
       this.flowOrchestrationFailedMeter = Optional.of(this.metricContext.meter(ServiceMetricNames.FLOW_ORCHESTRATION_FAILED_METER));
       this.flowOrchestrationTimer = Optional.of(this.metricContext.timer(ServiceMetricNames.FLOW_ORCHESTRATION_TIMER));
       this.eventSubmitter = Optional.of(new EventSubmitter.Builder(this.metricContext, "org.apache.gobblin.service").build());
-      this.flowAlreadyRunningGauge = java.util.Optional.of(metricContext.newContextAwareGauge(
-          MetricRegistry.name(MetricReportUtils.GOBBLIN_SERVICE_METRICS_PREFIX, ServiceMetricNames.FLOWS_ALREADY_RUNNING_GAUGE),
-          this.jobAlreadyRunning::get));
     } else {
       this.metricContext = null;
       this.flowOrchestrationSuccessFulMeter = Optional.absent();
       this.flowOrchestrationFailedMeter = Optional.absent();
       this.flowOrchestrationTimer = Optional.absent();
       this.eventSubmitter = Optional.absent();
-      this.flowAlreadyRunningGauge = java.util.Optional.empty();
     }
   }
 
@@ -241,10 +233,15 @@ public class Orchestrator implements SpecCatalogListener, Instrumentable {
       if (!canRun(flowName, flowGroup, allowConcurrentExecution)) {
         _log.warn("Another instance of flowGroup: {}, flowName: {} running; Skipping flow execution since "
             + "concurrent executions are disabled for this flow.", flowGroup, flowName);
-        if (this.flowAlreadyRunningGauge.isPresent()) {
-          this.jobAlreadyRunning.incrementAndGet();
-        }
+        // We send a gauge with value 0 signifying that the flow could not be compiled because previous execution is already running
+        metricContext.newContextAwareGauge(
+            MetricRegistry.name(MetricReportUtils.GOBBLIN_SERVICE_METRICS_PREFIX, flowGroup, flowName, ServiceMetricNames.COMPILED),
+            () -> 0L);
         return;
+      } else {
+        metricContext.newContextAwareGauge(
+            MetricRegistry.name(MetricReportUtils.GOBBLIN_SERVICE_METRICS_PREFIX, flowGroup, flowName, ServiceMetricNames.COMPILED),
+            () -> 1L);
       }
 
       Dag<JobExecutionPlan> jobExecutionPlanDag = specCompiler.compileFlow(spec);
