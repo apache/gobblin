eb4ccb050f380578f279ff7b22a4d2f6d07ed666
autumnust <lesun@linkedin.com>
2019-09-18 18:03:56 -0700

    Initial commit for schema evolution support among different mappers for ORC

--- gobblin-compaction/src/main/java/org/apache/gobblin/compaction/mapreduce/orc/OrcValueMapper.java
+++ gobblin-compaction/src/main/java/org/apache/gobblin/compaction/mapreduce/orc/OrcValueMapper.java
@@ -18,9 +18,17 @@
 package org.apache.gobblin.compaction.mapreduce.orc;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.gobblin.compaction.mapreduce.RecordKeyMapperBase;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.orc.OrcConf;
+import org.apache.orc.TypeDescription;
+import org.apache.orc.impl.ConvertTreeReaderFactory;
+import org.apache.orc.impl.SchemaEvolution;
 import org.apache.orc.mapred.OrcKey;
 import org.apache.orc.mapred.OrcStruct;
 import org.apache.orc.mapred.OrcValue;
@@ -38,23 +46,148 @@ public class OrcValueMapper extends RecordKeyMapperBase<NullWritable, OrcStruct,
   private OrcValue outValue;
 
   @Override
-  protected void setup(Context context) throws IOException, InterruptedException {
+  protected void setup(Context context)
+      throws IOException, InterruptedException {
     super.setup(context);
     this.outKey = new OrcKey();
     this.outValue = new OrcValue();
   }
 
   @Override
-  protected void map(NullWritable key, OrcStruct orcStruct, Context context) throws IOException, InterruptedException {
+  protected void map(NullWritable key, OrcStruct orcStruct, Context context)
+      throws IOException, InterruptedException {
+    OrcStruct upConvertedStruct = upConvertOrcStruct(orcStruct, context);
     if (context.getNumReduceTasks() == 0) {
-      this.outKey.key = orcStruct;
+      this.outKey.key = upConvertedStruct;
       context.write(this.outKey, NullWritable.get());
     } else {
-      this.outValue.value = orcStruct;
-      context.write(getDedupKey(orcStruct), this.outValue);
+      this.outValue.value = upConvertedStruct;
+      context.write(getDedupKey(upConvertedStruct), this.outValue);
     }
 
     context.getCounter(EVENT_COUNTER.RECORD_COUNT).increment(1);
+  }
+
+  /**
+   * If a {@link OrcStruct}'s schema differs from newest schema obtained when creating MR jobs (which is the
+   * newest schema seen by the MR job), all the other ORC object will need to be up-converted.
+   */
+  OrcStruct upConvertOrcStruct(OrcStruct orcStruct, Context context) {
+    TypeDescription newestSchema =
+        TypeDescription.fromString(context.getConfiguration().get(OrcConf.MAPRED_INPUT_SCHEMA.getAttribute()));
+
+    // For ORC schema, if schema object differs that means schema itself is different while for Avro,
+    // there are chances that documentation or attributes' difference lead to the schema object difference.
+    if (!orcStruct.getSchema().equals(newestSchema)) {
+      OrcStruct newStruct = new OrcStruct(newestSchema);
+
+      int indexInNewSchema = 0;
+      for (String field : newestSchema.getFieldNames()) {
+        if (orcStruct.getSchema().getFieldNames().contains(field)) {
+          int fieldIndex = orcStruct.getSchema().getFieldNames().indexOf(field);
+
+          TypeDescription fileType = orcStruct.getSchema().getChildren().get(fieldIndex);
+          TypeDescription readerType = newestSchema.getChildren().get(indexInNewSchema);
+
+          if (isEvolutionValid(fileType, readerType)) {
+            newStruct.setFieldValue(field, orcStruct.getFieldValue(field));
+          } else {
+            throw new SchemaEvolution.IllegalEvolutionException(String
+                .format("ORC does not support type conversion from file" + " type %s to reader type %s ",
+                    fileType.toString(), readerType.toString()));
+          }
+        } else {
+          newStruct.setFieldValue(field, null);
+        }
+
+        indexInNewSchema++;
+      }
+
+      return newStruct;
+    } else {
+      return orcStruct;
+    }
+  }
+
+  /**
+   * Determine if two types are following valid evolution.
+   * Implementation stolen and manipulated from {@link SchemaEvolution} as that was package-private.
+   */
+  boolean isEvolutionValid(TypeDescription fileType, TypeDescription readerType) {
+    boolean isOk = true;
+    if (fileType.getCategory() == readerType.getCategory()) {
+      switch (readerType.getCategory()) {
+        case BOOLEAN:
+        case BYTE:
+        case SHORT:
+        case INT:
+        case LONG:
+        case DOUBLE:
+        case FLOAT:
+        case STRING:
+        case TIMESTAMP:
+        case BINARY:
+        case DATE:
+          // these are always a match
+          break;
+        case CHAR:
+        case VARCHAR:
+          break;
+        case DECIMAL:
+          break;
+        case UNION:
+        case MAP:
+        case LIST: {
+          // these must be an exact match
+          List<TypeDescription> fileChildren = fileType.getChildren();
+          List<TypeDescription> readerChildren = readerType.getChildren();
+          if (fileChildren.size() == readerChildren.size()) {
+            for (int i = 0; i < fileChildren.size(); ++i) {
+              isOk &= isEvolutionValid(fileChildren.get(i), readerChildren.get(i));
+            }
+            return isOk;
+          } else {
+            return false;
+          }
+        }
+        case STRUCT: {
+          List<TypeDescription> readerChildren = readerType.getChildren();
+          List<TypeDescription> fileChildren = fileType.getChildren();
+          if (fileChildren.size() != readerChildren.size()) {
+            return false;
+          }
+
+          List<String> readerFieldNames = readerType.getFieldNames();
+          List<String> fileFieldNames = fileType.getFieldNames();
+
+          final Map<String, TypeDescription> fileTypesIdx = new HashMap();
+          for (int i = 0; i < fileFieldNames.size(); i++) {
+            final String fileFieldName = fileFieldNames.get(i);
+            fileTypesIdx.put(fileFieldName, fileChildren.get(i));
+          }
+
+          for (int i = 0; i < readerFieldNames.size(); i++) {
+            final String readerFieldName = readerFieldNames.get(i);
+            TypeDescription readerField = readerChildren.get(i);
+            TypeDescription fileField = fileTypesIdx.get(readerFieldName);
+            if (fileField == null) {
+              continue;
+            }
+
+            isOk &= isEvolutionValid(fileField, readerField);
+          }
+          return isOk;
+        }
+        default:
+          throw new IllegalArgumentException("Unknown type " + readerType);
+      }
+      return isOk;
+    } else {
+      /*
+       * Check for the few cases where will not convert....
+       */
+      return ConvertTreeReaderFactory.canConvert(fileType, readerType);
+    }
   }
 
   /**
