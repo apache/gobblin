8c7ff20666f6f059bc1bd41e2b8a32e13d298f34
sv2000 <sudarshanv@gmail.com>
2019-11-06 13:31:49 -0800

    [GOBBLIN-945] Refactor Kafka extractor statistics tracking to allow co…
    Closes #2795 from sv2000/statsTracker

--- gobblin-modules/gobblin-kafka-common/src/main/java/org/apache/gobblin/kafka/client/GobblinKafkaConsumerClient.java
+++ gobblin-modules/gobblin-kafka-common/src/main/java/org/apache/gobblin/kafka/client/GobblinKafkaConsumerClient.java
@@ -17,6 +17,7 @@
 package org.apache.gobblin.kafka.client;
 
 import java.io.Closeable;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -71,6 +72,25 @@ public interface GobblinKafkaConsumerClient extends Closeable {
    * @throws UnsupportedOperationException - If the underlying kafka-client does not support getting latest offset
    */
   public long getLatestOffset(KafkaPartition partition) throws KafkaOffsetRetrievalFailureException;
+
+  /**
+   * Get the latest available offset for a {@link Collection} of {@link KafkaPartition}s. NOTE: The default implementation
+   * is not efficient i.e. it will make a getLatestOffset() call for every {@link KafkaPartition}. Individual implementations
+   * of {@link GobblinKafkaConsumerClient} should override this method to use more advanced APIs of the underlying KafkaConsumer
+   * to retrieve the latest offsets for a collection of partitions.
+   *
+   * @param partitions for which latest offset is retrieved
+   *
+   * @throws KafkaOffsetRetrievalFailureException - If the underlying kafka-client does not support getting latest offset
+   */
+  public default Map<KafkaPartition, Long> getLatestOffsets(Collection<KafkaPartition> partitions)
+      throws KafkaOffsetRetrievalFailureException {
+    Map<KafkaPartition, Long> offsetMap = Maps.newHashMap();
+    for (KafkaPartition partition: partitions) {
+      offsetMap.put(partition, getLatestOffset(partition));
+    }
+    return offsetMap;
+  }
 
   /**
    * API to consume records from kakfa starting from <code>nextOffset</code> till <code>maxOffset</code>.
