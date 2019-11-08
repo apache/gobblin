2fe0fc5962d46f397fc6aa76bc65a9154b2c9cbe
Zihan Li <zihli@zihli-mn1.linkedin.biz>
2019-11-08 10:49:18 -0800

    address typos

--- gobblin-core/src/main/java/org/apache/gobblin/converter/avro/JsonElementConversionWithAvroSchemaFactory.java
+++ gobblin-core/src/main/java/org/apache/gobblin/converter/avro/JsonElementConversionWithAvroSchemaFactory.java
@@ -19,7 +19,6 @@ package org.apache.gobblin.converter.avro;
 
 import com.google.common.base.Preconditions;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
