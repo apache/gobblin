5606fd6157ebb880b3d849f6cf7c13cd80d8ecd4
William Lo <wlo@linkedin.com>
2019-12-09 11:31:42 -0800

    WIP

--- gobblin-service-loadbalancer/src/main/java/org/apache/gobblin/service/loadbalancer/ForwardRequestServlet.java
+++ gobblin-service-loadbalancer/src/main/java/org/apache/gobblin/service/loadbalancer/ForwardRequestServlet.java
@@ -1,15 +1,11 @@
 package org.apache.gobblin.service.loadbalancer;
 
-import com.google.api.client.http.HttpResponse;
-import java.io.IOException;
 import java.util.concurrent.Future;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
-import org.apache.commons.httpclient.HttpHost;
-import org.apache.gobblin.http.ApacheHttpAsyncClient;
-import org.apache.http.client.config.RequestConfig;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
@@ -19,7 +15,7 @@ import org.apache.http.impl.nio.client.HttpAsyncClients;
 
 public class ForwardRequestServlet extends HttpServlet {
 
-  CloseableHttpAsyncClient client;
+  public CloseableHttpAsyncClient client;
 
   ForwardRequestServlet() {
     this.client = HttpAsyncClients.createDefault();
@@ -28,27 +24,29 @@ public class ForwardRequestServlet extends HttpServlet {
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+    System.out.println("GET Request");
+    System.out.println(request.getQueryString());
     HttpGet req = new HttpGet("http://localhost:6956");
 
     forwardRequestToServer(req, response);
   }
 
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+    System.out.println("POST Request");
     HttpPost req  = new HttpPost("http://localhost:6956");
     forwardRequestToServer(req, response);
   }
 
-  private void forwardRequestToServer(HttpRequestBase req, HttpServletResponse response) throws ServletException {
+  protected void forwardRequestToServer(HttpRequestBase req, HttpServletResponse response) throws ServletException {
     Future<org.apache.http.HttpResponse> future = client.execute(req, null);
-
-    HttpResponse resp = future.get();
-
-    response.setContentType("application/json");
-    response.setStatus(resp.getStatusCode());
     try {
-      response.getWriter().print(resp.getContent());
-    } catch (IOException e) {
-      throw new ServletException("Failed to parse request");
+      HttpResponse resp = future.get();
+      response.setContentType("application/json");
+      response.setStatus(resp.getStatusLine().getStatusCode());
+      System.out.println("Finished forwarding request");
+    } catch (Exception e) {
+      // TODO: log exception properly
+      System.out.println(e.toString());
     }
   }
 }
--- gobblin-service-loadbalancer/src/main/java/org/apache/gobblin/service/loadbalancer/GobblinServiceLoadBalancer.java
+++ gobblin-service-loadbalancer/src/main/java/org/apache/gobblin/service/loadbalancer/GobblinServiceLoadBalancer.java
@@ -1,25 +1,88 @@
 package org.apache.gobblin.service.loadbalancer;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import com.google.common.base.Optional;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.gobblin.runtime.app.ApplicationException;
 import org.apache.gobblin.runtime.app.ApplicationLauncher;
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.Connector;
+
 
 public class GobblinServiceLoadBalancer implements ApplicationLauncher {
+  private Server server;
 
   @Override
   public void start() throws ApplicationException {
-    Server server = new Server(8080);
-    server.start();
-    server.join();
+    this.server = new Server(6956);
+    try {
+      ServletHandler servletHandler = new ServletHandler();
+      this.server.setHandler(servletHandler);
+      servletHandler.addServletWithMapping(ForwardRequestServlet.class, "/");
+      this.server.start();
+      this.server.join();
+
+      System.out.println("Started server");
+
+
+
+    } catch (Exception e) {
+      // TODO: handle error and log it properly
+      System.out.println(e.toString());
+    }
   }
 
   @Override
   public void stop() throws ApplicationException {
+  }
 
+  @Override
+  public void close() {
+    //
+  }
+
+  public static void main(String[] args) throws Exception {
+//    Options options = buildOptions();
+//    try {
+//      CommandLine cmd = new DefaultParser().parse(options, args);
+//      if (!cmd.hasOption(SERVICE_NAME_OPTION_NAME)) {
+//        printUsage(options);
+//        System.exit(1);
+//      }
+//
+//      if (!cmd.hasOption(SERVICE_ID_OPTION_NAME)) {
+//        printUsage(options);
+//        LOGGER.warn("Please assign globally unique ID for a GobblinServiceManager instance, or it will use default ID");
+//      }
+//
+//      boolean isTestMode = false;
+//      if (cmd.hasOption("test_mode")) {
+//        isTestMode = Boolean.parseBoolean(cmd.getOptionValue("test_mode", "false"));
+//      }
+//
+//      Config config = ConfigFactory.load();
+//      try (GobblinServiceManager gobblinServiceManager = new GobblinServiceManager(
+//          cmd.getOptionValue(SERVICE_NAME_OPTION_NAME), getServiceId(cmd),
+//          config, Optional.<Path>absent())) {
+//        gobblinServiceManager.getOrchestrator().setFlowStatusGenerator(gobblinServiceManager.buildFlowStatusGenerator(config));
+//        gobblinServiceManager.start();
+//
+//        if (isTestMode) {
+//          testGobblinService(gobblinServiceManager);
+//        }
+//      }
+//
+//    } catch (ParseException pe) {
+//      printUsage(options);
+//      System.exit(1);
+//    }
+    GobblinServiceLoadBalancer loadBalancer = new GobblinServiceLoadBalancer();
+    loadBalancer.start();
   }
 }
