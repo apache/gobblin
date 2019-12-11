73008c245d95dbdc9162f408de8686b2c094f4aa
William Lo <wlo@linkedin.com>
2019-12-10 17:25:14 -0800

    has post request working

--- gobblin-service-loadbalancer/src/main/java/org/apache/gobblin/service/loadbalancer/ForwardRequestServlet.java
+++ gobblin-service-loadbalancer/src/main/java/org/apache/gobblin/service/loadbalancer/ForwardRequestServlet.java
@@ -1,7 +1,11 @@
 package org.apache.gobblin.service.loadbalancer;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
+import java.util.Map;
 import java.util.concurrent.Future;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,23 +19,28 @@ import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.protocol.HTTP;
+import com.google.common.hash.Hashing;
 
 
 public class ForwardRequestServlet extends HttpServlet {
 
   public CloseableHttpAsyncClient client;
 
-  private String forwardedURI = "http://localhost:6956";
+  private String statefulSetBaseURL = "http://gaas-deployment-";
+  // TODO: split this up into configuration or something
+  private String restOfTheURL = ".default.svc.cluster.local";
+  private int numSchedulers = 3;
 
   ForwardRequestServlet() {
     this.client = HttpAsyncClients.createDefault();
     client.start();
-
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
     System.out.println("GET Request");
     System.out.println(request.getRequestURI() + "?" + request.getQueryString());
+    String forwardedURI = statefulSetBaseURL + "0";
     String requestURI = forwardedURI + request.getRequestURI();
     if (request.getQueryString() != null) {
       requestURI += "?" + request.getQueryString();
@@ -44,31 +53,73 @@ public class ForwardRequestServlet extends HttpServlet {
 
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
     System.out.println("POST Request");
-    System.out.println(request.getRequestURI() + "?" + request.getQueryString());
-    String requestURI = forwardedURI + request.getRequestURI();
-    if (request.getQueryString() != null) {
-      requestURI += "?" + request.getQueryString();
-    }
-    HttpPost forwardRequest = new HttpPost(requestURI);
-    parseAndAddHeaders(forwardRequest, request);
+
     try {
       String body = IOUtils.toString(request.getReader());
       System.out.println(body);
+      // get the flowName and flowGroup to hash
+      JsonParser parser = new JsonParser();
+      JsonObject jsonBody =  parser.parse(body).getAsJsonObject();
+      System.out.println("parsed body");
+
+      JsonObject flowIDMap = ((JsonObject) jsonBody.get("id"));
+
+      System.out.println("got id map");
+
+      if (flowIDMap.get("flowName") == null || flowIDMap.get("flowGroup") == null) {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        return;
+      }
+
+      String flowID = flowIDMap.get("flowName").getAsString() + flowIDMap.get("flowGroup").getAsString();
+
+      System.out.println(flowID);
+
+      System.out.println(Hashing.sha256().hashString(flowID, StandardCharsets.UTF_8).asInt());
+
+      int hashID = Hashing.sha256().hashString(flowID, StandardCharsets.UTF_8).asInt() % this.numSchedulers;
+      if (hashID < 0) {
+        hashID *= -1;
+      }
+      System.out.println(hashID);
+
+      String forwardedURI = statefulSetBaseURL + hashID + this.restOfTheURL;
+      System.out.println(forwardedURI);
+
+      String requestURI = forwardedURI +  request.getRequestURI();
+      HttpPost forwardRequest = new HttpPost(requestURI);
+      parseAndAddHeaders(forwardRequest, request);
+
       StringEntity requestBody = new StringEntity(body);
       forwardRequest.setEntity(requestBody);
+
+      if (request.getQueryString() != null) {
+        requestURI += "?" + request.getQueryString();
+      }
+      System.out.println(requestURI);
+
+      forwardRequestToServer(forwardRequest, response);
     } catch (IOException e) {
-      // TODO: do something useful with the error
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    } catch (ClassCastException e) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      System.out.println(e.toString());
     }
 
-     forwardRequestToServer(forwardRequest, response);
   }
 
   private void parseAndAddHeaders(HttpRequestBase forwardRequest, HttpServletRequest request) {
     Enumeration<String> headerNames = request.getHeaderNames();
     while (headerNames.hasMoreElements()) {
       String header = headerNames.nextElement();
-      forwardRequest.addHeader(header, request.getHeader(header));
+      if (forwardRequest.containsHeader(header)) {
+        forwardRequest.setHeader(header, request.getHeader(header));
+      } else {
+        forwardRequest.addHeader(header, request.getHeader(header));
+      }
     }
+    // remove this header as it is implicitly added when body is set
+    forwardRequest.removeHeaders(HTTP.CONTENT_LEN);
   }
 
   private void forwardRequestToServer(HttpRequestBase req, HttpServletResponse response) throws ServletException {
@@ -81,6 +132,7 @@ public class ForwardRequestServlet extends HttpServlet {
     } catch (Exception e) {
       // TODO: log exception properly
       System.out.println(e.toString());
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
   }
 
