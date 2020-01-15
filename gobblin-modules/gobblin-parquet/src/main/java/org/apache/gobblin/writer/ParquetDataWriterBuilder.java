f44ad4f2b19f9fcd00518a748748bd5b72c34df3
Shirshanka Das <sdas@linkedin.com>
2020-01-15 14:25:12 -0800

    [GOBBLIN-1015] Adding support for direct Avro and Protobuf writes in Parquet format
    Closes #2860 from shirshanka/protoavroparquet

--- gobblin-modules/gobblin-parquet/src/main/java/org/apache/gobblin/writer/ParquetDataWriterBuilder.java
+++ gobblin-modules/gobblin-parquet/src/main/java/org/apache/gobblin/writer/ParquetDataWriterBuilder.java
@@ -17,97 +17,101 @@
 package org.apache.gobblin.writer;
 
 import java.io.IOException;
-import java.util.Optional;
 
+import org.apache.avro.Schema;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 
-import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
+import com.google.protobuf.Message;
 
+import lombok.extern.slf4j.Slf4j;
+import parquet.avro.AvroParquetWriter;
 import parquet.column.ParquetProperties;
 import parquet.example.data.Group;
 import parquet.hadoop.ParquetWriter;
+import parquet.hadoop.api.WriteSupport;
 import parquet.hadoop.example.GroupWriteSupport;
 import parquet.hadoop.metadata.CompressionCodecName;
+import parquet.proto.ProtoParquetWriter;
 import parquet.schema.MessageType;
 
-import org.apache.gobblin.configuration.State;
-import org.apache.gobblin.util.ForkOperatorUtils;
+import org.apache.gobblin.parquet.writer.AbstractParquetDataWriterBuilder;
+import org.apache.gobblin.parquet.writer.ParquetWriterConfiguration;
+import org.apache.gobblin.parquet.writer.ParquetWriterShim;
 
-import static org.apache.gobblin.configuration.ConfigurationKeys.LOCAL_FS_URI;
-import static org.apache.gobblin.configuration.ConfigurationKeys.WRITER_CODEC_TYPE;
-import static org.apache.gobblin.configuration.ConfigurationKeys.WRITER_FILE_SYSTEM_URI;
-import static org.apache.gobblin.configuration.ConfigurationKeys.WRITER_PREFIX;
-import static parquet.hadoop.ParquetWriter.DEFAULT_BLOCK_SIZE;
-import static parquet.hadoop.ParquetWriter.DEFAULT_IS_DICTIONARY_ENABLED;
-import static parquet.hadoop.ParquetWriter.DEFAULT_IS_VALIDATING_ENABLED;
-import static parquet.hadoop.ParquetWriter.DEFAULT_PAGE_SIZE;
-
-
-public class ParquetDataWriterBuilder extends FsDataWriterBuilder<MessageType, Group> {
-  public static final String WRITER_PARQUET_PAGE_SIZE = WRITER_PREFIX + ".parquet.pageSize";
-  public static final String WRITER_PARQUET_DICTIONARY_PAGE_SIZE = WRITER_PREFIX + ".parquet.dictionaryPageSize";
-  public static final String WRITER_PARQUET_DICTIONARY = WRITER_PREFIX + ".parquet.dictionary";
-  public static final String WRITER_PARQUET_VALIDATE = WRITER_PREFIX + ".parquet.validate";
-  public static final String WRITER_PARQUET_VERSION = WRITER_PREFIX + ".parquet.version";
-  public static final String DEFAULT_PARQUET_WRITER = "v1";
-
-  @Override
-  public DataWriter<Group> build()
-      throws IOException {
-    Preconditions.checkNotNull(this.destination);
-    Preconditions.checkArgument(!Strings.isNullOrEmpty(this.writerId));
-    Preconditions.checkNotNull(this.schema);
-    Preconditions.checkArgument(this.format == WriterOutputFormat.PARQUET);
-
-    switch (this.destination.getType()) {
-      case HDFS:
-        return new ParquetHdfsDataWriter(this, this.destination.getProperties());
-      default:
-        throw new RuntimeException("Unknown destination type: " + this.destination.getType());
-    }
-  }
+@Slf4j
+public class ParquetDataWriterBuilder<S,D> extends AbstractParquetDataWriterBuilder<S,D> {
 
   /**
-   * Build a {@link ParquetWriter<Group>} for given file path with a block size.
-   * @param blockSize
-   * @param stagingFile
+   * Build a version-specific {@link ParquetWriter} for given {@link ParquetWriterConfiguration}
+   * @param writerConfiguration
    * @return
    * @throws IOException
    */
-  public ParquetWriter<Group> getWriter(int blockSize, Path stagingFile)
+  @Override
+  public ParquetWriterShim getVersionSpecificWriter(ParquetWriterConfiguration writerConfiguration)
       throws IOException {
-    State state = this.destination.getProperties();
-    int pageSize = state.getPropAsInt(getProperty(WRITER_PARQUET_PAGE_SIZE), DEFAULT_PAGE_SIZE);
-    int dictPageSize = state.getPropAsInt(getProperty(WRITER_PARQUET_DICTIONARY_PAGE_SIZE), DEFAULT_BLOCK_SIZE);
-    boolean enableDictionary =
-        state.getPropAsBoolean(getProperty(WRITER_PARQUET_DICTIONARY), DEFAULT_IS_DICTIONARY_ENABLED);
-    boolean validate = state.getPropAsBoolean(getProperty(WRITER_PARQUET_VALIDATE), DEFAULT_IS_VALIDATING_ENABLED);
-    String rootURI = state.getProp(WRITER_FILE_SYSTEM_URI, LOCAL_FS_URI);
-    Path absoluteStagingFile = new Path(rootURI, stagingFile);
-    CompressionCodecName codec = getCodecFromConfig();
-    GroupWriteSupport support = new GroupWriteSupport();
+
+    CompressionCodecName codecName = CompressionCodecName.fromConf(writerConfiguration.getCodecName());
+    ParquetProperties.WriterVersion writerVersion = ParquetProperties.WriterVersion
+        .fromString(writerConfiguration.getWriterVersion());
+
     Configuration conf = new Configuration();
-    GroupWriteSupport.setSchema(this.schema, conf);
-    ParquetProperties.WriterVersion writerVersion = getWriterVersion();
-    return new ParquetWriter<>(absoluteStagingFile, support, codec, blockSize, pageSize, dictPageSize, enableDictionary,
-        validate, writerVersion, conf);
-  }
+    ParquetWriter versionSpecificWriter = null;
+    switch (writerConfiguration.getRecordFormat()) {
+      case GROUP: {
+        GroupWriteSupport.setSchema((MessageType) this.schema, conf);
+        WriteSupport support = new GroupWriteSupport();
+        versionSpecificWriter = new ParquetWriter<Group>(
+            writerConfiguration.getAbsoluteStagingFile(),
+            support,
+            codecName,
+            writerConfiguration.getBlockSize(),
+            writerConfiguration.getPageSize(),
+            writerConfiguration.getDictPageSize(),
+            writerConfiguration.isDictionaryEnabled(),
+            writerConfiguration.isValidate(),
+            writerVersion,
+            conf);
+        break;
+      }
+      case AVRO:  {
+        versionSpecificWriter = new AvroParquetWriter(
+            writerConfiguration.getAbsoluteStagingFile(),
+            (Schema) this.schema,
+            codecName,
+            writerConfiguration.getBlockSize(),
+            writerConfiguration.getPageSize(),
+            writerConfiguration.isDictionaryEnabled(),
+            conf);
+        break;
+      }
+      case PROTOBUF: {
+        versionSpecificWriter = new ProtoParquetWriter(
+            writerConfiguration.getAbsoluteStagingFile(),
+            (Class<? extends Message>) this.schema,
+            codecName,
+            writerConfiguration.getBlockSize(),
+            writerConfiguration.getPageSize(),
+            writerConfiguration.isDictionaryEnabled(),
+            writerConfiguration.isValidate());
+        break;
+      }
+      default: throw new RuntimeException("Record format not supported");
+    }
+    ParquetWriter finalVersionSpecificWriter = versionSpecificWriter;
 
-  private ParquetProperties.WriterVersion getWriterVersion() {
-    return ParquetProperties.WriterVersion.fromString(
-        this.destination.getProperties().getProp(getProperty(WRITER_PARQUET_VERSION), DEFAULT_PARQUET_WRITER));
-  }
+    return new ParquetWriterShim() {
+      @Override
+      public void write(Object record)
+          throws IOException {
+        finalVersionSpecificWriter.write(record);
+      }
 
-  private CompressionCodecName getCodecFromConfig() {
-    State state = this.destination.getProperties();
-    String codecValue = Optional.ofNullable(state.getProp(getProperty(WRITER_CODEC_TYPE)))
-        .orElse(CompressionCodecName.SNAPPY.toString());
-    return CompressionCodecName.valueOf(codecValue.toUpperCase());
-  }
-
-  private String getProperty(String key) {
-    return ForkOperatorUtils.getPropertyNameForBranch(key, this.getBranches(), this.getBranch());
+      @Override
+      public void close()
+          throws IOException {
+        finalVersionSpecificWriter.close();
+      }
+    };
   }
 }
