453603573a6ce7cfb03e6a4241f901d7c4113916
Arjun <abora@linkedin.com>
2020-01-29 09:29:58 -0800

    make hive dataset descriptor accepts regexed db and tables

--- gobblin-service/src/test/java/org/apache/gobblin/service/modules/dataset/HiveDatasetDescriptorTest.java
+++ gobblin-service/src/test/java/org/apache/gobblin/service/modules/dataset/HiveDatasetDescriptorTest.java
@@ -30,16 +30,43 @@ import org.apache.gobblin.service.modules.flowgraph.DatasetDescriptorConfigKeys;
 
 
 public class HiveDatasetDescriptorTest {
+  Config baseConfig = ConfigFactory.empty().withValue(DatasetDescriptorConfigKeys.PLATFORM_KEY, ConfigValueFactory.fromAnyRef("hive"))
+      .withValue(DatasetDescriptorConfigKeys.DATABASE_KEY, ConfigValueFactory.fromAnyRef("testDb_Db1"))
+      .withValue(DatasetDescriptorConfigKeys.TABLE_KEY, ConfigValueFactory.fromAnyRef("testTable_Table1"));;
 
   @Test
   public void objectCreation() throws IOException {
-    Config baseConfig = ConfigFactory.empty().withValue(DatasetDescriptorConfigKeys.PLATFORM_KEY, ConfigValueFactory.fromAnyRef("hive"))
-        .withValue(DatasetDescriptorConfigKeys.DATABASE_KEY, ConfigValueFactory.fromAnyRef("testDb_Db1"))
-        .withValue(DatasetDescriptorConfigKeys.TABLE_KEY, ConfigValueFactory.fromAnyRef("testTable_Table1"));;
-
     SqlDatasetDescriptor descriptor1 = new HiveDatasetDescriptor(baseConfig.withValue(HiveDatasetDescriptor.IS_PARTITIONED_KEY, ConfigValueFactory.fromAnyRef(true)));
     SqlDatasetDescriptor descriptor2 = new HiveDatasetDescriptor(baseConfig.withValue(HiveDatasetDescriptor.IS_PARTITIONED_KEY, ConfigValueFactory.fromAnyRef(false)));
 
     Assert.assertNotEquals(descriptor1, descriptor2);
+  }
+
+  @Test
+  public void testWhitelist() throws IOException {
+    HiveDatasetDescriptor descriptor = new HiveDatasetDescriptor(baseConfig
+        .withValue(DatasetDescriptorConfigKeys.DATABASE_KEY, ConfigValueFactory.fromAnyRef("test*"))
+        .withValue(DatasetDescriptorConfigKeys.TABLE_KEY, ConfigValueFactory.fromAnyRef("abc,def,fgh"))
+    );
+    Assert.assertTrue(descriptor.whitelistBlacklist.acceptTable("testDb", "abc"));
+    Assert.assertTrue(descriptor.whitelistBlacklist.acceptTable("testDb", "abca"));
+    Assert.assertFalse(descriptor.whitelistBlacklist.acceptTable("otherTestDb", "abc"));
+
+    descriptor = new HiveDatasetDescriptor(baseConfig
+        .withValue(DatasetDescriptorConfigKeys.DATABASE_KEY, ConfigValueFactory.fromAnyRef("testDb"))
+        .withValue(DatasetDescriptorConfigKeys.TABLE_KEY, ConfigValueFactory.fromAnyRef("abc,def,ghi"))
+    );
+    Assert.assertTrue(descriptor.whitelistBlacklist.acceptTable("testDb", "abc"));
+    Assert.assertFalse(descriptor.whitelistBlacklist.acceptTable("testDb", "abca"));
+    Assert.assertFalse(descriptor.whitelistBlacklist.acceptTable("otherTestDb", "abc"));
+
+    descriptor = new HiveDatasetDescriptor(baseConfig
+        .withValue(DatasetDescriptorConfigKeys.DATABASE_KEY, ConfigValueFactory.fromAnyRef("testDb"))
+        .withValue(DatasetDescriptorConfigKeys.TABLE_KEY, ConfigValueFactory.fromAnyRef("abc*,ghi"))
+    );
+    Assert.assertTrue(descriptor.whitelistBlacklist.acceptTable("testDb", "abc"));
+    Assert.assertTrue(descriptor.whitelistBlacklist.acceptTable("testDb", "ghi"));
+    Assert.assertTrue(descriptor.whitelistBlacklist.acceptTable("testDb", "abcabc"));
+    Assert.assertFalse(descriptor.whitelistBlacklist.acceptTable("otherTestDb", "abc"));
   }
 }
