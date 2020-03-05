737543872e483682661b35de08221cb4ca79b9dd
vbohra <vbohra@linkedin.com>
2020-03-04 22:29:44 -0800

    removed log for debugging

--- gobblin-modules/gobblin-kafka-08/src/test/java/org/apache/gobblin/metrics/reporter/KafkaTestBase.java
+++ gobblin-modules/gobblin-kafka-08/src/test/java/org/apache/gobblin/metrics/reporter/KafkaTestBase.java
@@ -66,7 +66,6 @@ public class KafkaTestBase implements Closeable {
     if (serverStarted && serverClosed) {
       throw new RuntimeException("Kafka test server has already been closed. Cannot generate Kafka server twice.");
     }
-    System.out.println("Boo: " + TestZKUtils.class.getProtectionDomain().getCodeSource().getLocation().getPath());
 
     if (!serverStarted) {
       serverStarted = true;
