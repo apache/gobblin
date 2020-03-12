623dcfa225f935e8235f8fc22840f19d194ad379
Zihan Li <zihli@zihli-mn1.linkedin.biz>
2020-03-12 13:06:41 -0700

    address commemnts

--- gobblin-utility/src/main/java/org/apache/gobblin/util/AvroUtils.java
+++ gobblin-utility/src/main/java/org/apache/gobblin/util/AvroUtils.java
@@ -120,11 +120,15 @@ public class AvroUtils {
   }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   public static Schema addSchemaCreationTime(Schema inputSchema, Schema outputSchema) {
     if (inputSchema.getProp(SCHEMA_CREATION_TIME_KEY) != null && outputSchema.getProp(SCHEMA_CREATION_TIME_KEY) == null) {
       outputSchema.addProp(SCHEMA_CREATION_TIME_KEY, inputSchema.getProp(SCHEMA_CREATION_TIME_KEY));
 =======
   public static Schema preserveCreationTime(Schema inputSchema, Schema outputSchema) {
+=======
+  public static Schema addSchemaCreationTime(Schema inputSchema, Schema outputSchema) {
+>>>>>>> address commemnts
     if (inputSchema.getProp(ConfigurationKeys.SCHEMA_CREATION_TIME_KEY) != null && outputSchema.getProp(ConfigurationKeys.SCHEMA_CREATION_TIME_KEY) == null) {
       outputSchema.addProp(ConfigurationKeys.SCHEMA_CREATION_TIME_KEY, inputSchema.getProp(ConfigurationKeys.SCHEMA_CREATION_TIME_KEY));
 >>>>>>> Add configuration to preserve schema creation time in converter
