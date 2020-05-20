3267842963a83c5d48ee21db5fa54895a9349187
Alex Li <aleli@linkedin.com>
2020-05-20 16:33:54 -0700

    refactor if-else to functions in util class

--- gobblin-salesforce/src/main/java/org/apache/gobblin/typedconfig/ConstraintUtil.java
+++ gobblin-salesforce/src/main/java/org/apache/gobblin/typedconfig/ConstraintUtil.java
@@ -41,61 +41,76 @@ public class ConstraintUtil {
     }
     Class type = field.getType();
     if (type == int.class) {
-      IntRange intRange = field.getAnnotation(IntRange.class);
-      if (intRange == null) {
-        return value;
-      }
-      int[] range = intRange.value();
-      int intValue = Integer.parseInt(value.toString());
-      if (range.length != 2 || (range.length == 2 && intValue >= range[0] && intValue <= range[1])) {
-        return value;
-      } else {
-        return defaultValue;
-      }
+      return getIntValue(field, value, defaultValue);
     } else if (type == long.class) {
-      LongRange longRange = field.getAnnotation(LongRange.class);
-      long[] range = longRange.value();
-      if (range == null) {
-        return value;
-      }
-      long longValue = Long.parseLong(value.toString());
-      if (range.length != 2 || (range.length == 2 && longValue > range[0] && longValue < range[1])) {
-        return value;
-      } else {
-        return defaultValue;
-      }
+      return getLongValue(field, value, defaultValue);
     } else if (type == String.class) {
-      StringRegex stringRegex = field.getAnnotation(StringRegex.class);
-      if (stringRegex == null) {
-        return value;
-      }
-      String regex = stringRegex.value();
-      if (regex == null) {
-        return value;
-      }
-      boolean isMatching = value.toString().matches(regex);
-      if (isMatching) {
-        return value;
-      } else {
-        return defaultValue;
-      }
+      return getStringValue(field, value, defaultValue);
     } else if (type.isEnum()) {
-      EnumOptions enumOptions = field.getAnnotation(EnumOptions.class);
-      if (enumOptions == null) {
-        return value;
-      }
-      List<String> options = Arrays.asList(enumOptions.value());
-      if (options.indexOf(value) >= 0) {
-        return value;
-      } else {
-        return defaultValue;
-      }
+      return getEnumValue(field, value, defaultValue);
     } else if (type == Boolean.class || type == boolean.class) {
       return value;
     } else if (type == Date.class) {
       return value;
     } else {
       throw new RuntimeException("not supported the return type: " + type.getName());
+    }
+  }
+
+  static private Object getIntValue(Field field, Object value, Object defaultValue) {
+    IntRange intRange = field.getAnnotation(IntRange.class);
+    if (intRange == null) {
+      return value;
+    }
+    int[] range = intRange.value();
+    int intValue = Integer.parseInt(value.toString());
+    if (range.length != 2 || (range.length == 2 && intValue >= range[0] && intValue <= range[1])) {
+      return value;
+    } else {
+      return defaultValue;
+    }
+  }
+  static private Object getLongValue(Field field, Object value, Object defaultValue) {
+    LongRange longRange = field.getAnnotation(LongRange.class);
+    long[] range = longRange.value();
+    if (range == null) {
+      return value;
+    }
+    long longValue = Long.parseLong(value.toString());
+    if (range.length != 2 || (range.length == 2 && longValue > range[0] && longValue < range[1])) {
+      return value;
+    } else {
+      return defaultValue;
+    }
+  }
+
+  static private Object getStringValue(Field field, Object value, Object defaultValue) {
+    StringRegex stringRegex = field.getAnnotation(StringRegex.class);
+    if (stringRegex == null) {
+      return value;
+    }
+    String regex = stringRegex.value();
+    if (regex == null) {
+      return value;
+    }
+    boolean isMatching = value.toString().matches(regex);
+    if (isMatching) {
+      return value;
+    } else {
+      return defaultValue;
+    }
+  }
+
+  static private Object getEnumValue(Field field, Object value, Object defaultValue) {
+    EnumOptions enumOptions = field.getAnnotation(EnumOptions.class);
+    if (enumOptions == null) {
+      return value;
+    }
+    List<String> options = Arrays.asList(enumOptions.value());
+    if (options.indexOf(value) >= 0) {
+      return value;
+    } else {
+      return defaultValue;
     }
   }
 }
