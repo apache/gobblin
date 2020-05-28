bc07d20231f7589737125fee6387067d76fb4a78
vbohra <vbohra@linkedin.com>
2020-05-28 11:21:46 -0700

    test commit to test bintray publishing

--- travis/bintrayDeploy.sh
+++ travis/bintrayDeploy.sh
@@ -32,7 +32,8 @@ echo "Build Version: $BUILD_VERSION"
 
 echo "Pull request: [$TRAVIS_PULL_REQUEST], Travis branch: [$TRAVIS_BRANCH]"
 # release only from master when no pull request build
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
+
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "true" ]
 then
     echo "Uploading artifacts to bintray for version $BUILD_VERSION"
     ./gradlew -i bintrayUpload -Pversion=$BUILD_VERSION
