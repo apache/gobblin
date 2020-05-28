e490cb091747eade2e370a3a50a070d3bf9c18a5
vbohra <vbohra@linkedin.com>
2020-05-28 12:11:13 -0700

    Added deploy step to travis to deploy artifacts to bintray when merged to master

--- travis/bintrayDeploy.sh
+++ travis/bintrayDeploy.sh
@@ -33,7 +33,7 @@ echo "Build Version: $BUILD_VERSION"
 echo "Pull request: [$TRAVIS_PULL_REQUEST], Travis branch: [$TRAVIS_BRANCH]"
 # release only from master when no pull request build
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "true" ]
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
     echo "Uploading artifacts to bintray for version $BUILD_VERSION"
     ./gradlew -i bintrayUpload -Pversion=$BUILD_VERSION
