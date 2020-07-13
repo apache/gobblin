152c4733363b485bc3924ede08ff40c0da82a10d
Zihan Li <zihli@zihli-mn1.linkedin.biz>
2020-07-13 16:53:17 -0700

    address comments

--- gobblin-yarn/src/main/java/org/apache/gobblin/yarn/YarnHelixUtils.java
+++ gobblin-yarn/src/main/java/org/apache/gobblin/yarn/YarnHelixUtils.java
@@ -24,12 +24,14 @@ import java.util.Collection;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.gobblin.cluster.GobblinClusterUtils;
 import org.apache.gobblin.util.ConfigUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.security.Credentials;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.security.token.Token;
 import org.apache.hadoop.security.token.TokenIdentifier;
 import org.apache.hadoop.yarn.api.ApplicationConstants;
@@ -42,6 +44,8 @@ import org.apache.hadoop.yarn.util.ConverterUtils;
 import org.apache.hadoop.yarn.util.Records;
 
 import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -50,6 +54,8 @@ import com.google.common.collect.Maps;
  * @author Yinan Li
  */
 public class YarnHelixUtils {
+  private static final Logger LOGGER = LoggerFactory.getLogger(YarnHelixUtils.class);
+
   /**
    * Write a {@link Token} to a given file.
    *
@@ -63,6 +69,27 @@ public class YarnHelixUtils {
     Credentials credentials = new Credentials();
     credentials.addToken(token.getService(), token);
     credentials.writeTokenStorageFile(tokenFilePath, configuration);
+  }
+
+  /**
+   * Update {@link Token} with the configured token path.
+   *
+   * @param config a {@link Config} object carrying Hadoop configuration properties and token path config
+   * @throws IOException
+   */
+  public static void updateToken(Config config) throws IOException{
+    FileSystem fs = GobblinClusterUtils.buildFileSystem(config, new Configuration());
+    Path tokenPath = new Path(fs.getHomeDirectory(),
+        config.getString(GobblinYarnConfigurationKeys.APPLICATION_NAME_KEY) + Path.SEPARATOR
+            + GobblinYarnConfigurationKeys.TOKEN_FILE_NAME);
+    if(fs.exists(tokenPath)) {
+      Credentials credentials = Credentials.readTokenStorageFile(tokenPath, fs.getConf());
+      for (Token<? extends TokenIdentifier> token : credentials.getAllTokens()) {
+        LOGGER.info("updating " + token.toString());
+      }
+      UserGroupInformation.getCurrentUser()
+          .addCredentials(credentials);
+    }
   }
 
   /**
