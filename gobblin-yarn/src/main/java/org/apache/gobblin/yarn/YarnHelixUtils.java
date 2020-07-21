c4da928bbfcb8e91ae0221fc4d97f483fbf2b947
Arjun <abora@linkedin.com>
2020-07-21 16:20:03 -0700

    [GOBBLIN-1217] start metrics reporting with a few map-reduce properties
    Closes #3065 from arjun4084346/mapperNum

--- gobblin-yarn/src/main/java/org/apache/gobblin/yarn/YarnHelixUtils.java
+++ gobblin-yarn/src/main/java/org/apache/gobblin/yarn/YarnHelixUtils.java
@@ -17,14 +17,12 @@
 
 package org.apache.gobblin.yarn;
 
-import com.typesafe.config.Config;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.gobblin.util.ConfigUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -45,6 +43,9 @@ import org.apache.hadoop.yarn.util.Records;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.typesafe.config.Config;
+
+import org.apache.gobblin.util.ConfigUtils;
 
 
 /**
