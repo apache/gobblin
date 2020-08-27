a297fe137c5442360aa62ff23f913f3e3deeb278
Zihan Li <zihli@zihli-mn1.linkedin.biz>
2020-08-27 12:29:13 -0700

    modify test to pass checkStyle

--- gobblin-hive-registration/src/test/java/org/apache/gobblin/hive/metastore/HiveMetaStoreBasedRegisterTest.java
+++ gobblin-hive-registration/src/test/java/org/apache/gobblin/hive/metastore/HiveMetaStoreBasedRegisterTest.java
@@ -1,6 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.gobblin.hive.metastore;
 
 import com.google.common.base.Optional;
+import java.io.IOException;
 import java.util.Properties;
 import org.apache.avro.Schema;
 import org.apache.gobblin.configuration.State;
@@ -21,72 +38,68 @@ import org.testng.annotations.Test;
 
 public class HiveMetaStoreBasedRegisterTest {
   @Test
-  public void testUpdateSchemaMethod() {
-    try {
-      final String databaseName = "testdb";
-      final String tableName = "testtable";
+  public void testUpdateSchemaMethod() throws IOException {
 
-      State state = new State();
-      state.setProp(HiveMetaStoreBasedRegister.FETCH_LATEST_SCHEMA, true);
-      state.setProp(KafkaSchemaRegistry.KAFKA_SCHEMA_REGISTRY_CLASS, MockSchemaRegistry.class.getName());
-      HiveMetaStoreBasedRegister register = new HiveMetaStoreBasedRegister(state, Optional.absent());
-      Schema writerSchema = new Schema.Parser().parse("{\"type\": \"record\", \"name\": \"TestEvent\","
-          + " \"namespace\": \"test.namespace\", \"fields\": [{\"name\":\"testName\"," + " \"type\": \"int\"}]}");
-      AvroUtils.setSchemaCreationTime(writerSchema, "111");
+    final String databaseName = "testdb";
+    final String tableName = "testtable";
 
-      //Build hiveTable
-      HiveTable.Builder builder = new HiveTable.Builder();
-      builder.withDbName(databaseName).withTableName(tableName);
+    State state = new State();
+    state.setProp(HiveMetaStoreBasedRegister.FETCH_LATEST_SCHEMA, true);
+    state.setProp(KafkaSchemaRegistry.KAFKA_SCHEMA_REGISTRY_CLASS, MockSchemaRegistry.class.getName());
+    HiveMetaStoreBasedRegister register = new HiveMetaStoreBasedRegister(state, Optional.absent());
+    Schema writerSchema = new Schema.Parser().parse("{\"type\": \"record\", \"name\": \"TestEvent\","
+        + " \"namespace\": \"test.namespace\", \"fields\": [{\"name\":\"testName\"," + " \"type\": \"int\"}]}");
+    AvroUtils.setSchemaCreationTime(writerSchema, "111");
 
-      State serdeProps = new State();
-      serdeProps.setProp("avro.schema.literal", writerSchema.toString());
-      builder.withSerdeProps(serdeProps);
+    //Build hiveTable
+    HiveTable.Builder builder = new HiveTable.Builder();
+    builder.withDbName(databaseName).withTableName(tableName);
 
-      HiveTable hiveTable = builder.build();
-      HiveTable existingTable = builder.build();
+    State serdeProps = new State();
+    serdeProps.setProp("avro.schema.literal", writerSchema.toString());
+    builder.withSerdeProps(serdeProps);
 
-      hiveTable.setInputFormat(AvroContainerInputFormat.class.getName());
-      hiveTable.setOutputFormat(AvroContainerOutputFormat.class.getName());
-      hiveTable.setSerDeType(AvroSerDe.class.getName());
+    HiveTable hiveTable = builder.build();
+    HiveTable existingTable = builder.build();
 
-      existingTable.setInputFormat(AvroContainerInputFormat.class.getName());
-      existingTable.setOutputFormat(AvroContainerOutputFormat.class.getName());
-      existingTable.setSerDeType(AvroSerDe.class.getName());
+    hiveTable.setInputFormat(AvroContainerInputFormat.class.getName());
+    hiveTable.setOutputFormat(AvroContainerOutputFormat.class.getName());
+    hiveTable.setSerDeType(AvroSerDe.class.getName());
 
-      SimpleHiveSpec.Builder specBuilder = new SimpleHiveSpec.Builder(new Path("pathString"))
-          .withPartition(Optional.absent())
-          .withTable(hiveTable);
-      Table table = HiveMetaStoreUtils.getTable(hiveTable);
-      SimpleHiveSpec simpleHiveSpec = specBuilder.build();
+    existingTable.setInputFormat(AvroContainerInputFormat.class.getName());
+    existingTable.setOutputFormat(AvroContainerOutputFormat.class.getName());
+    existingTable.setSerDeType(AvroSerDe.class.getName());
 
-      //Test new schema equals existing schema, we don't change anything
-      register.updateSchema(simpleHiveSpec, table, existingTable);
-      Assert.assertEquals(table.getSd().getSerdeInfo().getParameters()
-          .get(AvroSerdeUtils.AvroTableProperties.SCHEMA_LITERAL.getPropName()), writerSchema.toString());
+    SimpleHiveSpec.Builder specBuilder = new SimpleHiveSpec.Builder(new Path("pathString"))
+        .withPartition(Optional.absent())
+        .withTable(hiveTable);
+    Table table = HiveMetaStoreUtils.getTable(hiveTable);
+    SimpleHiveSpec simpleHiveSpec = specBuilder.build();
 
-      //Test new schema does not equal to existing schema, and latest schema does not equals to existing schema
-      //We set schema to writer schema
-      register.schemaRegistry.get().register(writerSchema);
-      Schema existingSchema = new Schema.Parser().parse("{\"type\": \"record\", \"name\": \"TestEvent_1\","
-          + " \"namespace\": \"test.namespace\", \"fields\": [{\"name\":\"testName_1\"," + " \"type\": \"double\"}]}");
-      AvroUtils.setSchemaCreationTime(writerSchema, "110");
-      existingTable.getSerDeProps()
-          .setProp(AvroSerdeUtils.AvroTableProperties.SCHEMA_LITERAL.getPropName(), existingSchema.toString());
-      register.updateSchema(simpleHiveSpec, table, existingTable);
-      Assert.assertEquals(table.getSd().getSerdeInfo().getParameters()
-          .get(AvroSerdeUtils.AvroTableProperties.SCHEMA_LITERAL.getPropName()), writerSchema.toString());
+    //Test new schema equals existing schema, we don't change anything
+    register.updateSchema(simpleHiveSpec, table, existingTable);
+    Assert.assertEquals(table.getSd().getSerdeInfo().getParameters()
+        .get(AvroSerdeUtils.AvroTableProperties.SCHEMA_LITERAL.getPropName()), writerSchema.toString());
 
-      //Test new schema does not equal to existing schema, latest schema equals to existing schema,
-      //in this case, table schema should be existingSchema
-      register.schemaRegistry.get().register(existingSchema);
-      register.updateSchema(simpleHiveSpec, table, existingTable);
-      Assert.assertEquals(table.getSd().getSerdeInfo().getParameters()
-          .get(AvroSerdeUtils.AvroTableProperties.SCHEMA_LITERAL.getPropName()), existingSchema.toString());
+    //Test new schema does not equal to existing schema, and latest schema does not equals to existing schema
+    //We set schema to writer schema
+    register.schemaRegistry.get().register(writerSchema, "writerSchema");
+    Schema existingSchema = new Schema.Parser().parse("{\"type\": \"record\", \"name\": \"TestEvent_1\","
+        + " \"namespace\": \"test.namespace\", \"fields\": [{\"name\":\"testName_1\"," + " \"type\": \"double\"}]}");
+    AvroUtils.setSchemaCreationTime(existingSchema, "110");
+    existingTable.getSerDeProps()
+        .setProp(AvroSerdeUtils.AvroTableProperties.SCHEMA_LITERAL.getPropName(), existingSchema.toString());
+    register.updateSchema(simpleHiveSpec, table, existingTable);
+    Assert.assertEquals(table.getSd().getSerdeInfo().getParameters()
+        .get(AvroSerdeUtils.AvroTableProperties.SCHEMA_LITERAL.getPropName()), writerSchema.toString());
 
+    //Test new schema does not equal to existing schema, latest schema equals to existing schema,
+    //in this case, table schema should be existingSchema
+    register.schemaRegistry.get().register(existingSchema, "existingSchema");
+    register.updateSchema(simpleHiveSpec, table, existingTable);
+    Assert.assertEquals(table.getSd().getSerdeInfo().getParameters()
+        .get(AvroSerdeUtils.AvroTableProperties.SCHEMA_LITERAL.getPropName()), existingSchema.toString());
 
-    } catch (Exception e) {
-      Assert.assertFalse(e instanceof NoSuchMethodException);
-    }
   }
 
   public static class MockSchemaRegistry extends KafkaSchemaRegistry<String, Schema> {
