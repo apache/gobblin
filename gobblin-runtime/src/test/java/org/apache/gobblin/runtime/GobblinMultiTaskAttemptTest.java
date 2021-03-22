24ebc02852f70ffaaef97139ff0ae61cf6dba2ce
suvasude <suvasude@linkedin.biz>
2021-03-22 15:12:37 -0700

    [GOBBLIN-1416] Fix a race condition caused by Helix task cancellation being invoked before Gobblin task creation
    Closes #3250 from sv2000/taskCancelRaceCondition

--- gobblin-runtime/src/test/java/org/apache/gobblin/runtime/GobblinMultiTaskAttemptTest.java
+++ gobblin-runtime/src/test/java/org/apache/gobblin/runtime/GobblinMultiTaskAttemptTest.java
@@ -19,6 +19,7 @@ package org.apache.gobblin.runtime;
 
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.Assert;
 import org.mockito.Mockito;
@@ -65,7 +66,7 @@ public class GobblinMultiTaskAttemptTest {
   public void testRunWithTaskCreationFailure()
       throws Exception {
     // Preparing Instance of TaskAttempt with designed failure on task creation
-    WorkUnit tmpWU = new WorkUnit();
+    WorkUnit tmpWU = WorkUnit.createEmpty();
     // Put necessary attributes in workunit
     tmpWU.setProp(ConfigurationKeys.TASK_ID_KEY, "task_test");
     List<WorkUnit> workUnit = ImmutableList.of(tmpWU);
@@ -96,7 +97,7 @@ public class GobblinMultiTaskAttemptTest {
       throws Exception {
     TaskStateTracker stateTracker = new DummyTestStateTracker(new Properties(), log);
     // Preparing Instance of TaskAttempt with designed failure on task creation
-    WorkUnit tmpWU = new WorkUnit();
+    WorkUnit tmpWU = WorkUnit.createEmpty();
     // Put necessary attributes in workunit
     tmpWU.setProp(ConfigurationKeys.TASK_ID_KEY, "task_test");
     List<WorkUnit> workUnit = ImmutableList.of(tmpWU);
@@ -120,6 +121,31 @@ public class GobblinMultiTaskAttemptTest {
     // Should never reach here.
     Assert.fail();
   }
+
+  @Test
+  public void testRunAfterCancellation() throws Exception {
+    WorkUnit tmpWU = WorkUnit.createEmpty();
+    // Put necessary attributes in workunit
+    tmpWU.setProp(ConfigurationKeys.TASK_ID_KEY, "task_test");
+    List<WorkUnit> workUnit = ImmutableList.of(tmpWU);
+    JobState jobState = new JobState();
+    // Limit the number of times of retry in task-creation.
+    jobState.setProp(RETRY_TIME_OUT_MS, 1000);
+    jobState.setProp(ConfigurationKeys.SOURCE_CLASS_KEY, DatasetStateStoreTest.DummySource.class.getName());
+
+    TaskStateTracker stateTrackerMock = Mockito.mock(TaskStateTracker.class);
+
+    taskAttempt =
+        new GobblinMultiTaskAttempt(workUnit.iterator(), "testJob1", jobState, stateTrackerMock, taskExecutorMock,
+            Optional.absent(), Optional.absent(), jobBroker);
+
+    //Call shutdown() before creation of underlying Gobblin tasks.
+    taskAttempt.shutdownTasks();
+    taskAttempt.runAndOptionallyCommitTaskAttempt(GobblinMultiTaskAttempt.CommitPolicy.IMMEDIATE);
+    Assert.assertEquals(taskAttempt.getNumTasksCreated(), 0);
+    Assert.assertTrue(taskAttempt.getStopped().get());
+  }
+
 
   public static class DummyTestStateTracker extends AbstractTaskStateTracker {
     public DummyTestStateTracker(Properties properties, Logger logger) {
