44c73168125e1132b0cf511a78bee3cfc88522d3
Lei Sun <autumnust@gmail.com>
2021-08-20 14:56:27 -0700

    [GOBBLIN-1520] Standardize FsSpec's protocol in file name
    Standardize FsSpec's protocol in file name
    
    Address reviewer's comments
    
    Remove unused imports
    
    Closes #3370 from autumnust/spec-consumer-avro

--- gobblin-runtime/src/test/java/org/apache/gobblin/runtime/api/FsSpecProducerTest.java
+++ gobblin-runtime/src/test/java/org/apache/gobblin/runtime/api/FsSpecProducerTest.java
@@ -23,6 +23,7 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.testng.Assert;
@@ -40,18 +41,26 @@ import org.apache.gobblin.util.ConfigUtils;
 public class FsSpecProducerTest {
   private FsSpecProducer _fsSpecProducer;
   private FsSpecConsumer _fsSpecConsumer;
+  private Config _config;
+  private File workDir;
 
   @BeforeMethod
   public void setUp()
       throws IOException {
-    File tmpDir = Files.createTempDir();
+    this.workDir = Files.createTempDir();
+    this.workDir.deleteOnExit();
     Config config = ConfigFactory.empty().withValue(FsSpecConsumer.SPEC_PATH_KEY, ConfigValueFactory.fromAnyRef(
-        tmpDir.getAbsolutePath()));
+        this.workDir.getAbsolutePath()));
     this._fsSpecProducer = new FsSpecProducer(config);
     this._fsSpecConsumer = new FsSpecConsumer(config);
+    this._config = config;
   }
 
   private JobSpec createTestJobSpec() throws URISyntaxException {
+    return createTestJobSpec("testJob");
+  }
+
+  private JobSpec createTestJobSpec(String jobSpecUri) throws URISyntaxException {
     Properties properties = new Properties();
     properties.put("key1", "val1");
     properties.put("key2", "val2");
@@ -59,7 +68,7 @@ public class FsSpecProducerTest {
     properties.put("key3.1", "val3");
     properties.put("key3.1.1", "val4");
 
-    JobSpec jobSpec = JobSpec.builder("testJob")
+    JobSpec jobSpec = JobSpec.builder(jobSpecUri)
         .withConfig(ConfigUtils.propertiesToConfig(properties))
         .withVersion("1")
         .withDescription("")
@@ -69,8 +78,13 @@ public class FsSpecProducerTest {
 
   @Test
   public void testAddSpec()
-      throws URISyntaxException, ExecutionException, InterruptedException {
+      throws URISyntaxException, ExecutionException, InterruptedException, IOException {
     this._fsSpecProducer.addSpec(createTestJobSpec());
+
+    // Add some random files(with non-avro extension name) into the folder observed by consumer, they shall not be picked up.
+    File randomFile = new File(workDir, "random");
+    Assert.assertTrue(randomFile.createNewFile());
+    randomFile.deleteOnExit();
 
     List<Pair<SpecExecutor.Verb, Spec>> jobSpecs = this._fsSpecConsumer.changedSpecs().get();
     Assert.assertEquals(jobSpecs.size(), 1);
@@ -80,6 +94,17 @@ public class FsSpecProducerTest {
     Assert.assertEquals(((JobSpec) jobSpecs.get(0).getRight()).getConfig().getString("key2"), "val2");
     Assert.assertEquals(((JobSpec) jobSpecs.get(0).getRight()).getConfig().getString("key3.1" + ConfigUtils.STRIP_SUFFIX), "val3");
     Assert.assertEquals(((JobSpec) jobSpecs.get(0).getRight()).getConfig().getString("key3.1.1"), "val4");
+    jobSpecs.clear();
+
+    // If there are other jobSpec in .avro files added by testSpecProducer, they shall still be found.
+    this._fsSpecProducer.addSpec(createTestJobSpec("newTestJob"));
+    jobSpecs = this._fsSpecConsumer.changedSpecs().get();
+    Assert.assertEquals(jobSpecs.size(), 2);
+    Assert.assertEquals(jobSpecs.get(0).getLeft(), SpecExecutor.Verb.ADD);
+    Assert.assertEquals(jobSpecs.get(1).getLeft(), SpecExecutor.Verb.ADD);
+    List<String> uriList = jobSpecs.stream().map(s -> s.getRight().getUri().toString()).collect(Collectors.toList());
+    Assert.assertTrue(uriList.contains( "testJob"));
+    Assert.assertTrue(uriList.contains( "newTestJob"));
   }
 
   @Test (dependsOnMethods = "testAddSpec")
