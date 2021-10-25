71680205a0124269bbec8d82823550f46afb20fb
Arjun <abora@linkedin.com>
2021-10-25 10:22:08 -0700

    do not set a custom maxConnLifetime for sql connection

--- gobblin-service/src/main/java/org/apache/gobblin/service/modules/db/ServiceDatabaseProviderImpl.java
+++ gobblin-service/src/main/java/org/apache/gobblin/service/modules/db/ServiceDatabaseProviderImpl.java
@@ -70,7 +70,6 @@ public class ServiceDatabaseProviderImpl implements ServiceDatabaseProvider {
     // To improve performance, we only check connections on creation, and set a maximum connection lifetime
     // If database goes to read-only mode, then connection would not work correctly for up to configured lifetime
     dataSource.setTestOnCreate(true);
-    dataSource.setMaxConnLifetimeMillis(configuration.getMaxConnectionLifetime().toMillis());
     dataSource.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(10).toMillis());
     dataSource.setMinIdle(2);
     dataSource.setMaxTotal(configuration.getMaxConnections());
