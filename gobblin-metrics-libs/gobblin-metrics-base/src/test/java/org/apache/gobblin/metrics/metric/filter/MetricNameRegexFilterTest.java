780fa40a2a31591fc0149dc936f4acfaeb4186f1
Zihan Li <zihli@linkedin.com>
2022-01-21 16:13:57 -0800

    fix typo

--- gobblin-metrics-libs/gobblin-metrics-base/src/test/java/org/apache/gobblin/metrics/metric/filter/MetricNameRegexFilterTest.java
+++ gobblin-metrics-libs/gobblin-metrics-base/src/test/java/org/apache/gobblin/metrics/metric/filter/MetricNameRegexFilterTest.java
@@ -36,7 +36,7 @@ public class MetricNameRegexFilterTest {
     MetricNameRegexFilter metricNameRegexFilter1 = new MetricNameRegexFilter(".*");
     Assert.assertTrue(metricNameRegexFilter1.matches("test1", mock(Metric.class)));
     Assert.assertTrue(metricNameRegexFilter1.matches("test2", mock(Metric.class)));
-    Assert.assertTrue(metricNameRegexFilter1.matches("test3", mock(Metric.class)));;
+    Assert.assertTrue(metricNameRegexFilter1.matches("test3", mock(Metric.class)));
 
     MetricNameRegexFilter metricNameRegexFilter2 = new MetricNameRegexFilter("test1");
     Assert.assertTrue(metricNameRegexFilter2.matches("test1", mock(Metric.class)));
