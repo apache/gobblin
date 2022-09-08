61efe60ee52cdfa4efb3e2e7ac180f83a3da9a53
AndyJiang99 <20528968+AndyJiang99@users.noreply.github.com>
2022-09-08 09:44:02 -0700

    Reformat file

--- gobblin-modules/gobblin-azkaban/src/main/java/org/apache/gobblin/service/modules/orchestration/AzkabanClient.java
+++ gobblin-modules/gobblin-azkaban/src/main/java/org/apache/gobblin/service/modules/orchestration/AzkabanClient.java
@@ -139,9 +139,9 @@ public class AzkabanClient implements Closeable {
   private void initializeSessionManager() throws AzkabanClientException {
     if (sessionManager == null) {
       this.sessionManager = new AzkabanSessionManager(this.httpClient,
-          this.url,
-          this.username,
-          this.password);
+                                                      this.url,
+                                                      this.username,
+                                                      this.password);
     }
 
   private void intializeExecutorService() {
