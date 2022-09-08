72be494990c54c54484d73048f215a393d20580f
AndyJiang99 <20528968+AndyJiang99@users.noreply.github.com>
2022-09-08 10:10:45 -0700

    Format file for try/catch

--- gobblin-modules/gobblin-azkaban/src/main/java/org/apache/gobblin/service/modules/orchestration/AzkabanClient.java
+++ gobblin-modules/gobblin-azkaban/src/main/java/org/apache/gobblin/service/modules/orchestration/AzkabanClient.java
@@ -119,8 +119,7 @@ public class AzkabanClient implements Closeable {
         .build();
     try {
       this.sessionId = this.sessionManager.fetchSession();
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
       this.sessionId = null;
       this.sessionCreationTime = -1;
       log.error("Failed to fetch session in constructor due to: ", e);
