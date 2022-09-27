ae2ea1965f57f2e5ea0cfc5f1f510cb5c3b45a68
umustafi <umust77@gmail.com>
2022-09-27 15:03:13 -0700

    [GOBBLIN-1713] Add missing sql source validation (#3567)
    * [GOBBLIN-1713] Add missing sql source validation and logs to notify if validation query is not set
    
    * add stacktrace to log message and use existing logger
    
    * set validation query in derived class
    
    * fix class name in log
    
    Co-authored-by: Urmi Mustafi <umustafi@umustafi-mn1.linkedin.biz>

--- gobblin-metastore/src/main/java/org/apache/gobblin/metastore/JobHistoryDataSourceProvider.java
+++ gobblin-metastore/src/main/java/org/apache/gobblin/metastore/JobHistoryDataSourceProvider.java
@@ -17,24 +17,41 @@
 
 package org.apache.gobblin.metastore;
 
+import java.time.Duration;
 import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
 import org.apache.gobblin.configuration.ConfigurationKeys;
 import org.apache.gobblin.password.PasswordManager;
+import org.apache.gobblin.util.jdbc.MysqlDataSourceUtils;
 
 
 /**
  * This class extends {@link org.apache.gobblin.util.jdbc.DataSourceProvider} with its own property keys.
  */
 public class JobHistoryDataSourceProvider extends org.apache.gobblin.util.jdbc.DataSourceProvider {
+  private static final Logger LOG = LoggerFactory.getLogger(JobHistoryDataSourceProvider.class);
 
   @Inject
   public JobHistoryDataSourceProvider(@Named("dataSourceProperties") Properties properties) {
     this.basicDataSource.setDriverClassName(properties.getProperty(ConfigurationKeys.JOB_HISTORY_STORE_JDBC_DRIVER_KEY,
         ConfigurationKeys.DEFAULT_JOB_HISTORY_STORE_JDBC_DRIVER));
+
+    // Set validation query to verify connection
+    if (!Boolean.parseBoolean(properties.getProperty(SKIP_VALIDATION_QUERY, "false"))) {
+      // MySQL server can timeout a connection so need to validate connections before use
+      final String validationQuery = MysqlDataSourceUtils.QUERY_CONNECTION_IS_VALID_AND_NOT_READONLY;
+      LOG.info("setting `DataSource` validation query: '" + validationQuery + "'");
+      this.basicDataSource.setValidationQuery(validationQuery);
+      this.basicDataSource.setTestOnBorrow(true);
+      this.basicDataSource.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(60).toMillis());
+    }
+
     this.basicDataSource.setUrl(properties.getProperty(ConfigurationKeys.JOB_HISTORY_STORE_URL_KEY));
     if (properties.containsKey(ConfigurationKeys.JOB_HISTORY_STORE_USER_KEY)
         && properties.containsKey(ConfigurationKeys.JOB_HISTORY_STORE_PASSWORD_KEY)) {
