47b9bb0024c6005925a6b958c0e0563a35ecb8bb
Zihan Li <zihli@linkedin.com>
2022-11-14 10:01:48 -0800

    [GOBBLIN-1736] Add metrics for change stream monitor and mysql quota manager (#3593)
    * address comments
    
    * use connectionmanager when httpclient is not cloesable
    
    * [GOBBLIN-1730] Include flow execution id when try to cancel/submit job using SimpleKafkaSpecProducer
    
    * remove unnecessary dependency
    
    * [GOBBLIN-1736] Add metrics for change stream monitor and mysql quota manager
    
    * Revert "remove unnecessary dependency"
    
    This reverts commit d6871dc4caa382ea431bd254959c42230b60471c.
    
    * Revert "[GOBBLIN-1730] Include flow execution id when try to cancel/submit job using SimpleKafkaSpecProducer"
    
    This reverts commit f13e6ead489cec226c10db36d82642ba818dd60b.
    
    * address comments
    
    * address comments
    
    Co-authored-by: Zihan Li <zihli@zihli-mn2.linkedin.biz>

--- gobblin-service/src/main/java/org/apache/gobblin/service/monitoring/DagActionStoreChangeMonitor.java
+++ gobblin-service/src/main/java/org/apache/gobblin/service/monitoring/DagActionStoreChangeMonitor.java
@@ -33,6 +33,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.apache.gobblin.kafka.client.DecodeableKafkaRecord;
 import org.apache.gobblin.metrics.ContextAwareMeter;
+import org.apache.gobblin.metrics.event.EventSubmitter;
 import org.apache.gobblin.runtime.api.DagActionStore;
 import org.apache.gobblin.runtime.api.SpecNotFoundException;
 import org.apache.gobblin.runtime.kafka.HighLevelConsumer;
@@ -50,9 +51,10 @@ public class DagActionStoreChangeMonitor extends HighLevelConsumer {
   public static final String DAG_ACTION_CHANGE_MONITOR_PREFIX = "dagActionChangeStore";
 
   // Metrics
-  ContextAwareMeter killsInvoked;
-  ContextAwareMeter resumesInvoked;
-  ContextAwareMeter unexpectedErrors;
+  private ContextAwareMeter killsInvoked;
+  private ContextAwareMeter resumesInvoked;
+  private ContextAwareMeter unexpectedErrors;
+  private ContextAwareMeter messageProcessedMeter;
 
   protected CacheLoader<String, String> cacheLoader = new CacheLoader<String, String>() {
     @Override
@@ -92,7 +94,8 @@ public class DagActionStoreChangeMonitor extends HighLevelConsumer {
   partitioned and processed by only one thread (and corresponding queue).
    */
   protected void processMessage(DecodeableKafkaRecord message) {
-    // TODO: Add metric that service is healthy and we're continuously processing messages.
+    // This will also include the heathCheck message so that we can rely on this to monitor the health of this Monitor
+    messageProcessedMeter.mark();
     String key = (String) message.getKey();
     DagActionStoreChangeEvent value = (DagActionStoreChangeEvent) message.getValue();
 
@@ -171,6 +174,7 @@ public class DagActionStoreChangeMonitor extends HighLevelConsumer {
     this.killsInvoked = this.getMetricContext().contextAwareMeter(RuntimeMetrics.GOBBLIN_DAG_ACTION_STORE_MONITOR_KILLS_INVOKED);
     this.resumesInvoked = this.getMetricContext().contextAwareMeter(RuntimeMetrics.GOBBLIN_DAG_ACTION_STORE_MONITOR_RESUMES_INVOKED);
     this.unexpectedErrors = this.getMetricContext().contextAwareMeter(RuntimeMetrics.GOBBLIN_DAG_ACTION_STORE_MONITOR_UNEXPECTED_ERRORS);
+    this.messageProcessedMeter = this.getMetricContext().contextAwareMeter(RuntimeMetrics.GOBBLIN_DAG_ACTION_STORE_MONITOR_MESSAGE_PROCESSED);
   }
 
 }
