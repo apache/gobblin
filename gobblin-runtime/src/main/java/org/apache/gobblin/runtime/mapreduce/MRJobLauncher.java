22cfc5ceb2d89aeadf6626763c3e67a0b7d816f8
Arjun Singh Bora <abora@linkedin.com>
2023-04-17 15:49:39 -0700

    [GOBBLIN-1785] add MR_JARS_BASE_DIR and logic to delete old mr jar dirs (#3642)
    * add MR_JARS_BASE_DIR and logic to delete old mr jar dirs
    
    * add unit test
    
    * address review comments

--- gobblin-runtime/src/main/java/org/apache/gobblin/runtime/mapreduce/MRJobLauncher.java
+++ gobblin-runtime/src/main/java/org/apache/gobblin/runtime/mapreduce/MRJobLauncher.java
@@ -21,11 +21,14 @@ import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
@@ -228,8 +231,18 @@ public class MRJobLauncher extends AbstractJobLauncher {
       this.fs.delete(this.mrJobDir, true);
     }
     this.unsharedJarsDir = new Path(this.mrJobDir, JARS_DIR_NAME);
-    this.jarsDir = this.jobProps.containsKey(ConfigurationKeys.MR_JARS_DIR) ? new Path(
-        this.jobProps.getProperty(ConfigurationKeys.MR_JARS_DIR)) : this.unsharedJarsDir;
+
+    if (this.jobProps.containsKey(ConfigurationKeys.MR_JARS_BASE_DIR)) {
+      Path jarsBaseDir = new Path(this.jobProps.getProperty(ConfigurationKeys.MR_JARS_BASE_DIR));
+      String monthSuffix = new SimpleDateFormat("yyyy-MM").format(System.currentTimeMillis());
+      cleanUpOldJarsDirIfRequired(this.fs, jarsBaseDir);
+      this.jarsDir = new Path(jarsBaseDir, monthSuffix);
+    } else if (this.jobProps.containsKey(ConfigurationKeys.MR_JARS_DIR)) {
+      this.jarsDir = new Path(this.jobProps.getProperty(ConfigurationKeys.MR_JARS_DIR));
+    } else {
+      this.jarsDir = this.unsharedJarsDir;
+    }
+
     this.fs.mkdirs(this.mrJobDir);
 
     this.jobInputPath = new Path(this.mrJobDir, INPUT_DIR_NAME);
@@ -262,6 +275,14 @@ public class MRJobLauncher extends AbstractJobLauncher {
     log.info("Configured fs:{}", fs);
     log.debug("Configuration: {}", conf);
     startCancellationExecutor();
+  }
+
+  static void cleanUpOldJarsDirIfRequired(FileSystem fs, Path jarsBaseDir) throws IOException {
+    List<FileStatus> jarDirs = Arrays.stream(fs.exists(jarsBaseDir)
+        ? fs.listStatus(jarsBaseDir) : new FileStatus[0]).sorted().collect(Collectors.toList());
+    if (jarDirs.size() > 2) {
+      fs.delete(jarDirs.get(0).getPath(), true);
+    }
   }
 
   @Override
