05149d51a1e688477337cd2843a35829af53bde4
Urmi Mustafi <umustafi@linkedin.com>
2023-09-01 14:09:50 -0700

    update comment

--- gobblin-service/src/main/java/org/apache/gobblin/service/modules/orchestration/Orchestrator.java
+++ gobblin-service/src/main/java/org/apache/gobblin/service/modules/orchestration/Orchestrator.java
@@ -251,7 +251,7 @@ public class Orchestrator implements SpecCatalogListener, Instrumentable {
       // If multi-active scheduler is enabled do not pass onto DagManager, otherwise scheduler forwards it directly
       // Skip flow compilation as well, since we recompile after receiving event from DagActionStoreChangeMonitor later
       if (flowTriggerHandler.isPresent()) {
-        // If triggerTimestampMillis is -1, then it was not set by the job trigger handler, and we cannot handle this event
+        // If triggerTimestampMillis was not set by the job trigger handler, then we do not handle this event
         if (triggerTimestampMillis == TRIGGER_EVENT_TIME_NEVER_SET_VALUE) {
           _log.warn("Skipping execution of spec: {} because missing trigger timestamp in job properties",
               jobProps.getProperty(ConfigurationKeys.JOB_NAME_KEY));
