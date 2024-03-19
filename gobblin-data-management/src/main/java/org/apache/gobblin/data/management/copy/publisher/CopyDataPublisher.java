21cb6abbd8c2941c973020919eb0416b2a467a29
William Lo <wlo@apache.org>
2024-03-19 19:11:55 -0400

    Add logging to see when directories have their permissions resynced to which perms for manifest distcp

--- gobblin-data-management/src/main/java/org/apache/gobblin/data/management/copy/publisher/CopyDataPublisher.java
+++ gobblin-data-management/src/main/java/org/apache/gobblin/data/management/copy/publisher/CopyDataPublisher.java
@@ -196,7 +196,8 @@ public class CopyDataPublisher extends DataPublisher implements UnpublishedHandl
     if (copyableFile.getFileStatus().isDirectory() && this.resyncDirOwnerAndPermission){
       FileStatus dstFile = this.fs.getFileStatus(copyableFile.getDestination());
       // User specifically try to copy dir metadata, so we change the group and permissions on destination even when the dir already existed
-      FileAwareInputStreamDataWriter.safeSetPathPermission(this.fs, dstFile,copyableFile.getDestinationOwnerAndPermission());
+      log.info("Setting destination directory {} owner and permission to {}", dstFile.getPath(), copyableFile.getDestinationOwnerAndPermission().getFsPermission());
+      FileAwareInputStreamDataWriter.safeSetPathPermission(this.fs, dstFile, copyableFile.getDestinationOwnerAndPermission());
     }
     if (preserveDirModTime || copyableFile.getFileStatus().isFile()) {
       // Preserving File ModTime, and set the access time to an initializing value when ModTime is declared to be preserved.
