ae257acae0b321946c8173f0cdf71be7d9fed753
Zihan Li <zihli@linkedin.com>
2024-03-29 16:33:08 -0700

    [HOTFIX] catch exception when fail to compute the iceberg file metrics (#3909)
    * address comments
    
    * use connectionmanager when httpclient is not cloesable
    
    * [GOBBLIN-2024] Improve the performance of GobblinMCEWriter
    
    * not catch exception but just throwing them when failing to get file metrics
    
    * [hotFix] catch exception when fail to compute the metrics
    
    ---------
    
    Co-authored-by: Zihan Li <zihli@zihli-mn2.linkedin.biz>

--- gobblin-iceberg/src/main/java/org/apache/gobblin/iceberg/publisher/GobblinMCEPublisher.java
+++ gobblin-iceberg/src/main/java/org/apache/gobblin/iceberg/publisher/GobblinMCEPublisher.java
@@ -210,7 +210,13 @@ public class GobblinMCEPublisher extends DataPublisher {
           //This means the table is not compatible with iceberg, so return a dummy metric
           return DUMMY_METRICS;
         }
-        return OrcMetrics.fromInputFile(HadoopInputFile.fromPath(path, conf), MetricsConfig.getDefault(), mapping);
+        try {
+          return OrcMetrics.fromInputFile(HadoopInputFile.fromPath(path, conf), MetricsConfig.getDefault(), mapping);
+        } catch (Exception e) {
+          //This means either file is corrupted or the table is not compatible with iceberg (contains complex union), so return a dummy metric
+          return DUMMY_METRICS;
+        }
+
       }
       case AVRO: {
         try {
