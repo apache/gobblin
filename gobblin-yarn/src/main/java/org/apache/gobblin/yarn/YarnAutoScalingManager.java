402b7299d35f29d56a8200db0cf7f0895d50316d
Pradeep Pallikila <pradreddy@linkedin.com>
2024-05-02 09:35:38 +0530

    fixed build

--- gobblin-yarn/src/main/java/org/apache/gobblin/yarn/YarnAutoScalingManager.java
+++ gobblin-yarn/src/main/java/org/apache/gobblin/yarn/YarnAutoScalingManager.java
@@ -18,7 +18,6 @@
 package org.apache.gobblin.yarn;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
