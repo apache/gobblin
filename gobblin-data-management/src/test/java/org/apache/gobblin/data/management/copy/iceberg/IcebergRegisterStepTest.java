30709e32ecf7a71ec6d3d2d2f310291b14d7a0fe
Vivek Rai <vivekrai5839@gmail.com>
2024-06-21 14:16:02 +0530

    added test to retry with override config

--- gobblin-data-management/src/test/java/org/apache/gobblin/data/management/copy/iceberg/IcebergRegisterStepTest.java
+++ gobblin-data-management/src/test/java/org/apache/gobblin/data/management/copy/iceberg/IcebergRegisterStepTest.java
@@ -29,6 +29,8 @@ import org.testng.annotations.Test;
 
 import static org.mockito.Mockito.*;
 
+import static org.apache.gobblin.util.retry.RetryerFactory.RETRY_TIMES;
+
 
 /** Tests for {@link org.apache.gobblin.data.management.copy.iceberg.IcebergRegisterStep} */
 public class IcebergRegisterStepTest {
@@ -41,13 +43,7 @@ public class IcebergRegisterStepTest {
     TableMetadata justPriorDestTableMetadata = mockTableMetadata("foo", "bar");
     TableMetadata readTimeSrcTableMetadata = Mockito.mock(TableMetadata.class); // (no mocked behavior)
     IcebergTable mockTable = mockIcebergTable("foo", "bar"); // matches!
-    IcebergRegisterStep regStep =
-        new IcebergRegisterStep(srcTableId, destTableId, readTimeSrcTableMetadata, justPriorDestTableMetadata, new Properties()) {
-          @Override
-          protected IcebergCatalog createDestinationCatalog() throws IOException {
-            return mockSingleTableIcebergCatalog(mockTable);
-          }
-        };
+    IcebergRegisterStep regStep = createIcebergRegisterStepInstance(readTimeSrcTableMetadata, justPriorDestTableMetadata, mockTable, new Properties());
     try {
       regStep.execute();
       Mockito.verify(mockTable, Mockito.times(1)).registerIcebergTable(any(), any());
@@ -80,14 +76,13 @@ public class IcebergRegisterStepTest {
     TableMetadata justPriorDestTableMetadata = mockTableMetadata("foo", "bar");
     TableMetadata readTimeSrcTableMetadata = Mockito.mock(TableMetadata.class);
     IcebergTable mockTable = mockIcebergTable("foo", "bar");
-    Mockito.doThrow(new RuntimeException()).doThrow(new RuntimeException()).doNothing().when(mockTable).registerIcebergTable(any(), any());
-    IcebergRegisterStep regStep =
-        new IcebergRegisterStep(srcTableId, destTableId, readTimeSrcTableMetadata, justPriorDestTableMetadata, new Properties()) {
-          @Override
-          protected IcebergCatalog createDestinationCatalog() throws IOException {
-            return mockSingleTableIcebergCatalog(mockTable);
-          }
-        };
+    // Mocking registerIcebergTable() call to fail for first two attempts and then succeed
+    // So total number of invocations to registerIcebergTable() should be 3 only
+    Mockito.doThrow(new RuntimeException())
+        .doThrow(new RuntimeException())
+        .doNothing()
+        .when(mockTable).registerIcebergTable(any(), any());
+    IcebergRegisterStep regStep = createIcebergRegisterStepInstance(readTimeSrcTableMetadata, justPriorDestTableMetadata, mockTable, new Properties());
     try {
       regStep.execute();
       Mockito.verify(mockTable, Mockito.times(3)).registerIcebergTable(any(), any());
@@ -97,23 +92,42 @@ public class IcebergRegisterStepTest {
   }
 
   @Test
-  public void testRegisterIcebergTableWithRetryerThrowsRuntimeException() throws IOException {
+  public void testRegisterIcebergTableWithDefaultRetryerConfig() throws IOException {
     TableMetadata justPriorDestTableMetadata = mockTableMetadata("foo", "bar");
     TableMetadata readTimeSrcTableMetadata = Mockito.mock(TableMetadata.class);
     IcebergTable mockTable = mockIcebergTable("foo", "bar");
+    // Mocking registerIcebergTable() call to always throw exception
     Mockito.doThrow(new RuntimeException()).when(mockTable).registerIcebergTable(any(), any());
-    IcebergRegisterStep regStep =
-        new IcebergRegisterStep(srcTableId, destTableId, readTimeSrcTableMetadata, justPriorDestTableMetadata, new Properties()) {
-          @Override
-          protected IcebergCatalog createDestinationCatalog() throws IOException {
-            return mockSingleTableIcebergCatalog(mockTable);
-          }
-        };
+    IcebergRegisterStep regStep = createIcebergRegisterStepInstance(readTimeSrcTableMetadata, justPriorDestTableMetadata, mockTable, new Properties());
     try {
       regStep.execute();
       Assert.fail("Expected Runtime Exception");
     } catch (RuntimeException re) {
-      Assert.assertTrue(re.getMessage().startsWith("Failed to register iceberg table (retried"), re.getMessage());
+      // The default number of retries is 5 so register iceberg table should fail after retrying for 5 times
+      String msg = String.format("Failed to register iceberg table : (src: {%s}) - (dest: {%s}) : (retried %d times)", srcTableId, destTableId, 5);
+      Assert.assertTrue(re.getMessage().startsWith(msg), re.getMessage());
+    }
+  }
+
+  @Test
+  public void testRegisterIcebergTableWithOverrideRetryerConfig() throws IOException {
+    TableMetadata justPriorDestTableMetadata = mockTableMetadata("foo", "bar");
+    TableMetadata readTimeSrcTableMetadata = Mockito.mock(TableMetadata.class);
+    IcebergTable mockTable = mockIcebergTable("foo", "bar");
+    // Mocking registerIcebergTable() call to always throw exception
+    Mockito.doThrow(new RuntimeException()).when(mockTable).registerIcebergTable(any(), any());
+    Properties properties = new Properties();
+    String retryCount = "10";
+    // Changing the number of retries to 10
+    properties.setProperty(IcebergRegisterStep.RETRYER_CONFIG_PREFIX + "." + RETRY_TIMES, retryCount);
+    IcebergRegisterStep regStep = createIcebergRegisterStepInstance(readTimeSrcTableMetadata, justPriorDestTableMetadata, mockTable, properties);
+    try {
+      regStep.execute();
+      Assert.fail("Expected Runtime Exception");
+    } catch (RuntimeException re) {
+      // register iceberg table should fail after retrying for retryCount times mentioned above
+      String msg = String.format("Failed to register iceberg table : (src: {%s}) - (dest: {%s}) : (retried %d times)", srcTableId, destTableId, Integer.parseInt(retryCount));
+      Assert.assertTrue(re.getMessage().startsWith(msg), re.getMessage());
     }
   }
 
@@ -141,5 +155,17 @@ public class IcebergRegisterStepTest {
     IcebergCatalog catalog = Mockito.mock(IcebergCatalog.class);
     Mockito.when(catalog.openTable(any())).thenReturn(mockTable);
     return catalog;
+  }
+
+  private IcebergRegisterStep createIcebergRegisterStepInstance(TableMetadata readTimeSrcTableMetadata,
+                                                                TableMetadata justPriorDestTableMetadata,
+                                                                IcebergTable mockTable,
+                                                                Properties properties) {
+    return new IcebergRegisterStep(srcTableId, destTableId, readTimeSrcTableMetadata, justPriorDestTableMetadata, properties) {
+      @Override
+      protected IcebergCatalog createDestinationCatalog() throws IOException {
+        return mockSingleTableIcebergCatalog(mockTable);
+      }
+    };
   }
 }
