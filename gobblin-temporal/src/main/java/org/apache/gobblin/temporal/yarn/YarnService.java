17b71a22faacf0ecec9986ec2371a836f002a63f
William Lo <wlo@apache.org>
2024-10-02 19:56:31 -0400

    Only load added jars in the cache

--- gobblin-temporal/src/main/java/org/apache/gobblin/temporal/yarn/YarnService.java
+++ gobblin-temporal/src/main/java/org/apache/gobblin/temporal/yarn/YarnService.java
@@ -25,6 +25,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -546,11 +547,20 @@ class YarnService extends AbstractIdleService {
 
     FileStatus[] statuses = this.fs.listStatus(destDir);
     if (statuses != null) {
+      Set<String> libJarNames = new HashSet<>(Arrays.asList(this.config.getString(GobblinYarnConfigurationKeys.YARN_APPLICATION_LIB_JAR_LIST).split(",")));
+      String containerJars = this.config.hasPath(GobblinYarnConfigurationKeys.CONTAINER_JARS_KEY) ?
+          this.config.getString(GobblinYarnConfigurationKeys.CONTAINER_JARS_KEY) : "";
       for (FileStatus status : statuses) {
+        String fileName = status.getPath().getName();
+        // Ensure that we are only adding jars that were uploaded by the YarnAppLauncher for this application
+        if (fileName.contains(".jar") && !(libJarNames.contains(fileName) || containerJars.contains(fileName))) {
+          continue;
+        }
         YarnHelixUtils.addFileAsLocalResource(this.fs, status.getPath(), LocalResourceType.FILE, resourceMap);
       }
     }
   }
+
 
 
   protected ByteBuffer getSecurityTokens() throws IOException {
