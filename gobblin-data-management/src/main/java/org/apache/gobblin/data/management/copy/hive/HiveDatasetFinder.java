e736bab9beb1e1ce61a7f9cb3964b76d71ea23b2
William Lo <wlo@apache.org>
2024-10-20 21:47:34 -0400

    add more details to log

--- gobblin-data-management/src/main/java/org/apache/gobblin/data/management/copy/hive/HiveDatasetFinder.java
+++ gobblin-data-management/src/main/java/org/apache/gobblin/data/management/copy/hive/HiveDatasetFinder.java
@@ -271,7 +271,8 @@ public class HiveDatasetFinder implements IterableDatasetFinder<HiveDataset> {
             Table table = client.get().getTable(dbAndTable.getDb(), dbAndTable.getTable());
             if ((tableFilter.isPresent() && !tableFilter.get().apply(table))
                 || !shouldAllowTableLocation(tableFolderAllowlistRegex, table)) {
-              log.info("Ignoring table {} as its underlying location does not part of allowlist regex {}", dbAndTable, tableFolderAllowlistRegex);
+              log.info("Ignoring table {} as its underlying location {} does not part of allowlist regex {}", dbAndTable,
+                  table.getSd().getLocation(), tableFolderAllowlistRegex);
               continue;
             }
 
