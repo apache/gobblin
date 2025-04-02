51fe43e8897acec417bfc20539cb8be7585cb256
pratapAditya04 <thesingh.adi.work@gmail.com>
2025-04-02 23:36:35 +0530

    added a unique UUID identifier for a gaas job execution

--- gobblin-api/src/main/java/org/apache/gobblin/configuration/ConfigurationKeys.java
+++ gobblin-api/src/main/java/org/apache/gobblin/configuration/ConfigurationKeys.java
@@ -1047,7 +1047,7 @@ public class ConfigurationKeys {
   public static final String AZKABAN_FLOW_ID = "azkaban.flow.flowid";
   public static final String AZKABAN_JOB_ID = "azkaban.job.id";
   public static final String AZKABAN_EXEC_ID = "azkaban.flow.execid";
-
+  // Configuration Key for setting a unique job execution identifier in GaaS, the value is a UUID
   public static final String GAAS_JOB_EXEC_ID = "gaas.job.execid";
   public static final String AZKABAN_URL = "azkaban.link.execution.url";
   public static final String AZKABAN_FLOW_URL = "azkaban.link.workflow.url";
