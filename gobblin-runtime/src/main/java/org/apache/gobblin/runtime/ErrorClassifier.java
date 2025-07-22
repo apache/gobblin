01fc9a6be99b349c6dfbf0c5ab31424f7499c981
NamsB7 <126075176+NamsB7@users.noreply.github.com>
2025-07-22 21:16:05 +0530

    resolving static tests

--- gobblin-runtime/src/main/java/org/apache/gobblin/runtime/ErrorClassifier.java
+++ gobblin-runtime/src/main/java/org/apache/gobblin/runtime/ErrorClassifier.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.gobblin.runtime;
 
 import java.io.IOException;
@@ -183,7 +200,8 @@ public class ErrorClassifier {
       result.highestCategoryName = _defaultErrorCategory.getCategoryName();
 
       for (Issue issue : result.unmatched) {
-        result.categoryToIssues.computeIfAbsent(_defaultErrorCategory.getCategoryName(), k -> new ArrayList<>()).add(issue);
+        result.categoryToIssues.computeIfAbsent(_defaultErrorCategory.getCategoryName(), k -> new ArrayList<>())
+            .add(issue);
       }
     }
   }
@@ -193,7 +211,8 @@ public class ErrorClassifier {
     String details = buildDetailsString(matchedIssues);
 
     return Issue.builder().summary("ErrorCategory: " + categoryName).details(details).severity(IssueSeverity.ERROR)
-        .time(ZonedDateTime.now()).code(FINAL_ISSUE_ERROR_CODE).sourceClass(null).exceptionClass(null).properties(null).build();
+        .time(ZonedDateTime.now()).code(FINAL_ISSUE_ERROR_CODE).sourceClass(null).exceptionClass(null).properties(null)
+        .build();
   }
 
   private String buildDetailsString(List<Issue> issues) {
