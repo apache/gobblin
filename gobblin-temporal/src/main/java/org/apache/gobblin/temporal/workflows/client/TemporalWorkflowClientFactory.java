36d7a52e2e45c1e7761ab8f58ac7915423cdc682
pratapAditya04 <thesingh.adi.work@gmail.com>
2025-11-06 15:37:48 +0530

    removed chacha cipher suites

--- gobblin-temporal/src/main/java/org/apache/gobblin/temporal/workflows/client/TemporalWorkflowClientFactory.java
+++ gobblin-temporal/src/main/java/org/apache/gobblin/temporal/workflows/client/TemporalWorkflowClientFactory.java
@@ -73,9 +73,7 @@ public class TemporalWorkflowClientFactory {
                 /* REQUIRED BY HTTP/2 SPEC */
 
                 "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-                "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-                "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
-                "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256"
+                "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
         ));
 
         String keyStoreType = config.getString(SSL_KEYSTORE_TYPE);
